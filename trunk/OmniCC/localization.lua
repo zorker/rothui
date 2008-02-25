f3ec0ec7987da94b7fadf4dd69e31b705371f782
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2008-02-25 17:56:11 +0000

    the omnicc i use

---
+++ trunk/OmniCC/localization.lua
@@ -0,0 +1,35 @@
+--[[
+	OmniCC Localization File
+		English (default language)
+--]]
+
+OMNICC_LOCALS = {}
+local L = OMNICC_LOCALS
+L.UpgradeIncompatible = "Upgrading from an incompatible version. Default settings loaded"
+L.Updated = "Updated to v%s"
+
+-- L.Commands = "Commands (/omnicc)"
+-- L.SetSizeDesc = "Set font size. 20 is default"
+-- L.SetFontDesc = format("Set the font to use. %s is default", STANDARD_TEXT_FONT)
+-- L.SetColorDesc = "Set the color to use for cooldowns of <dur>. <dur> can be vlong, long, med or short"
+-- L.SetScaleDesc = "Set the scale to use for cooldowns of <dur>. <dur> can be vlong, long, med or short"
+-- L.SetMinDurDesc = "Set the minimum duration (secs) a cooldown should be to show text. Default value of 3"
+-- L.ToggleModelDesc = "Toggles the cooldown model"
+-- L.TogglePulseDesc = "Toggles a pulse when cooldowns are finished"
+-- L.ToggleMMSSDesc = "Toggles MM:SS format"
+-- L.ToggleBlacklistDesc = "Toggles showing cooldown text on blacklisted frames"
+-- L.ResetDesc = "Returns to default settings"
+
+-- L.SetFont = "Set font to %s"
+-- L.SetFontSize = "Set font size to %s"
+-- L.SetMinDur = "Set minimum duration to %s"
+-- L.ModelsEnabled = "Cooldown models enabled"
+-- L.ModelsDisabled = "Cooldown models disabled"
+-- L.PulseEnabled = "Pulse enabled"
+-- L.PulseDisabled = "Pulse disabled"
+-- L.MMSSEnabled = "MM:SS format enabled"
+-- L.MMSSDisabled = "MM:SS format disabled"
+-- L.BlacklistEnabled = "Hiding cooldown text on blacklisted frames"
+-- L.BlacklistDisabled = "Showing cooldown text on all frames"
+-- L.Reset = "Loaded default settings"
+-- L.InvalidCommand = "\"%s\" is an invalid command"

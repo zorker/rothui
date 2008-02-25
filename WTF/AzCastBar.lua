c91d5463f6b94f8c49b9874a2fa0c360d84213b8
Erik Raetz <zorker@users.noreply.github.com>
2008-02-25 18:00:19 +0000

    my azcastbar settings

---
+++ WTF/AzCastBar.lua
@@ -0,0 +1,264 @@
+
+AzCastBar_Config = {
+	["Player"] = {
+		["fontFace"] = "Fonts\\FRIZQT__.TTF",
+		["left"] = 460.6666564941406,
+		["colSafezone"] = {
+			0.3, -- [1]
+			0.8, -- [2]
+			0.3, -- [3]
+			0.6, -- [4]
+		},
+		["colInterrupt"] = {
+			1, -- [1]
+			0.75, -- [2]
+			0.5, -- [3]
+			1, -- [4]
+		},
+		["colNormal"] = {
+			0.7372549019607844, -- [1]
+			0.580392156862745, -- [2]
+			0.3882352941176471, -- [3]
+			1, -- [4]
+		},
+		["colBackdrop"] = {
+			0, -- [1]
+			0, -- [2]
+			0, -- [3]
+			1, -- [4]
+		},
+		["fontFlags"] = "",
+		["enabled"] = true,
+		["backdropIndent"] = -1,
+		["texture"] = "Interface\\Addons\\rTextures\\Rounded",
+		["bottom"] = 177.1111602783203,
+		["fontSize"] = 12,
+		["fadeTime"] = 0.6,
+		["showRank"] = false,
+		["width"] = 215,
+		["alpha"] = 1,
+		["backdropBG"] = "Interface\\Tooltips\\UI-Tooltip-Background",
+		["colFailed"] = {
+			1, -- [1]
+			0.5, -- [2]
+			0.5, -- [3]
+			1, -- [4]
+		},
+		["showIcon"] = true,
+		["height"] = 18,
+		["safeZone"] = true,
+		["colFont"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+		["colBackground"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+	},
+	["optionsLeft"] = 380.5556640625,
+	["Focus"] = {
+		["fontFace"] = "Fonts\\FRIZQT__.TTF",
+		["left"] = 57.11111831665039,
+		["colInterrupt"] = {
+			1, -- [1]
+			0.75, -- [2]
+			0.5, -- [3]
+			1, -- [4]
+		},
+		["colNormal"] = {
+			0.4, -- [1]
+			0.6, -- [2]
+			0.8, -- [3]
+			1, -- [4]
+		},
+		["colBackdrop"] = {
+			0.1, -- [1]
+			0.22, -- [2]
+			0.35, -- [3]
+			1, -- [4]
+		},
+		["fontFlags"] = "",
+		["enabled"] = false,
+		["backdropIndent"] = -3,
+		["bottom"] = 459.7778930664063,
+		["texture"] = "Interface\\Addons\\rTextures\\Rounded",
+		["fadeTime"] = 0.6,
+		["alpha"] = 1,
+		["width"] = 300,
+		["fontSize"] = 12,
+		["showRank"] = false,
+		["colFailed"] = {
+			1, -- [1]
+			0.5, -- [2]
+			0.5, -- [3]
+			1, -- [4]
+		},
+		["backdropBG"] = "Interface\\Tooltips\\UI-Tooltip-Background",
+		["height"] = 20,
+		["showIcon"] = false,
+		["colFont"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+		["colBackground"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+	},
+	["Target"] = {
+		["fontFace"] = "Fonts\\FRIZQT__.TTF",
+		["left"] = 461.5559997558594,
+		["colInterrupt"] = {
+			1, -- [1]
+			0.75, -- [2]
+			0.5, -- [3]
+			1, -- [4]
+		},
+		["colNormal"] = {
+			0.8, -- [1]
+			0.007843137254901961, -- [2]
+			0, -- [3]
+			1, -- [4]
+		},
+		["colBackdrop"] = {
+			0.1, -- [1]
+			0.22, -- [2]
+			0.35, -- [3]
+			1, -- [4]
+		},
+		["fontFlags"] = "",
+		["enabled"] = true,
+		["backdropIndent"] = -1,
+		["bottom"] = 209.5554046630859,
+		["texture"] = "Interface\\Addons\\rTextures\\Rounded",
+		["fadeTime"] = 0.6,
+		["alpha"] = 1,
+		["width"] = 215,
+		["fontSize"] = 12,
+		["showRank"] = false,
+		["colFailed"] = {
+			1, -- [1]
+			0.5, -- [2]
+			0.5, -- [3]
+			1, -- [4]
+		},
+		["backdropBG"] = "Interface\\Tooltips\\UI-Tooltip-Background",
+		["height"] = 18,
+		["showIcon"] = true,
+		["colFont"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+		["colBackground"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+	},
+	["optionsBottom"] = 315.333251953125,
+	["Pet"] = {
+		["fontFace"] = "Fonts\\FRIZQT__.TTF",
+		["left"] = 58,
+		["colInterrupt"] = {
+			1, -- [1]
+			0.75, -- [2]
+			0.5, -- [3]
+			1, -- [4]
+		},
+		["colNormal"] = {
+			0.4, -- [1]
+			0.6, -- [2]
+			0.8, -- [3]
+			1, -- [4]
+		},
+		["colBackdrop"] = {
+			0.1, -- [1]
+			0.22, -- [2]
+			0.35, -- [3]
+			1, -- [4]
+		},
+		["fontFlags"] = "",
+		["enabled"] = false,
+		["backdropIndent"] = -3,
+		["bottom"] = 421.111083984375,
+		["texture"] = "Interface\\Addons\\rTextures\\Rounded",
+		["fadeTime"] = 0.6,
+		["alpha"] = 1,
+		["width"] = 300,
+		["fontSize"] = 12,
+		["showRank"] = false,
+		["colFailed"] = {
+			1, -- [1]
+			0.5, -- [2]
+			0.5, -- [3]
+			1, -- [4]
+		},
+		["backdropBG"] = "Interface\\Tooltips\\UI-Tooltip-Background",
+		["height"] = 20,
+		["showIcon"] = false,
+		["colFont"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+		["colBackground"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+	},
+	["Mirror"] = {
+		["fontSize"] = 12,
+		["left"] = 460.6665649414063,
+		["colNormal"] = {
+			0.4, -- [1]
+			0.6, -- [2]
+			0.8, -- [3]
+			1, -- [4]
+		},
+		["colBackdrop"] = {
+			0.1, -- [1]
+			0.22, -- [2]
+			0.35, -- [3]
+			1, -- [4]
+		},
+		["fontFlags"] = "",
+		["enabled"] = true,
+		["backdropIndent"] = -1,
+		["bottom"] = 144.6664428710938,
+		["fadeTime"] = 0.6,
+		["alpha"] = 1,
+		["width"] = 215,
+		["texture"] = "Interface\\Addons\\rTextures\\Rounded",
+		["fontFace"] = "Fonts\\FRIZQT__.TTF",
+		["showIcon"] = true,
+		["height"] = 18,
+		["backdropBG"] = "Interface\\Tooltips\\UI-Tooltip-Background",
+		["colFont"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+		["colBackground"] = {
+			1, -- [1]
+			1, -- [2]
+			1, -- [3]
+			1, -- [4]
+		},
+	},
+}

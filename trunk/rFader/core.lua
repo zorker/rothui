ddb898d5f443816a747377b570984d10eb44ccc6
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2008-03-01 12:40:28 +0000

    modified combat fader by Minilyn

---
+++ trunk/rFader/core.lua
@@ -0,0 +1,35 @@
+local hide = CreateFrame("Frame", nil, UIParent)
+
+hide:RegisterEvent("PLAYER_REGEN_ENABLED")
+hide:RegisterEvent("PLAYER_REGEN_DISABLED")
+hide:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+hide:SetScript("OnEvent", function ()
+  if event == "PLAYER_REGEN_ENABLED" or event == "PLAYER_ENTERING_WORLD" then
+
+    ShapeshiftBarFrame:SetAlpha(0)
+    MultiBarRight:SetAlpha(0.5)
+    MultiBarLeft:SetAlpha(0.5)
+    MultiBarBottomLeft:SetAlpha(1)
+    MultiBarBottomRight:SetAlpha(1)
+    MainMenuBar:SetAlpha(1)
+    BonusActionBarFrame:SetAlpha(1)
+    pUF_Player:SetAlpha(1)
+    pUF_Target:SetAlpha(1)
+    pUF_ToT:SetAlpha(1)
+
+  elseif event == "PLAYER_REGEN_DISABLED" then
+
+    ShapeshiftBarFrame:SetAlpha(0)
+    MultiBarRight:SetAlpha(0)
+    MultiBarLeft:SetAlpha(0)
+    MultiBarBottomLeft:SetAlpha(1)
+    MultiBarBottomRight:SetAlpha(1)
+    MainMenuBar:SetAlpha(1)
+    BonusActionBarFrame:SetAlpha(1)
+    pUF_Player:SetAlpha(1)
+    pUF_Target:SetAlpha(1)
+    pUF_ToT:SetAlpha(1)
+    
+  end
+end)

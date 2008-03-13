7c87862f0a9e178658a156f9b856f100e8a71ca3
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2008-03-13 11:45:20 +0000

    frame

--- trunk/rRaidCheck/rRaidCheck.lua
+++ trunk/rRaidCheck/rRaidCheck.lua
@@ -1,5 +1,84 @@
+--[[-------------------------------------------------------------------------
+  Copyright (c) 2008, zork
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above
+        copyright notice, this list of conditions and the following
+        disclaimer in the documentation and/or other materials provided
+        with the distribution.
+      * Neither the name of rRaidCheck nor the names of its contributors may
+        be used to endorse or promote products derived from this
+        software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES LOSS OF USE,
+  DATA, OR PROFITS OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+---------------------------------------------------------------------------]]--
   
-  -- NOT FUNCTIONAL ATM, DON'T USE IT
+  local addon = CreateFrame"Frame"
+  local _G = getfenv(0)  
+  
+  addon:SetScript("OnEvent", function()
+  
+    if(event=="PLAYER_LOGIN") then
+      addon:rrc_createframe()
+    end  
+
+  end)
+  
+  
+  function addon:rrc_createframe()
+    local f = CreateFrame("Frame","rrc_frame",UIParent)
+    f:SetFrameStrata("BACKGROUND")
+    f:SetWidth(100)
+    f:SetHeight(100)
+    f:SetBackdrop({
+      bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
+      edgeFile = "", 
+      tile = true, tileSize = 16, edgeSize = 16, 
+      insets = { left = 0, right = 0, top = 0, bottom = 0 }
+    })
+    f:SetBackdropColor(0,0,0,0.8)
+    f:ClearAllPoints()  
+    f:SetPoint("TOPLEFT",UIParent,"TOPLEFT",0,0)
+    f:Show()
+    
+    local rrc_t0 = f:CreateFontString("rrc_t0")
+    rrc_t0:SetPoint("TOPLEFT", 5, -5)
+    rrc_t0:SetFontObject(GameFontHighlight)
+    rrc_t0:SetTextColor(0, 1, 0)
+    rrc_t0:SetText("Raid Health")
+    rrc_t0:Show()
+  
+    local rrc_t1 = f:CreateFontString("rrc_t1")
+    rrc_t1:SetPoint("TOPLEFT", "rrc_t0", "BOTTOMLEFT", 0, -2)
+    rrc_t1:SetFontObject(GameFontHighlight)
+    rrc_t1:SetTextColor(0, 0, 1)
+    rrc_t1:SetText("Raid Mana")
+    rrc_t1:Show()
+    
+    local rrc_t2 = f:CreateFontString("rrc_t2")
+    rrc_t2:SetPoint("TOPLEFT", "rrc_t1", "TOPRIGHT", 5, 0)
+    rrc_t2:SetFontObject(GameFontHighlight)
+    rrc_t2:SetTextColor(0.6, 0.6, 0.6)
+    rrc_t2:SetText("Dead")
+    rrc_t2:Show()
+    
+  end
   
   function addon:rrc_checkhealth()
     local n = GetNumRaidMembers()  
@@ -35,6 +114,7 @@
         avg_raid_hp = floor(sum_per_hp / d)
       end
       -- avg_raid_hp liefert den % HP wert des Raids, offline, tote, oder ghosts werden nicht gewertet
+      return avg_raid_hp
     end
   end
   
@@ -74,6 +154,7 @@
         avg_raid_mp = floor(sum_per_mp / d)
       end
       -- avg_raid_mp liefert den % MP wert des Raids, offline, tote, ghosts oder klassen ohne Powertype=0 werden nicht gewertet
+      return avg_raid_mp
     end
   end
   
@@ -90,6 +171,7 @@
         end
       end
       -- c liefert Anzahl an toten
+      return c
     end
   end
   
@@ -106,6 +188,7 @@
         end
       end
       -- c liefert Anzahl an ghosts
+      return c
     end
   end
   
@@ -122,5 +205,8 @@
         end
       end
       -- c liefert Anzahl an Offline
+      return c
     end
   end
+  
+  addon:RegisterEvent"PLAYER_LOGIN"

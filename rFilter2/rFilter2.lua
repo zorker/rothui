d863d19302600e10bd3751fc1c71a22266eafd5f
Erik Raetz <zorker@users.noreply.github.com>
2008-06-07 07:56:28 +0000

    small updates

--- rFilter2/rFilter2.lua
+++ rFilter2/rFilter2.lua
@@ -45,11 +45,11 @@
   
   --alpha when buff/debuff/cd not active
   --values between 0 and one are allowed
-  local alpha_when_not_active = 0.2
+  local alpha_when_not_active = 0
   
   --make icon grey when buff noch active
   --0 = off // 1 = on
-  local use_grey_vertex = 1
+  local use_grey_vertex = 0
 
   --DEFAULT_CHAT_FRAME:AddMessage("found "..rf2_player_name.." : "..rf2_player_class)
   
@@ -74,20 +74,20 @@
   elseif rf2_player_name == "Rothar" and rf2_player_class == "WARRIOR" then
     rf2_spell_list = {
       buffs = {
-        [1] = { tag = "commanding", spellid = 469, size = 20, fontsize = 16, posx = -60, posy = -90, framestrata = "BACKGROUND", anchor = "UIParent"},
-        [2] = { tag = "battle", spellid = 2048, size = 20, fontsize = 16, posx = -30, posy = -90, framestrata = "BACKGROUND", anchor = "UIParent"},
+        [1] = { tag = "commanding", spellid = 469, size = 20, fontsize = 16, posx = -371, posy = -180, framestrata = "BACKGROUND", anchor = "UIParent"},
+        [2] = { tag = "battle", spellid = 2048, size = 20, fontsize = 16, posx = -343, posy = -180, framestrata = "BACKGROUND", anchor = "UIParent"},
         --[3] = { tag = "block", spellid = 2565, size = 20, fontsize = 15, posx = 30, posy = -260, framestrata = "BACKGROUND", anchor = "UIParent"},
         --[3] = { tag = "rampage", spellid = 30033, size = 18, fontsize = 14, posx = -25, posy = -260, framestrata = "BACKGROUND", anchor = "UIParent"},
         --[3] = { tag = "berserker", spellid = 18499, size = 32, fontsize = 24, posx = 160, posy = 0, framestrata = "BACKGROUND", anchor = "UIParent"},
         --[5] = { tag = "block", spellid = 2565, size = 32, fontsize = 24, posx = 80, posy = 0, framestrata = "BACKGROUND", anchor = "UIParent"},
       },
       debuffs = {
-        [1] = { tag = "sunder", spellid = 25225, size = 20, fontsize = 16, posx = 0, posy = -90, framestrata = "BACKGROUND", anchor = "UIParent"},
-        [2] = { tag = "demo", spellid = 25203, size = 20, fontsize = 16, posx = 30, posy = -90, framestrata = "BACKGROUND", anchor = "UIParent"},
-        [3] = { tag = "clap", spellid = 25264, size = 20, fontsize = 16, posx = 60, posy = -90, framestrata = "BACKGROUND", anchor = "UIParent"},
-        --[4] = { tag = "scorpid", spellid = 3043, size = 18, fontsize = 14, posx = 0, posy = -230, framestrata = "BACKGROUND", anchor = "UIParent"},
-        --[5] = { tag = "fearie", spellid = 26993, size = 18, fontsize = 14, posx = 25, posy = -230, framestrata = "BACKGROUND", anchor = "UIParent"},
-        --[6] = { tag = "fearieferal", spellid = 27011, size = 18, fontsize = 14, posx = 25, posy = -230, framestrata = "BACKGROUND", anchor = "UIParent"},
+        [1] = { tag = "sunder", spellid = 25225, size = 20, fontsize = 16, posx = -300, posy = -180, framestrata = "BACKGROUND", anchor = "UIParent"},
+        [2] = { tag = "demo", spellid = 25203, size = 20, fontsize = 16, posx = -272, posy = -180, framestrata = "BACKGROUND", anchor = "UIParent"},
+        [3] = { tag = "clap", spellid = 25264, size = 20, fontsize = 16, posx = -244, posy = -180, framestrata = "BACKGROUND", anchor = "UIParent"},
+        [4] = { tag = "scorpid", spellid = 3043, size = 18, fontsize = 16, posx = -216, posy = -180, framestrata = "BACKGROUND", anchor = "UIParent"},
+        [5] = { tag = "fearie", spellid = 26993, size = 18, fontsize = 16, posx = -188, posy = -180, framestrata = "BACKGROUND", anchor = "UIParent"},
+        [6] = { tag = "fearieferal", spellid = 27011, size = 18, fontsize = 16, posx = -188, posy = -180, framestrata = "BACKGROUND", anchor = "UIParent"},
         --[7] = { tag = "curseofreck", spellid = 27226, size = 18, fontsize = 14, posx = 50, posy = -230, framestrata = "BACKGROUND", anchor = "UIParent"},
         --[8] = { tag = "curseoftong", spellid = 11719, size = 18, fontsize = 14, posx = 75, posy = -230, framestrata = "BACKGROUND", anchor = "UIParent"},
       },
@@ -216,7 +216,7 @@
     local f4 = _G["rf2_"..frameTag.."_icon"]
     if use_grey_vertex == 1
     then
-      f4:SetVertexColor(0.4,0.4,0.4)
+      f4:SetVertexColor(0.2,0.2,0.2)
     end
     
     for i = 1, 40 do
@@ -269,7 +269,7 @@
     local f4 = _G["rf2_"..frameTag.."_icon"]
     if use_grey_vertex == 1
     then
-      f4:SetVertexColor(0.4,0.4,0.4)
+      f4:SetVertexColor(0.2,0.2,0.2)
     end
 
     for i = 1, 40 do
@@ -323,7 +323,7 @@
     local f4 = _G["rf2_"..frameTag.."_icon"]
     if use_grey_vertex == 1
     then
-      f4:SetVertexColor(0.4,0.4,0.4)
+      f4:SetVertexColor(0.2,0.2,0.2)
     end
 
     local spellCooldownStartTime, spellCooldownDuration, spellEnabled = GetSpellCooldown(spellName);

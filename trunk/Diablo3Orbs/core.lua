f3b7ad3cb52c5164ee1ebe961cdfc658cd537ab6
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2008-07-04 18:43:53 +0000

    orb with animation

--- trunk/Diablo3Orbs/core.lua
+++ trunk/Diablo3Orbs/core.lua
@@ -15,9 +15,34 @@ function initDiabloHealth()
  diabloOrbHealthGloss:SetAllPoints(diabloOrbHealth)
  
  diabloOrbHealthBackground:SetTexture("Interface\\AddOns\\Diablo3Orbs\\Background.tga")
- diabloOrbHealthFilling:SetTexture("Interface\\AddOns\\Diablo3Orbs\\Filling.tga")
+ --diabloOrbHealthFilling:SetTexture("Interface\\AddOns\\Diablo3Orbs\\Filling.tga")
  diabloOrbHealthGloss:SetTexture("Interface\\AddOns\\Diablo3Orbs\\Gloss.tga")
+
+  diabloOrbHealthFilling:SetVertexColor(1,0,0)
  
+  local totalElapsed = 0
+  local counter = 0
+  local modifier = 100
+  
+  --update_timer = TIME IN SEC before update. BE CAREFUL HERE!
+  local update_timer = 2
+  
+  local function OnUpdateFunc(self, elapsed)
+    totalElapsed = totalElapsed + elapsed * modifier
+    local update_me = update_timer * modifier
+    if (totalElapsed < update_me) then 
+      return 
+    else
+      totalElapsed = totalElapsed - floor(totalElapsed)
+      --totalElapsed = 0
+      diabloOrbHealthFilling:SetTexture("Interface\\AddOns\\Diablo3Orbs\\d3orbanimation\\d3_orb_ani_frame"..counter..".tga")
+      counter = counter+1
+      if counter > 24 then
+        counter = 0
+      end
+    end
+  end
+  diabloOrbHealth:SetScript("OnUpdate", OnUpdateFunc)
  
  diabloOrbHealth:SetScript("OnEvent", function(frame, event, unit)
   if event == "UNIT_HEALTH" and unit == "player" then
@@ -50,9 +75,35 @@ function initDiabloMana()
  diabloOrbManaGloss:SetAllPoints(diabloOrbMana)
  
  diabloOrbManaBackground:SetTexture("Interface\\AddOns\\Diablo3Orbs\\Background.tga")
- diabloOrbManaFilling:SetTexture("Interface\\AddOns\\Diablo3Orbs\\FillingMana.tga")
+ --diabloOrbManaFilling:SetTexture("Interface\\AddOns\\Diablo3Orbs\\FillingMana.tga")
  diabloOrbManaGloss:SetTexture("Interface\\AddOns\\Diablo3Orbs\\Gloss.tga")
  
+  diabloOrbManaFilling:SetVertexColor(0,0.7,1)
+ 
+  local totalElapsed = 0
+  local counter = 13
+  local modifier = 100
+  local modifier = 100
+  
+  --update_timer = TIME IN SEC before update. BE CAREFUL HERE!
+  local update_timer = 2
+  
+  local function OnUpdateFunc(self, elapsed)
+    totalElapsed = totalElapsed + elapsed*modifier
+    local update_me = update_timer * modifier
+    if (totalElapsed < update_me) then 
+      return 
+    else
+      totalElapsed = totalElapsed - floor(totalElapsed)
+      --totalElapsed = 0
+      diabloOrbManaFilling:SetTexture("Interface\\AddOns\\Diablo3Orbs\\d3orbanimation\\d3_orb_ani_frame"..counter..".tga")
+      counter = counter+1
+      if counter > 24 then
+        counter = 0
+      end
+    end
+  end
+  diabloOrbMana:SetScript("OnUpdate", OnUpdateFunc)
  
  diabloOrbMana:SetScript("OnEvent", function(frame, event, unit)
   if (event == "UNIT_MANA" or event == "UNIT_RAGE" or event == "UNIT_ENERGY") and unit == "player" then

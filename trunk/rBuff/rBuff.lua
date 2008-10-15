56613ac3678d63d6c2e9946ec5c315d6f35650cd
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2008-10-15 10:37:40 +0000

    wotlk?!

--- trunk/rBuff/rBuff.lua
+++ trunk/rBuff/rBuff.lua
@@ -1,68 +1,54 @@
---[[-------------------------------------------------------------------------
-  Copyright (c) 2008, p3lim
-  Copyright (c) 2008, zork
-  All rights reserved.
 
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are
-  met:
+  --rdy up for wotlk
 
-      * Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-      * Redistributions in binary form must reproduce the above
-        copyright notice, this list of conditions and the following
-        disclaimer in the documentation and/or other materials provided
-        with the distribution.
-      * Neither the name of rBuff nor the names of its contributors may
-        be used to endorse or promote products derived from this
-        software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
----------------------------------------------------------------------------]]
-  
   BUFF_FLASH_TIME_ON = 0.8;
   BUFF_FLASH_TIME_OFF = 0.8;
   BUFF_MIN_ALPHA = 0.70;
 
-  local addon = CreateFrame"Frame"
+  local addon = CreateFrame("Frame")
   local _G = getfenv(0)
   
   addon:SetScript("OnEvent", function()
     
-    if(event=="PLAYER_LOGIN") then
+    if(event=="PLAYER_LOGIN") 
+    then
       
       TemporaryEnchantFrame:ClearAllPoints()
       TemporaryEnchantFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -190, -20)
       TemporaryEnchantFrame.SetPoint = function() end
-      
-      BuffFrame:SetScale(0.95)
-      
-      TicketStatusFrame:Hide()
-      TicketStatusFrame.Show = function() end
-      
-    elseif(event=="PLAYER_AURAS_CHANGED") then
-      local i
-      i = 1
-      while _G["BuffButton"..i] do addon:Update("BuffButton"..i, 1) i = i + 1 end
-      i = 1
-      while _G["DebuffButton"..i] do addon:Update("DebuffButton"..i, 2) i = i + 1 end
-      i = 1
-      while _G["TempEnchant"..i] do addon:Update("TempEnchant"..i, 3) i = i + 1 end
+    
+    elseif(event=="UNIT_AURA") 
+    then
+      if ( unit == PlayerFrame.unit ) 
+      then
+        
+        local i = 1
+        while _G["BuffButton"..i] 
+        do 
+          addon:checkgloss("BuffButton"..i) 
+          i = i + 1 
+        end
+        
+        i = 1
+        while _G["DebuffButton"..i] 
+        do 
+          addon:checkgloss("DebuffButton"..i) 
+          i = i + 1 
+        end
+        
+        i = 1
+        while _G["TempEnchant"..i] 
+        do 
+          addon:checkgloss("TempEnchant"..i) 
+          i = i + 1 
+        end
+        
+      end
     end
     
   end)
   
-  function addon:Update(name, isDebuff)
+  function addon:checkgloss(name)
   
     local b = _G[name.."Border"]
     local i = _G[name.."Icon"]
@@ -81,35 +67,13 @@
       
       local t = f:CreateTexture(nil,"ARTWORK")
       t:SetTexture("Interface\\AddOns\\rTextures\\gloss")
-      t:SetPoint("TOPLEFT", fg, "TOPLEFT", -0, 0)
-      t:SetPoint("BOTTOMRIGHT", fg, "BOTTOMRIGHT", 0, -0)
+      t:SetPoint("TOPLEFT", fg, "TOPLEFT", -2, 2)
+      t:SetPoint("BOTTOMRIGHT", fg, "BOTTOMRIGHT", 2, -2)
       fg.texture = t
       
-      --[[
-      if isDebuff == 2 then
-        local t = f:CreateTexture(nil,"ARTWORK")
-        t:SetTexture("Interface\\AddOns\\rTextures\\gloss_red")
-        t:SetPoint("TOPLEFT", fg, "TOPLEFT", -0, 0)
-        t:SetPoint("BOTTOMRIGHT", fg, "BOTTOMRIGHT", 0, -0)
-        fg.texture = t
-      elseif isDebuff == 3 then
-        local t = f:CreateTexture(nil,"ARTWORK")
-        t:SetTexture("Interface\\AddOns\\rTextures\\gloss_purple")
-        t:SetPoint("TOPLEFT", fg, "TOPLEFT", -0, 0)
-        t:SetPoint("BOTTOMRIGHT", fg, "BOTTOMRIGHT", 0, -0)
-        fg.texture = t
-      else
-        local t = f:CreateTexture(nil,"ARTWORK")
-        t:SetTexture("Interface\\AddOns\\rTextures\\gloss_green")
-        t:SetPoint("TOPLEFT", fg, "TOPLEFT", -0, 0)
-        t:SetPoint("BOTTOMRIGHT", fg, "BOTTOMRIGHT", 0, -0)
-        fg.texture = t
-      end
-      ]]--
-      
       i:SetTexCoord(0.1,0.9,0.1,0.9)
-      i:SetPoint("TOPLEFT", fg, "TOPLEFT", 2, -2)
-      i:SetPoint("BOTTOMRIGHT", fg, "BOTTOMRIGHT", -2, 2)
+      i:SetPoint("TOPLEFT", fg, "TOPLEFT", 0, 0)
+      i:SetPoint("BOTTOMRIGHT", fg, "BOTTOMRIGHT", 0, 0)
         
     end
 
@@ -120,21 +84,20 @@
   SecondsToTimeAbbrev = function(time)
     local hr, m, s, text
     if time <= 0 then text = ""
-    elseif(time < 3600 and time > 40) then
-      m = floor(time / 60)
+    elseif(time < 3600 and time > 60) then
+      --hr = floor(time / 3600)
+      m = floor(mod(time, 3600) / 60)+1
+      text = format("%dm", m)
+    elseif time < 60 then
+      --m = floor(time / 60)
       s = mod(time, 60)
-      text = (m == 0 and format("|cffffffff%d|r", s)) or format("|cffffffff%d:%02d|r", m, s)
-    elseif time < 40 then
-      m = floor(time / 60)
-      s = mod(time, 60)
-      text = (m == 0 and format("|cffffffff%d|r", s))
+      text = (m == 0 and format("%ds", s))
     else
-      hr = floor(time / 3600)
-      m = floor(mod(time, 3600) / 60)
-      text = format("%d:%2d", hr, m)
+      hr = floor(time / 3600)+1
+      text = format("%dh", hr)
     end
     return text
   end
   
-  addon:RegisterEvent"PLAYER_AURAS_CHANGED"
-  addon:RegisterEvent"PLAYER_LOGIN"
+  addon:RegisterEvent("UNIT_AURA");
+  addon:RegisterEvent("PLAYER_LOGIN");

10b125e44c21830f503db676dd0aad433981eed3
Erik Raetz <zorker@users.noreply.github.com>
2008-10-15 11:04:33 +0000

    run through icons

--- rBuff/rBuff.lua
+++ rBuff/rBuff.lua
@@ -8,63 +8,57 @@
   local addon = CreateFrame("Frame")
   local _G = getfenv(0)
   
-  addon:SetScript("OnEvent", function()
-    
+  addon:SetScript("OnEvent", function(self, event, ...)
+    local unit = ...;
     if(event=="PLAYER_LOGIN") 
     then
-      
       TemporaryEnchantFrame:ClearAllPoints()
       TemporaryEnchantFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -190, -20)
       TemporaryEnchantFrame.SetPoint = function() end
-    
-    elseif(event=="UNIT_AURA") 
-    then
-      if ( unit == PlayerFrame.unit ) 
-      then
-        
-        local i = 1
-        while _G["BuffButton"..i] 
-        do 
-          addon:checkgloss("BuffButton"..i) 
-          i = i + 1 
-        end
-        
-        i = 1
-        while _G["DebuffButton"..i] 
-        do 
-          addon:checkgloss("DebuffButton"..i) 
-          i = i + 1 
-        end
-        
-        i = 1
-        while _G["TempEnchant"..i] 
-        do 
-          addon:checkgloss("TempEnchant"..i) 
-          i = i + 1 
-        end
-        
-      end
     end
-    
+  	if ( event == "UNIT_AURA" ) then
+  		if ( unit == PlayerFrame.unit ) then
+  			addon:runthroughicons()
+  		end
+  	end
   end)
   
+  function addon:runthroughicons()
+    local i = 1
+    while _G["BuffButton"..i] 
+    do 
+      addon:checkgloss("BuffButton"..i) 
+      i = i + 1 
+    end
+    i = 1
+    while _G["DebuffButton"..i] 
+    do 
+      addon:checkgloss("DebuffButton"..i) 
+      i = i + 1 
+    end
+    i = 1
+    while _G["TempEnchant"..i] 
+    do 
+      addon:checkgloss("TempEnchant"..i) 
+      i = i + 1 
+    end
+  end
+
   function addon:checkgloss(name)
-  
     local b = _G[name.."Border"]
     local i = _G[name.."Icon"]
     local f = _G[name]
     local c = _G[name.."Gloss"]
     local ff = _G[name.."Duration"]
-
     ff:SetFont(NAMEPLATE_FONT, 14, "THINOUTLINE")
     ff:ClearAllPoints()
     ff:SetPoint("TOP",f,"BOTTOM",0,0)
-    
+
     if not c then
-   
+
       local fg = CreateFrame("Frame", name.."Gloss", f)
       fg:SetAllPoints(f)
-      
+
       local t = f:CreateTexture(nil,"ARTWORK")
       t:SetTexture("Interface\\AddOns\\rTextures\\gloss")
       t:SetPoint("TOPLEFT", fg, "TOPLEFT", -2, 2)
@@ -80,7 +74,7 @@
     if b then b:Hide() end
   
   end
-  
+
   SecondsToTimeAbbrev = function(time)
     local hr, m, s, text
     if time <= 0 then text = ""
@@ -98,6 +92,7 @@
     end
     return text
   end
+
   
   addon:RegisterEvent("UNIT_AURA");
   addon:RegisterEvent("PLAYER_LOGIN");

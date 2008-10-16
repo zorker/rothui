240c25a0079582fdad5052d12f6ab1bf34d66153
Erik Raetz <zorker@users.noreply.github.com>
2008-10-16 15:16:32 +0000

    cleaner code and some comments

--- rBars2/rBars2.lua
+++ rBars2/rBars2.lua
@@ -3,17 +3,13 @@
   local _G = getfenv(0)
   local dummy = function() end
   
+  RANGE_INDICATOR = "";
+  
   UIPARENT_MANAGED_FRAME_POSITIONS["MultiBarRight"] = nil
   UIPARENT_MANAGED_FRAME_POSITIONS["MultiBarLeft"] = nil
   UIPARENT_MANAGED_FRAME_POSITIONS["MultiBarBottomLeft"] = nil
   UIPARENT_MANAGED_FRAME_POSITIONS["MultiBarBottomRight"] = nil
   UIPARENT_MANAGED_FRAME_POSITIONS["MainMenuBar"] = nil
-  
-  --------------------------------------
-  -- HIDE STUFF
-  ---------------------------------------
-
-
 
   a:RegisterEvent("PLAYER_LOGIN")
   
@@ -24,6 +20,8 @@
   end)
 
   function a:cre_actionbarframe1()
+    
+    --creating a helper frame to hold the actionbuttons
     local f = CreateFrame("Frame",nil,UIParent)
     f:SetWidth(498)
     f:SetHeight(100)
@@ -31,10 +29,10 @@
     f:SetPoint("BOTTOM",0,20)
     f:Show()
     
+    -- hack to fix the bonusactionbar when hiding the mainmenubar
     BonusActionBarFrame:SetParent(f)
-
-    --cannot hide art frame it will hide the mainbar too..trying to repoint mainbar though...
-    --MainMenuBarArtFrame:Hide()
+    
+    -- since I am reanchoring all the buttons I can hide the MainMenuBar
     MainMenuBar:Hide()  
     
     local j
@@ -49,10 +47,9 @@
     end
     
     ActionButton1:ClearAllPoints()
-    ActionButton1:SetParent(f)
-    ActionButton1:SetPoint("BOTTOMLEFT",f,"BOTTOMLEFT",0,0);
-    
+    ActionButton1:SetPoint("BOTTOMLEFT",f,"BOTTOMLEFT",0,0);    
 
+    --need to do this since I reanchored the bonusactionbar to f
     BonusActionBarTexture0:Hide()
     BonusActionBarTexture1:Hide()
     
@@ -80,6 +77,8 @@
     ShapeshiftButton1:ClearAllPoints()
     ShapeshiftBarFrame:SetParent(f)
     ShapeshiftButton1:SetPoint("BOTTOMLEFT",MultiBarBottomRightButton1,"TOPLEFT",5,15);
+    
+    -- hide the shapeshift 
     ShapeshiftBarFrame:Hide()
     ShapeshiftBarFrame.Show = dummy
     
@@ -87,31 +86,28 @@
     PetActionBarFrame:SetParent(f)
     PetActionButton1:SetPoint("BOTTOMLEFT",MultiBarBottomRightButton1,"TOPLEFT",5,15);
   
+    -- hmmm I don't know if this does anything since the PossesBar becomes the BonusActionBar upon possessing sth
     PossessButton1:ClearAllPoints()
     PossessBarFrame:SetParent(f)
     PossessButton1:SetPoint("BOTTOMLEFT",MultiBarBottomRightButton1,"TOPLEFT",5,15);
   
-    -------------
-    -- SCALE
-    -------------
-    
-  local myscale = 0.8
-  MainMenuBar:SetScale(myscale)
-  f:SetScale(myscale)
-  BonusActionBarFrame:SetScale(1)
-  MultiBarBottomLeft:SetScale(myscale)
-  MultiBarBottomRight:SetScale(myscale)
-  MultiBarRight:SetScale(myscale)
-  MultiBarLeft:SetScale(myscale)
-  PetActionBarFrame:SetScale(0.85)
-    
+    -- scaling
+    local myscale = 0.8
+    MainMenuBar:SetScale(myscale)
+    f:SetScale(myscale)
+    BonusActionBarFrame:SetScale(1)
+    MultiBarBottomLeft:SetScale(myscale)
+    MultiBarBottomRight:SetScale(myscale)
+    MultiBarRight:SetScale(myscale)
+    MultiBarLeft:SetScale(myscale)
+    PetActionBarFrame:SetScale(0.85)
     
   end
   
-  -------------------
-  -- cpt HOOK --
-  -----------------
+  -- some functions  
   
+  -- hides the actionbuttons when a bonusactionbar is active (normaly the blizzard background texture would overlap it
+  -- but I use no background texture
   local bonushooks = {};
   local i;
   
@@ -130,123 +126,125 @@
     for i = 1, 12, 1 do
       _G["ActionButton"..i]:SetAlpha(1);
     end;
-  end);
+  end);  
   
+  -- rewrite the Blizzard ActionButton_Update function. I kept most of it the same but I changed the normaltexture to my gloss texture
   ActionButton_Update = function(self)
-  	-- Special case code for bonus bar buttons
-  	-- Prevents the button from updating if the bonusbar is still in an animation transition
-  	if ( self.isBonus and self.inTransition ) then
-  		self.needsUpdate = true;
-  		ActionButton_UpdateUsable(self);
-  		return;
-  	end
-  	
-  	local name = self:GetName();
+    -- Special case code for bonus bar buttons
+    -- Prevents the button from updating if the bonusbar is still in an animation transition
+    if ( self.isBonus and self.inTransition ) then
+      self.needsUpdate = true;
+      ActionButton_UpdateUsable(self);
+      return;
+    end
+    
+    local name = self:GetName();
   
-  	local action = self.action;
-  	local icon = getglobal(name.."Icon");
-  	local buttonCooldown = getglobal(name.."Cooldown");
-  	local texture = GetActionTexture(action);	
-  	
-  	if ( HasAction(action) ) then
-  		if ( not self.eventsRegistered ) then
-  			self:RegisterEvent("ACTIONBAR_UPDATE_STATE");
-  			self:RegisterEvent("ACTIONBAR_UPDATE_USABLE");
-  			self:RegisterEvent("ACTIONBAR_UPDATE_COOLDOWN");
-  			self:RegisterEvent("UPDATE_INVENTORY_ALERTS");
-  			self:RegisterEvent("PLAYER_TARGET_CHANGED");
-  			self:RegisterEvent("TRADE_SKILL_SHOW");
-  			self:RegisterEvent("TRADE_SKILL_CLOSE");
-  			self:RegisterEvent("PLAYER_ENTER_COMBAT");
-  			self:RegisterEvent("PLAYER_LEAVE_COMBAT");
-  			self:RegisterEvent("START_AUTOREPEAT_SPELL");
-  			self:RegisterEvent("STOP_AUTOREPEAT_SPELL");
-  			self.eventsRegistered = true;
-  		end
+    local action = self.action;
+    local icon = getglobal(name.."Icon");
+    local buttonCooldown = getglobal(name.."Cooldown");
+    local texture = GetActionTexture(action);  
+    
+    if ( HasAction(action) ) then
+      if ( not self.eventsRegistered ) then
+        self:RegisterEvent("ACTIONBAR_UPDATE_STATE");
+        self:RegisterEvent("ACTIONBAR_UPDATE_USABLE");
+        self:RegisterEvent("ACTIONBAR_UPDATE_COOLDOWN");
+        self:RegisterEvent("UPDATE_INVENTORY_ALERTS");
+        self:RegisterEvent("PLAYER_TARGET_CHANGED");
+        self:RegisterEvent("TRADE_SKILL_SHOW");
+        self:RegisterEvent("TRADE_SKILL_CLOSE");
+        self:RegisterEvent("PLAYER_ENTER_COMBAT");
+        self:RegisterEvent("PLAYER_LEAVE_COMBAT");
+        self:RegisterEvent("START_AUTOREPEAT_SPELL");
+        self:RegisterEvent("STOP_AUTOREPEAT_SPELL");
+        self.eventsRegistered = true;
+      end
   
-  		if ( not self:GetAttribute("statehidden") ) then
-  			self:Show();
-  		end
-  		ActionButton_UpdateState(self);
-  		ActionButton_UpdateUsable(self);
-  		ActionButton_UpdateCooldown(self);
-  		ActionButton_UpdateFlash(self);
-  	else
-  		if ( self.eventsRegistered ) then
-  			self:UnregisterEvent("ACTIONBAR_UPDATE_STATE");
-  			self:UnregisterEvent("ACTIONBAR_UPDATE_USABLE");
-  			self:UnregisterEvent("ACTIONBAR_UPDATE_COOLDOWN");
-  			self:UnregisterEvent("UPDATE_INVENTORY_ALERTS");
-  			self:UnregisterEvent("PLAYER_TARGET_CHANGED");
-  			self:UnregisterEvent("TRADE_SKILL_SHOW");
-  			self:UnregisterEvent("TRADE_SKILL_CLOSE");
-  			self:UnregisterEvent("PLAYER_ENTER_COMBAT");
-  			self:UnregisterEvent("PLAYER_LEAVE_COMBAT");
-  			self:UnregisterEvent("START_AUTOREPEAT_SPELL");
-  			self:UnregisterEvent("STOP_AUTOREPEAT_SPELL");
-  			self.eventsRegistered = nil;
-  		end
+      if ( not self:GetAttribute("statehidden") ) then
+        self:Show();
+      end
+      ActionButton_UpdateState(self);
+      ActionButton_UpdateUsable(self);
+      ActionButton_UpdateCooldown(self);
+      ActionButton_UpdateFlash(self);
+    else
+      if ( self.eventsRegistered ) then
+        self:UnregisterEvent("ACTIONBAR_UPDATE_STATE");
+        self:UnregisterEvent("ACTIONBAR_UPDATE_USABLE");
+        self:UnregisterEvent("ACTIONBAR_UPDATE_COOLDOWN");
+        self:UnregisterEvent("UPDATE_INVENTORY_ALERTS");
+        self:UnregisterEvent("PLAYER_TARGET_CHANGED");
+        self:UnregisterEvent("TRADE_SKILL_SHOW");
+        self:UnregisterEvent("TRADE_SKILL_CLOSE");
+        self:UnregisterEvent("PLAYER_ENTER_COMBAT");
+        self:UnregisterEvent("PLAYER_LEAVE_COMBAT");
+        self:UnregisterEvent("START_AUTOREPEAT_SPELL");
+        self:UnregisterEvent("STOP_AUTOREPEAT_SPELL");
+        self.eventsRegistered = nil;
+      end
   
-  		if ( self:GetAttribute("showgrid") == 0 ) then
-  			self:Hide();
-  		else
-  			buttonCooldown:Hide();
-  		end
-  	end
+      if ( self:GetAttribute("showgrid") == 0 ) then
+        self:Hide();
+      else
+        buttonCooldown:Hide();
+      end
+    end
   
-  	-- Add a green border if button is an equipped item
-  	local border = getglobal(name.."Border");
-  	if ( IsEquippedAction(action) ) then
-  		--border:SetVertexColor(0, 1.0, 0, 0.35);
-  		--border:Show();
-  		--self:SetNormalTexture("Interface\\AddOns\\rTextures\\gloss_red");
-  	else
-  		--border:Hide();
-  		--self:SetNormalTexture("Interface\\AddOns\\rTextures\\gloss");
-  	end
+    -- Add a green border if button is an equipped item
+    local border = getglobal(name.."Border");
+    
+    -- This does not change the normaltexture, I have no clue why, so I commented it out
+    if ( IsEquippedAction(action) ) then
+      --border:SetVertexColor(0, 1.0, 0, 0.35);
+      --border:Show();
+      --self:SetNormalTexture("Interface\\AddOns\\rTextures\\gloss_red");
+    else
+      --border:Hide();
+      --self:SetNormalTexture("Interface\\AddOns\\rTextures\\gloss");
+    end
   
-  	-- Update Macro Text
-  	local macroName = getglobal(name.."Name");
-  	if ( not IsConsumableAction(action) and not IsStackableAction(action) ) then
-  		macroName:SetText(GetActionText(action));
-  	else
-  		macroName:SetText("");
-  	end
+    -- Update Macro Text
+    local macroName = getglobal(name.."Name");
+    if ( not IsConsumableAction(action) and not IsStackableAction(action) ) then
+      macroName:SetText(GetActionText(action));
+    else
+      macroName:SetText("");
+    end
   
-  	-- Update icon and hotkey text
-  	if ( texture ) then
-  		icon:SetTexture(texture);
-  		icon:Show();
-  		self.rangeTimer = -1;
-  		self:SetNormalTexture("Interface\\AddOns\\rTextures\\gloss");
-  	else
-  		icon:Hide();
-  		buttonCooldown:Hide();
-  		self.rangeTimer = nil;
-  		self:SetNormalTexture("Interface\\AddOns\\rTextures\\gloss");
-  		local hotkey = getglobal(name.."HotKey");
+    -- Update icon and hotkey text
+    if ( texture ) then
+      icon:SetTexture(texture);
+      icon:Show();
+      self.rangeTimer = -1;
+      self:SetNormalTexture("Interface\\AddOns\\rTextures\\gloss");
+    else
+      icon:Hide();
+      buttonCooldown:Hide();
+      self.rangeTimer = nil;
+      self:SetNormalTexture("Interface\\AddOns\\rTextures\\gloss");
+      local hotkey = getglobal(name.."HotKey");
           if ( hotkey:GetText() == RANGE_INDICATOR ) then
-  			hotkey:Hide();
-  		else
-  			hotkey:SetVertexColor(0.6, 0.6, 0.6);
-  		end
-  	end
-  	ActionButton_UpdateCount(self);	
-  	
-  	-- Update tooltip
-  	if ( GameTooltip:GetOwner() == self ) then
-  		ActionButton_SetTooltip(self);
-  	end
+        hotkey:Hide();
+      else
+        hotkey:SetVertexColor(0.6, 0.6, 0.6);
+      end
+    end
+    ActionButton_UpdateCount(self);  
+    
+    -- Update tooltip
+    if ( GameTooltip:GetOwner() == self ) then
+      ActionButton_SetTooltip(self);
+    end
   
-  	self.feedback_action = action;
+    self.feedback_action = action;
   end
 
 
-  -- update usable
-
+  -- update usable no make funky colors
   ActionButton_UpdateUsable = function()
-  	local icon = getglobal(this:GetName().."Icon");
-  	local normalTexture = getglobal(this:GetName().."NormalTexture");
+    local icon = getglobal(this:GetName().."Icon");
+    local normalTexture = getglobal(this:GetName().."NormalTexture");
     if ( IsActionInRange(ActionButton_GetPagedID(this)) == 0 ) 
     then
       icon:SetVertexColor(0.7,0.1,0.1);
@@ -266,31 +264,21 @@
     end
   end
   
-  --
-  
+  -- hack to fix the grid alpha. by default the NormalTexture gets 0.5 alpha when you move a button and it keeps this alpha
+  -- this fixes this issue 
   ActionButton_ShowGrid = function (button)
-		button:SetAttribute("showgrid", button:GetAttribute("showgrid") + 1);
-  	getglobal(button:GetName().."NormalTexture"):SetVertexColor(1.0, 1.0, 1.0, 1);
- 		button:Show();
+    button:SetAttribute("showgrid", button:GetAttribute("showgrid") + 1);
+    getglobal(button:GetName().."NormalTexture"):SetVertexColor(1.0, 1.0, 1.0, 1);
+     button:Show();
   end
 
+  -- function to anchor the button to f
   function a:setnewparent(name, i, frame)
-  
     local bu  = _G[name..i]
-    local ic  = _G[name..i.."Icon"]
-    local co  = _G[name..i.."Count"]
-    local bo  = _G[name..i.."Border"]
-    local ho  = _G[name..i.."HotKey"]
-    local cd  = _G[name..i.."Cooldown"]
-    local na  = _G[name..i.."Name"]
-    local fl  = _G[name..i.."Flash"]
-    local nt  = _G[name..i.."NormalTexture"]
-    
     bu:SetParent(frame);
-        
   end  
 
-
+  -- gives the buttons nice new textures....bling bling
   function a:dostuff(name, i, frame)
   
     local bu  = _G[name..i]
@@ -302,8 +290,6 @@
     local na  = _G[name..i.."Name"]
     local fl  = _G[name..i.."Flash"]
     local nt  = _G[name..i.."NormalTexture"]
-    
-    --bu:SetParent(frame);
     
     ho:Hide()
     na:Hide()

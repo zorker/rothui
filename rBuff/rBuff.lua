459dee21e95033ba449c5b8e59169fe08186390e
Erik Raetz <zorker@users.noreply.github.com>
2008-10-17 18:45:25 +0000

    new pos for new rMinimap

--- rBuff/rBuff.lua
+++ rBuff/rBuff.lua
@@ -13,7 +13,7 @@
     if(event=="PLAYER_ENTERING_WORLD") 
     then
       TemporaryEnchantFrame:ClearAllPoints()
-      TemporaryEnchantFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -190, -20)
+      TemporaryEnchantFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -180, -19)
       TemporaryEnchantFrame.SetPoint = function() end
       addon:runthroughicons()
       

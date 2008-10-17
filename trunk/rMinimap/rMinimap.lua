d4ae666dd6b86255afc225a6e3937a357ca47803
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2008-10-17 20:28:21 +0000

    rotating ring is back

--- trunk/rMinimap/rMinimap.lua
+++ trunk/rMinimap/rMinimap.lua
@@ -8,6 +8,7 @@
   a:SetScript("OnEvent", function (self,event,arg1)
     if(event=="PLAYER_LOGIN") then
       a:dostuff()
+      a:rotateme()
     end
   end)
   
@@ -69,11 +70,74 @@
     MiniMapMeetingStoneFrame:SetAlpha(0)
     MinimapNorthTag:SetAlpha(0)
   
-    Minimap:SetMaskTexture("Interface\\AddOns\\rMinimap\\mask")
-  
+    --Minimap:SetMaskTexture("Interface\\AddOns\\rMinimap\\mask")
+    --[[
     local t = Minimap:CreateTexture(nil,"Overlay")
     t:SetTexture("Interface\\AddOns\\rTextures\\minigloss")
     t:SetPoint("TOPLEFT", Minimap, "TOPLEFT", -1, 1)
     t:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT", 1, -1)
+    ]]--
   
+  end
+  
+  function a:rotateme()
+    
+    --DEFAULT_CHAT_FRAME:AddMessage("ping")
+    
+    local r2, r42, realUpdate, colorTable = math.sqrt(0.5^2+0.5^2), math.sqrt(42), true, {[4]=0.9};
+    
+    local f = CreateFrame("Frame",nil,Minimap)
+    f:SetWidth(194)
+    f:SetHeight(194)
+    f:SetPoint("CENTER",0,0)
+    f:Show()
+    
+    local t = f:CreateTexture(nil,"BACKGROUND")
+    t:SetTexture("Interface\\AddOns\\rTextures\\map_texture.tga")
+    --t:SetTexCoord(0.1,0.9,0.1,0.9)
+    t:SetAllPoints(f)
+    t:SetVertexColor(0.8,0.8,0.8,1)
+    
+    local f2 = CreateFrame("Frame",nil,f)
+    f2:SetPoint("TOPLEFT",f,"TOPLEFT",20,-20)
+    f2:SetPoint("BOTTOMRIGHT",f,"BOTTOMRIGHT",-20,20)
+    --f2:SetFrameStrata("DIALOG")
+    f2:Show()
+    
+  	t2 = f2:CreateTexture(nil, "OVERLAY")
+    --t2:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_gloss.tga")
+    t2:SetAllPoints(f2)  
+    t2:SetAlpha(0.5)
+    
+    local totalElapsed = 0
+    local degrees = 0
+    
+    local function OnUpdateFunc(self, elapsed)
+      totalElapsed = totalElapsed + elapsed
+      local update_timer = 1
+      if (totalElapsed < update_timer) then 
+        return 
+      else
+        totalElapsed = totalElapsed - floor(totalElapsed)
+        
+        t:SetTexCoord(
+        0.5+r2*cos(degrees+135), 0.5+r2*sin(degrees+135),
+        0.5+r2*cos(degrees-135), 0.5+r2*sin(degrees-135),
+        0.5+r2*cos(degrees+45), 0.5+r2*sin(degrees+45),
+        0.5+r2*cos(degrees-45), 0.5+r2*sin(degrees-45)
+        )
+	      
+	      degrees = degrees+1
+	      
+	      if degrees > 360 then
+	        degrees = 0
+	      end
+
+
+      end
+    end
+    
+    f:SetScript("OnUpdate", OnUpdateFunc)
+    
+    
   end

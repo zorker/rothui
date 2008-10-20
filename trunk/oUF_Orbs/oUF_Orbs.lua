69c7ae6a0894cac6beb4adf5098552dca5f0b2f5
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2008-10-20 19:25:49 +0000

    Deleted d3orb style (created own oUF_D3Orbs addon for it)

--- trunk/oUF_Orbs/oUF_Orbs.lua
+++ trunk/oUF_Orbs/oUF_Orbs.lua
@@ -1,9 +1,4 @@
-  
-  -- possible styles
-  -- d3orb, futureorb1
-  
-  local actstyle = "futureorb1"
-  
+   
   -- Colors
   local colors2 = {
     power = {
@@ -72,305 +67,84 @@
   local function updateHealth(self, event, unit, bar, min, max)
     local lifeact = UnitHealth(unit)
     local lifemax = UnitHealthMax(unit)
-    if actstyle == "d3orb" then
-      self.Health.Filling:SetHeight((lifeact / lifemax) * self.Health:GetWidth())
-      self.Health.Filling:SetTexCoord(0,1,  math.abs(lifeact / lifemax - 1),1)
-      self.pm1:SetAlpha((lifeact / lifemax)/2)
-      self.pm2:SetAlpha((lifeact / lifemax)/2)
+      
+    --DEFAULT_CHAT_FRAME:AddMessage(unit)
     
-    elseif actstyle == "futureorb1" then
-      
-      --DEFAULT_CHAT_FRAME:AddMessage(unit)
-      
-      local c = max - min
-      local d = floor(min/max*100)
-      
-      if d == 0 or d == 100 then
-        bar.value:Hide()
-      else
-        bar.value:Show()
-        bar.value:SetText(d)
-      end
-      
-      if d <= 25 and d > 0 then
-        self.LowHP:Show()
-      else
-        self.LowHP:Hide()
-      end
-      
-      self.Health.Filling:SetHeight((lifeact / lifemax) * self.Health:GetWidth())
-      self.Health.Filling:SetTexCoord(0,1,  math.abs(lifeact / lifemax - 1),1)
-      
-      --DEFAULT_CHAT_FRAME:AddMessage("unit "..unit.." aggro "..self.HasAggro)
-
-			if UnitIsPlayer(unit) then
-        if RAID_CLASS_COLORS[select(2, UnitClass(unit))] then
-          local color = RAID_CLASS_COLORS[select(2, UnitClass(unit))]
-          self.Health.Filling:SetVertexColor(color.r, color.g, color.b,1)
-        end
-      elseif unit == "pet" and UnitExists("pet") and GetPetHappiness() then
-        local happiness, _, _ = GetPetHappiness()
-        --DEFAULT_CHAT_FRAME:AddMessage("Pet is " .. happiness)
-        local color = colors2.happiness[happiness]
-        --DEFAULT_CHAT_FRAME:AddMessage("color "..color.r.." "..color.g.." "..color.b)
-        self.Health.Filling:SetVertexColor(color.r, color.g, color.b,1)
-      else
-        local color = FACTION_BAR_COLORS[UnitReaction(unit, "player")]
-        self.Health.Filling:SetVertexColor(color.r, color.g, color.b,1)
-      end
-      
-      
-      
+    local c = max - min
+    local d = floor(min/max*100)
+    
+    if d == 0 or d == 100 then
+      bar.value:Hide()
+    else
+      bar.value:Show()
+      bar.value:SetText(d)
     end
+    
+    if d <= 25 and d > 0 then
+      self.LowHP:Show()
+    else
+      self.LowHP:Hide()
+    end
+    
+    self.Health.Filling:SetHeight((lifeact / lifemax) * self.Health:GetWidth())
+    self.Health.Filling:SetTexCoord(0,1,  math.abs(lifeact / lifemax - 1),1)
+    
+    --DEFAULT_CHAT_FRAME:AddMessage("unit "..unit.." aggro "..self.HasAggro)
+
+		if UnitIsPlayer(unit) then
+      if RAID_CLASS_COLORS[select(2, UnitClass(unit))] then
+        local color = RAID_CLASS_COLORS[select(2, UnitClass(unit))]
+        self.Health.Filling:SetVertexColor(color.r, color.g, color.b,1)
+      end
+    elseif unit == "pet" and UnitExists("pet") and GetPetHappiness() then
+      local happiness, _, _ = GetPetHappiness()
+      --DEFAULT_CHAT_FRAME:AddMessage("Pet is " .. happiness)
+      local color = colors2.happiness[happiness]
+      --DEFAULT_CHAT_FRAME:AddMessage("color "..color.r.." "..color.g.." "..color.b)
+      self.Health.Filling:SetVertexColor(color.r, color.g, color.b,1)
+    else
+      local color = FACTION_BAR_COLORS[UnitReaction(unit, "player")]
+      if color then
+        self.Health.Filling:SetVertexColor(color.r, color.g, color.b,1)
+      else
+        self.Health.Filling:SetVertexColor(0,1,0,1)
+      end
+    end
+
   end
   
   local function updatePower(self, event, unit, bar, min, max)
-    if actstyle == "d3orb" then
-      local manaact = UnitMana(unit)
-      local manamax = UnitManaMax(unit)
-      if (manamax == 0) then
-        self.Power.Filling:SetHeight(0)
-        self.Power.Filling:SetTexCoord(0,1,1,1)
-        self.pm3:SetAlpha(0)
-        self.pm4:SetAlpha(0)
-      else
-        self.Power.Filling:SetHeight((manaact / manamax) * self.Power:GetWidth())
-        self.Power.Filling:SetTexCoord(0,1,  math.abs(manaact / manamax - 1),1)
-        self.pm3:SetAlpha((manaact / manamax)/2)
-        self.pm4:SetAlpha((manaact / manamax)/2)
-      end
+     
+    local c, d
     
-    elseif actstyle == "futureorb1" then
-      
-      local c, d
-      
-      if max == 0 then
-        d = 0
-      else
-        c = max - min
-        d = floor(min/max*100)
-      end
-
-      if d == 0 or d == 100 then
-        bar.value:SetText("")
-      else
-        bar.value:SetText(d)
-      end
-
-      local color = colors2.power[UnitPowerType(unit)]
-      bar:SetStatusBarColor(color.r, color.g, color.b)
-      
-      bar.value:SetTextColor(.5, .5, .5)
-      
-      if unit == "player" or unit == "target" then
-        --nothing
-      else
-        bar.value:SetText("")
-      end
-      
+    if max == 0 then
+      d = 0
+    else
+      c = max - min
+      d = floor(min/max*100)
     end
-  end
-  
- 
-  local function styleFunc1(self, unit)
-    local _, class = UnitClass("player")
-    self.menu = menu
-    self:RegisterForClicks("AnyUp")
-    self:SetAttribute("*type2", "menu")
-    self:SetScript("OnEnter", UnitFrame_OnEnter)
-    self:SetScript("OnLeave", UnitFrame_OnLeave)
+
+    if d == 0 or d == 100 then
+      bar.value:SetText("")
+    else
+      bar.value:SetText(d)
+    end
+
+    local color = colors2.power[UnitPowerType(unit)]
+    bar:SetStatusBarColor(color.r, color.g, color.b)
     
-    self:SetFrameStrata("BACKGROUND")
+    bar.value:SetTextColor(.5, .5, .5)
     
     if unit == "player" or unit == "target" then
-      orbsize = 130
+      --nothing
     else
-      orbsize = 70
+      bar.value:SetText("")
     end
-    
-    self:SetHeight(orbsize)
-    self:SetWidth(orbsize)
-  
-    if unit == "player" then
-      self.Health = CreateFrame("StatusBar", nil, self)
-      self.Health:SetStatusBarTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_transparent.tga")
-      self.Health:SetHeight(orbsize)
-      self.Health:SetWidth(orbsize)
-      self.Health:SetPoint("TOPLEFT", 0, 0)
-    else
-      self.Health = CreateFrame("StatusBar", nil, self)
-      --want non-transparent background? comment this in.
-      --self.Health:SetBackdrop({bgFile = "Interface\\AddOns\\oUF_Orbs\\textures\\orb_back_flat.tga", insets = {top = 0, left = 0, bottom = 0, right = 0}})
-      self.Health:SetStatusBarTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_transparent.tga")
-      self.Health:SetHeight(orbsize)
-      self.Health:SetWidth(orbsize)
-      self.Health:SetPoint("TOPLEFT",0,0)
-    end
-    
-    self.Health.bg = self.Health:CreateTexture(nil, "BACKGROUND")
-    self.Health.bg:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_back.tga")
-    self.Health.bg:SetAllPoints(self.Health)
-    
-    self.Health.Filling = self.Health:CreateTexture(nil, "ARTWORK")
-    self.Health.Filling:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_filling4.tga")
-    self.Health.Filling:SetPoint("BOTTOMLEFT",0,0)
-    self.Health.Filling:SetWidth(orbsize)
-    self.Health.Filling:SetHeight(orbsize)
-    self.Health.Filling:SetVertexColor(0.4,0,0,1)
-    
-    self.Health.Gloss = self.Health:CreateTexture(nil, "OVERLAY")
-    self.Health.Gloss:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_gloss.tga")
-    self.Health.Gloss:SetAllPoints(self.Health)
-  
-    self.pm1 = CreateFrame("PlayerModel", nil,self.Health)
-    self.pm1:SetFrameStrata("BACKGROUND")
-    self.pm1:SetAllPoints(self.Health)
-    self.pm1:SetModel("SPELLS\\RedRadiationFog.m2")
-    self.pm1:SetModelScale(-.75)
-    self.pm1:SetPosition(-12, 1.5, -1) 
-    self.pm1:SetRotation(0)    
-    --self.pm1:SetAlpha(0.8)
-    
-    self.pm1:SetScript("OnShow",function() 
-      self.pm1:ClearModel()
-      self.pm1:SetModel("SPELLS\\RedRadiationFog.m2")
-      self.pm1:SetModelScale(.75)
-      self.pm1:SetPosition(-12, 1.5, -1) 
-      self.pm1:SetRotation(0)    
-      --self.pm1:SetAlpha(0.8)
-    end)
-    
-    self.pm2 = CreateFrame("PlayerModel", nil,self.Health)
-    self.pm2:SetFrameStrata("BACKGROUND")
-    self.pm2:SetAllPoints(self.Health)
-    self.pm2:SetModel("SPELLS\\RedRadiationFog.m2")
-    self.pm2:SetModelScale(-.75)
-    self.pm2:SetPosition(-12, 1.5, 0.5) 
-    self.pm2:SetRotation(0)    
-    --self.pm2:SetAlpha(0.8)
-    
-    self.pm2:SetScript("OnShow",function() 
-      self.pm2:ClearModel()
-      self.pm2:SetModel("SPELLS\\RedRadiationFog.m2")
-      self.pm2:SetModelScale(.75)
-      self.pm2:SetPosition(-12, 1.5, 0.5) 
-      self.pm2:SetRotation(0)    
-      --self.pm2:SetAlpha(0.8)
-    end)  
-  
-  
-    
-    if unit == "player" then
-      self.Power = CreateFrame("StatusBar", nil, self)
-      --want non-transparent background? comment this in.
-      --self.Power:SetBackdrop({bgFile = "Interface\\AddOns\\oUF_Orbs\\textures\\orb_back_flat.tga", insets = {top = 0, left = 0, bottom = 0, right = 0}})
-      self.Power:SetStatusBarTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_transparent.tga")
-      self.Power:SetHeight(orbsize)
-      self.Power:SetWidth(orbsize)
-      self.Power:SetPoint("BOTTOM", UIParent, "BOTTOM", 224, -8)
-    else
-      self.Power = CreateFrame("StatusBar", nil, self)
-      --want non-transparent background? comment this in.
-      --self.Power:SetBackdrop({bgFile = "Interface\\AddOns\\oUF_Orbs\\textures\\orb_back_flat.tga", insets = {top = 0, left = 0, bottom = 0, right = 0}})
-      self.Power:SetStatusBarTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_transparent.tga")
-      self.Power:SetHeight(orbsize/1.5)
-      self.Power:SetWidth(orbsize/1.5)
-      self.Power:SetPoint("CENTER", self.Health, "CENTER", orbsize/2.5, -orbsize/3)
-      self.Power:SetFrameLevel(4)
-    end
-    
-    self.Power.bg = self.Power:CreateTexture(nil, "BACKGROUND")
-    self.Power.bg:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_back.tga")
-    self.Power.bg:SetAllPoints(self.Power)
 
-    if unit == "player" then
-      self.Power.Filling = self.Power:CreateTexture(nil, "ARTWORK")
-      self.Power.Filling:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_filling4.tga")
-      self.Power.Filling:SetPoint("BOTTOMLEFT",0,0)
-      self.Power.Filling:SetWidth(orbsize)
-      self.Power.Filling:SetHeight(orbsize)
-      self.Power.Filling:SetVertexColor(0,0,0.4,1)
-    else
-      self.Power.Filling = self.Power:CreateTexture(nil, "ARTWORK")
-      self.Power.Filling:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_filling4.tga")
-      self.Power.Filling:SetPoint("BOTTOMLEFT",0,0)
-      self.Power.Filling:SetWidth(orbsize/1.5)
-      self.Power.Filling:SetHeight(orbsize/1.5)
-      self.Power.Filling:SetVertexColor(0,0,0.4,1)
-    end
-    
-    self.Power.Gloss = self.Power:CreateTexture(nil, "OVERLAY")
-    self.Power.Gloss:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_gloss.tga")
-    self.Power.Gloss:SetAllPoints(self.Power)
-  
-    self.pm3 = CreateFrame("PlayerModel", nil,self.Power)
-    self.pm3:SetFrameStrata("BACKGROUND")
-    self.pm3:SetAllPoints(self.Power)
-    self.pm3:SetModel("SPELLS\\BlueRadiationFog.m2")
-    self.pm3:SetModelScale(-.75)
-    self.pm3:SetPosition(-12, 1.5, -1) 
-    self.pm3:SetRotation(0)    
-    --self.pm3:SetAlpha(0.8)
-    
-    self.pm3:SetScript("OnShow",function() 
-      self.pm3:ClearModel()
-      self.pm3:SetModel("SPELLS\\BlueRadiationFog.m2")
-      self.pm3:SetModelScale(.75)
-      self.pm3:SetPosition(-12, 1.5, -1) 
-      self.pm3:SetRotation(0)    
-      --self.pm3:SetAlpha(0.8)
-    end)
-    
-    self.pm4 = CreateFrame("PlayerModel", nil,self.Power)
-    self.pm4:SetFrameStrata("BACKGROUND")
-    self.pm4:SetAllPoints(self.Power)
-    self.pm4:SetModel("SPELLS\\BlueRadiationFog.m2")
-    self.pm4:SetModelScale(-.75)
-    self.pm4:SetPosition(-12, 1.5, 0.5) 
-    self.pm4:SetRotation(0)    
-    --self.pm4:SetAlpha(0.8)
-    
-    self.pm4:SetScript("OnShow",function() 
-      self.pm4:ClearModel()
-      self.pm4:SetModel("SPELLS\\BlueRadiationFog.m2")
-      self.pm4:SetModelScale(.75)
-      self.pm4:SetPosition(-12, 1.5, 0.5) 
-      self.pm4:SetRotation(0)    
-      --self.pm4:SetAlpha(0.8)
-    end)  
-  
-  
-    if(unit) then
-      self.Castbar = CreateFrame("StatusBar", nil, self.Health)
-      self.Castbar:SetWidth(orbsize*1.2)
-      self.Castbar:SetHeight(orbsize*1.2)
-      self.Castbar:SetBackdrop({bgFile = "Interface\\AddOns\\oUF_Orbs\\textures\\orb_back.tga", insets = {top = 0, left = 0, bottom = 0, right = 0}})
-      self.Castbar:SetStatusBarTexture("Interface\\AddOns\\oUF_Orbs\\textures\\orb_filling2.tga")
-      self.Castbar:SetStatusBarColor(1,0.8,0,1)
-      self.Castbar:SetPoint("CENTER",0,0)
-      
-      --CASTBAR ABOVE OR BELOW ORB, YOUR CHOICE HERE. 0 = below
-      self.Castbar:SetFrameLevel(0)
-      
-      self.Castbar:Hide()
-      Castbar = self.Castbar
-    end
-    
-    self.outsideRangeAlpha = 1
-    self.inRangeAlpha = 1
-    self.Range = false
-      
-    self.PostUpdateHealth = updateHealth
-    self.PostUpdatePower = updatePower
-    self.PostCreateAuraIcon = auraIcon
-    self.UNIT_NAME_UPDATE = updateName
-    self.UNIT_HAPPINESS = updateName
-    self.PLAYER_TARGET_CHANGED = updateTarget
-  
-    return self
   end
   
-  local function styleFunc2(self, unit)
+  
+  local function styleFunc1(self, unit)
     local _, class = UnitClass("player")
     self.menu = menu
     self:RegisterForClicks("AnyUp")
@@ -638,114 +412,52 @@
   
     return self
   end
+ 
+  actstyle = "futureorb"
+  --oUF:RegisterSubTypeMapping("UNIT_LEVEL")
+  oUF:RegisterStyle(actstyle, styleFunc1)
+  oUF:SetActiveStyle(actstyle)
+  oUF:Spawn("player"):SetPoint("BOTTOM", UIParent, -250, 200)
+  oUF:Spawn("target"):SetPoint("BOTTOM", UIParent, 250, 200)
+  oUF:Spawn("pet"):SetPoint("LEFT", oUF.units.player, "RIGHT",20, -20)
+  oUF:Spawn("focus"):SetPoint("RIGHT", oUF.units.player, "LEFT", -20, -20)
+  oUF:Spawn("targettarget"):SetPoint("RIGHT", oUF.units.target, "LEFT", -20, -20)
   
-  if actstyle == "d3orb" then
-    --oUF:RegisterSubTypeMapping("UNIT_LEVEL")
-    oUF:RegisterStyle(actstyle, styleFunc1)
-    oUF:SetActiveStyle(actstyle)
-    oUF:Spawn("player"):SetPoint("BOTTOM", UIParent, "BOTTOM", -224, -8)
-    oUF:Spawn("target"):SetPoint("CENTER", UIParent, 250, -180)
-    oUF:Spawn("pet"):SetPoint("CENTER", oUF.units.player, "CENTER",-120, 110)
-    oUF:Spawn("focus"):SetPoint("CENTER", oUF.units.player, "CENTER", 0, 120)
-    oUF:Spawn("targettarget"):SetPoint("CENTER", oUF.units.target, "CENTER", 0, 120)
-  end
-
-  if actstyle == "futureorb1" then
-    --oUF:RegisterSubTypeMapping("UNIT_LEVEL")
-    oUF:RegisterStyle(actstyle, styleFunc2)
-    oUF:SetActiveStyle(actstyle)
-    oUF:Spawn("player"):SetPoint("BOTTOM", UIParent, -250, 200)
-    oUF:Spawn("target"):SetPoint("BOTTOM", UIParent, 250, 200)
-    oUF:Spawn("pet"):SetPoint("LEFT", oUF.units.player, "RIGHT",20, -20)
-    oUF:Spawn("focus"):SetPoint("RIGHT", oUF.units.player, "LEFT", -20, -20)
-    oUF:Spawn("targettarget"):SetPoint("RIGHT", oUF.units.target, "LEFT", -20, -20)
-    
-    local party  = oUF:Spawn("header", "oUF_Party")
-    party:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 15, -40)
-    party:SetManyAttributes("showParty", true, "xOffset", 10, "point", "LEFT", "showPlayer", false)
-    
-    --[[
-    local last = {}
-    for i = 1, 2 do
-      local raid = oUF:Spawn("header", "oUF_Raid"..i)
-      raid:SetManyAttributes("groupFilter", tostring(i), "showRaid", true, "yOffSet", -60)
-      table.insert(last, raid)
-      if(i==1) then
-        raid:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 15, -40)
-      else
-        raid:SetPoint("TOPLEFT", last[i-1], "TOPRIGHT", 20, 0)
-      end
-      raid:Show()
+  local party  = oUF:Spawn("header", "oUF_Party")
+  party:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 15, -40)
+  party:SetManyAttributes("showParty", true, "xOffset", 10, "point", "LEFT", "showPlayer", false)
+  
+  --[[
+  local last = {}
+  for i = 1, 2 do
+    local raid = oUF:Spawn("header", "oUF_Raid"..i)
+    raid:SetManyAttributes("groupFilter", tostring(i), "showRaid", true, "yOffSet", -60)
+    table.insert(last, raid)
+    if(i==1) then
+      raid:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 15, -40)
+    else
+      raid:SetPoint("TOPLEFT", last[i-1], "TOPRIGHT", 20, 0)
     end
-    ]]--
-    
-    local partyToggle = CreateFrame("Frame")
-    partyToggle:RegisterEvent("PLAYER_LOGIN")
-    partyToggle:RegisterEvent("RAID_ROSTER_UPDATE")
-    partyToggle:RegisterEvent("PARTY_LEADER_CHANGED")
-    partyToggle:RegisterEvent("PARTY_MEMBER_CHANGED")
-    partyToggle:SetScript("OnEvent", function(self)
-      if(InCombatLockdown()) then
-        self:RegisterEvent("PLAYER_REGEN_ENABLED")
+    raid:Show()
+  end
+  ]]--
+  
+  local partyToggle = CreateFrame("Frame")
+  partyToggle:RegisterEvent("PLAYER_LOGIN")
+  partyToggle:RegisterEvent("RAID_ROSTER_UPDATE")
+  partyToggle:RegisterEvent("PARTY_LEADER_CHANGED")
+  partyToggle:RegisterEvent("PARTY_MEMBER_CHANGED")
+  partyToggle:SetScript("OnEvent", function(self)
+    if(InCombatLockdown()) then
+      self:RegisterEvent("PLAYER_REGEN_ENABLED")
+    else
+      self:UnregisterEvent("PLAYER_REGEN_ENABLED")
+      if(GetNumRaidMembers() > 0) then
+        --party:Hide()
+        party:Show()
       else
-        self:UnregisterEvent("PLAYER_REGEN_ENABLED")
-        if(GetNumRaidMembers() > 0) then
-          party:Hide()
-        else
-          party:Show()
-        end
+        party:Show()
       end
-    end)
+    end
+  end)
 
-    
-  end
-  
-  --create textures for d3orb
-  
-  if actstyle == "d3orb" then  
-
-    local d3f = CreateFrame("Frame",nil,UIParent)
-    d3f:SetFrameStrata("TOOLTIP")
-    d3f:SetWidth(155)
-    d3f:SetHeight(155)
-    d3f:SetPoint("BOTTOM",280,0)
-    d3f:Show()
-    local d3t = d3f:CreateTexture(nil,"BACKGROUND")
-    d3t:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\d3_angel")
-    d3t:SetAllPoints(d3f)
-
-    local d3f2 = CreateFrame("Frame",nil,UIParent)
-    d3f2:SetFrameStrata("TOOLTIP")
-    d3f2:SetWidth(155)
-    d3f2:SetHeight(155)
-    d3f2:SetPoint("BOTTOM",-287,0)
-    d3f2:Show()
-    local d3t2 = d3f2:CreateTexture(nil,"HIGHLIGHT ")
-    d3t2:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\d3_demon")
-    d3t2:SetAllPoints(d3f2)
-    
-    local d3f3 = CreateFrame("Frame",nil,UIParent)
-    d3f3:SetFrameStrata("TOOLTIP")
-    d3f3:SetWidth(450)
-    d3f3:SetHeight(112)
-    d3f3:SetPoint("BOTTOM",0,-3)
-    d3f3:Show()
-    local d3t3 = d3f3:CreateTexture(nil,"BACKGROUND")
-    d3t3:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\d3_bottom")
-    d3t3:SetAllPoints(d3f3)
-    
-    local d3f4 = CreateFrame("Frame",nil,UIParent)
-    d3f4:SetFrameStrata("LOW")
-    d3f4:SetWidth(512)
-    d3f4:SetHeight(256)
-    d3f4:SetPoint("BOTTOM",0,11)
-    d3f4:Show()
-        
-    local d3t4 = d3f4:CreateTexture(nil,"BACKGROUND")
-    d3t4:SetTexture("Interface\\AddOns\\oUF_Orbs\\textures\\d3_bar4")
-    d3t4:SetAllPoints(d3f4)
-    
-
-
-    
-  end

15e22e3b84955d0dcffd19a4c443f30583f4a029
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2009-05-29 15:44:16 +0000

    mod tested and done.

--- trunk/rEmote/rEmote.lua
+++ trunk/rEmote/rEmote.lua
@@ -10,12 +10,26 @@
   a:RegisterEvent("CHAT_MSG_MONSTER_YELL")
   
   --setscript
-  a:SetScript("OnEvent", function (self,event,arg1)    
+  a:SetScript("OnEvent", function (self,event,arg1,arg2)    
+    
+    local string
+    
+    if event == "CHAT_MSG_MONSTER_EMOTE" then
+      string = format(arg1, arg2)
+    else
+      string = arg1
+    end
     
     -- choose chattype
-    CombatText_AddMessage(arg1, COMBAT_TEXT_SCROLL_FUNCTION, .7, .7, 1, "crit", nil)
-    --RaidNotice_AddMessage(RaidBossEmoteFrame, arg1, ChatTypeInfo["RAID_WARNING"]) 
-    --RaidNotice_AddMessage(RaidWarningFrame, arg1, ChatTypeInfo["RAID_WARNING"])    
+    
+    --post to blizzard combat text
+    CombatText_AddMessage(string, COMBAT_TEXT_SCROLL_FUNCTION, 1, 0, 0, "crit", nil)
+    
+    --post to boss emote frame
+    --RaidNotice_AddMessage(RaidBossEmoteFrame, string, ChatTypeInfo["RAID_WARNING"]) 
+    
+    --post to raidwarningframe
+    --RaidNotice_AddMessage(RaidWarningFrame, string, ChatTypeInfo["RAID_WARNING"])    
     
     --play a sound
     PlaySoundFile("Interface\\AddOns\\rEmote\\Alarm.mp3")

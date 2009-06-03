8aa632fb04ceeed55ad2350a81e1cd14c24fb329
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2009-06-03 11:46:16 +0000

    single quotes + utf8 test

--- trunk/rEmote/rEmote.lua
+++ trunk/rEmote/rEmote.lua
@@ -1,28 +1,28 @@
-  --helper frame
-  local a = CreateFrame("Frame")
+﻿  --helper frame
+  local a = CreateFrame('Frame')
   
   --register events
-  a:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
-  a:RegisterEvent("CHAT_MSG_MONSTER_EMOTE")
-  --a:RegisterEvent("CHAT_MSG_MONSTER_SAY")
-  --a:RegisterEvent("CHAT_MSG_MONSTER_WHISPER")
-  --a:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+  a:RegisterEvent('CHAT_MSG_RAID_BOSS_EMOTE')
+  a:RegisterEvent('CHAT_MSG_MONSTER_EMOTE')
+  --a:RegisterEvent('CHAT_MSG_MONSTER_SAY')
+  --a:RegisterEvent('CHAT_MSG_MONSTER_WHISPER')
+  --a:RegisterEvent('CHAT_MSG_MONSTER_YELL')
   
   --setscript
-  a:SetScript("OnEvent", function (self,event,arg1,arg2)    
+  a:SetScript('OnEvent', function (self,event,arg1,arg2)    
     
     --format the string (in case %s is used inside arg1 it will be replaced with value of arg2)
     local string = format(arg1, arg2)
     
     -- choose chattype
     --post to blizzard combat text
-    CombatText_AddMessage(string, COMBAT_TEXT_SCROLL_FUNCTION, 1, 0, 0, "crit", nil)
+    CombatText_AddMessage(string, COMBAT_TEXT_SCROLL_FUNCTION, 1, 0, 0, 'crit', nil)
     --post to boss emote frame
-    --RaidNotice_AddMessage(RaidBossEmoteFrame, string, ChatTypeInfo["RAID_WARNING"]) 
+    --RaidNotice_AddMessage(RaidBossEmoteFrame, string, ChatTypeInfo['RAID_WARNING']) 
     --post to raidwarningframe
-    --RaidNotice_AddMessage(RaidWarningFrame, string, ChatTypeInfo["RAID_WARNING"])    
+    --RaidNotice_AddMessage(RaidWarningFrame, string, ChatTypeInfo['RAID_WARNING'])    
     
     --play a sound...DING
-    PlaySoundFile("Interface\\AddOns\\rEmote\\Alarm.mp3")
+    PlaySoundFile('Interface\\AddOns\\rEmote\\Alarm.mp3')
 
   end)

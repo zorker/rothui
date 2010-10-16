562fb00c1db888e3b09e91eaabe32b3c71db553c
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2010-10-16 10:33:56 +0000

    New version, now finally with a REAL config! :)

---
+++ trunk/rActionButtonStyler/config.lua
@@ -0,0 +1,62 @@
+﻿  
+  -- // rActionButtonStyler
+  -- // zork - 2010
+  
+  -----------------------------
+  -- INIT
+  -----------------------------
+  
+  --get the addon namespace
+  local addon, ns = ...
+  
+  --generate a holder for the config data
+  local cfg = CreateFrame("Frame")
+  
+  -----------------------------
+  -- CONFIG
+  -----------------------------
+  
+  cfg.textures = {
+    normal            = "Interface\\AddOns\\rActionButtonStyler\\media\\gloss",
+    flash             = "Interface\\AddOns\\rActionButtonStyler\\media\\flash",
+    hover             = "Interface\\AddOns\\rActionButtonStyler\\media\\hover",
+    pushed            = "Interface\\AddOns\\rActionButtonStyler\\media\\pushed",
+    checked           = "Interface\\AddOns\\rActionButtonStyler\\media\\checked",
+    equipped          = "Interface\\AddOns\\rActionButtonStyler\\media\\gloss_grey",
+  }
+  
+  cfg.color = {
+    normal            = { r = 0.37, g = 0.3, b = 0.3, },
+    equipped          = { r = 0.1, g = 0.5, b = 0.1, },
+    classcolored      = false,    
+  }
+  
+  cfg.hotkeys = {
+    show            = true,
+    fontsize        = 12,
+    pos             = { a1 = "TOPRIGHT", x = 0, y = 0 }, 
+  }
+  
+  cfg.macroname = {
+    show            = true,
+    fontsize        = 12,
+  }
+  
+  cfg.itemcount = {
+    show            = true,
+    fontsize        = 12,
+    pos             = { a1 = "BOTTOMRIGHT", x = 0, y = 0 }, 
+  }
+  
+  cfg.cooldown = {
+    spacing         = 0,
+  }
+  
+  cfg.font = "Fonts\\FRIZQT__.TTF"
+
+  -----------------------------
+  -- HANDOVER
+  -----------------------------
+  
+  --hand the config to the namespace for usage in other lua files (remember: those lua files must be called after the cfg.lua)
+  ns.cfg = cfg

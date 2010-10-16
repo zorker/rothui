837f59897d2f853fa7148bc2cdf1de63e009c50c
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2010-10-16 20:52:54 +0000

    questwatchframemover is now a seperate mod

---
+++ trunk/rQuestWatchFrameMover/core.lua
@@ -0,0 +1,60 @@
+
+  -- // rQuestWatchFrameMover
+  -- // zork - 2010
+  
+  -----------------------------
+  -- INIT
+  -----------------------------
+
+  local _G = _G
+
+  -----------------------------
+  -- FUNCTIONS
+  -----------------------------
+
+  --tooltip for icon func
+  local function rQWFM_Tooltip(self)
+    GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
+    GameTooltip:AddLine("Drag me!", 0, 1, 0.5, 1, 1, 1)
+    GameTooltip:Show()
+  end
+
+  local function init()
+    
+    --make the quest watchframe movable
+    local wf = _G['WatchFrame']
+    local wfh = _G['WatchFrameHeader']
+    local wfl = _G['WatchFrameLines']
+    
+    wf:SetMovable(true)
+    wf:SetUserPlaced(true)
+    
+    wfh:EnableMouse(true)
+    wfh:RegisterForDrag("LeftButton")
+    wfh:SetHitRectInsets(-15, -15, -5, -5)
+    wfh:SetScript("OnDragStart", function(s) 
+      local f = s:GetParent()
+      f:StartMoving()
+    end)
+    wfh:SetScript("OnDragStop", function(s) 
+      local f = s:GetParent()
+      f:StopMovingOrSizing()
+    end)
+    wfh:SetScript("OnEnter", function(s) 
+      rQWFM_Tooltip(s) 
+    end)
+    wfh:SetScript("OnLeave", function(s) 
+      GameTooltip:Hide() 
+    end)
+
+  end
+
+  local a = CreateFrame("Frame")
+
+  a:SetScript("OnEvent", function(self, event)
+    if(event=="PLAYER_LOGIN") then
+      init()
+    end
+  end)
+  
+  a:RegisterEvent("PLAYER_LOGIN")

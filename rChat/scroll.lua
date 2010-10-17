5bbfe08951ed023d26d9091e2955b9dfd7f5e41b
Erik Raetz <zorker@users.noreply.github.com>
2010-10-17 20:32:02 +0000

    tabs!

--- rChat/scroll.lua
+++ rChat/scroll.lua
@@ -1,16 +1,16 @@
   
   FloatingChatFrame_OnMouseScroll = function(self, dir)
-  	if(dir > 0) then
-  		if(IsShiftKeyDown()) then
-  			self:ScrollToTop()
-  		else
-  		  self:ScrollUp()
-  		end
-  	else
-  		if(IsShiftKeyDown()) then
-  			self:ScrollToBottom()
-  	  else
-  	    self:ScrollDown()
-  	  end
-  	end
+    if(dir > 0) then
+      if(IsShiftKeyDown()) then
+        self:ScrollToTop()
+      else
+        self:ScrollUp()
+      end
+    else
+      if(IsShiftKeyDown()) then
+        self:ScrollToBottom()
+      else
+        self:ScrollDown()
+      end
+    end
   end

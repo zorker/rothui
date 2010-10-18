0be0590745ea48e0154ba86b182fa0f2a8078d89
erik.raetz <erik.raetz@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2010-10-18 17:36:08 +0000

    removed unneeded code and fixed the mana coloring, actually going on powertype now

--- trunk/rBottomBarStyler/core.lua
+++ trunk/rBottomBarStyler/core.lua
@@ -47,13 +47,10 @@
   rBottomBarStyler = rBottomBarStyler or {}
   local frame_to_scale
   local bar_to_show
-  local hglow1, hglow2, mglow1, mglow2, hfill, mfill
+  local hfill, mfill
   local hgal1,hgal2,hgal3,mgal1,mgal2,mgal3
   local frame_to_drag
-  local fog_smoother = 1.3
-  local text_display = 1
-  local save_dudustance
-  
+    
   local player_class_color = RAID_CLASS_COLORS[select(2, UnitClass("player"))]
   
   local orbtab = {
@@ -74,36 +71,23 @@
     [5] = {r = 0.35, g = 0.9,   b = 0.9, }, -- runic
   }
   
+  local rBBS_PowerColor = {}  
+  rBBS_PowerColor["MANA"] = { r = 0, g = 0.4, b = 1 };
+  rBBS_PowerColor["RAGE"] = { r = 1.00, g = 0.00, b = 0.00 };
+  rBBS_PowerColor["FOCUS"] = { r = 1.00, g = 0.50, b = 0.25 };
+  rBBS_PowerColor["ENERGY"] = { r = 1.00, g = 0.75, b = 0.10 };
+  rBBS_PowerColor["HAPPINESS"] = { r = 0.00, g = 1.00, b = 1.00 };
+  rBBS_PowerColor["RUNES"] = { r = 0.50, g = 0.50, b = 0.50 };
+  rBBS_PowerColor["RUNIC_POWER"] = { r = 0.00, g = 0.82, b = 1.00 };
+  rBBS_PowerColor["AMMOSLOT"] = { r = 0.80, g = 0.60, b = 0.00 };
+  rBBS_PowerColor["FUEL"] = { r = 0.0, g = 0.55, b = 0.5 };
+  
   ------------------------------------------------------
   -- / CHAT OUTPUT FUNC / --
   ------------------------------------------------------
   
   local function am(text)
     DEFAULT_CHAT_FRAME:AddMessage(text)
-  end
-  
-  ------------------------------------------------------
-  -- / SET AUTO MANA / --
-  ------------------------------------------------------
-  
-  local function set_automana()
-    if rBottomBarStyler.automana == 1 then
-      local st = GetShapeshiftForm()
-      local _, pt = UnitClass("player")
-      if pt == "WARRIOR" then
-        rBottomBarStyler.manaorb = 1
-      elseif pt == "DEATHKNIGHT" then
-        rBottomBarStyler.manaorb = 5
-      elseif pt == "ROGUE" then
-        rBottomBarStyler.manaorb = 4
-      elseif pt == "DRUID" and st == 3 then
-        rBottomBarStyler.manaorb = 4
-      elseif pt == "DRUID" and st == 1 then
-        rBottomBarStyler.manaorb = 1
-      else
-        rBottomBarStyler.manaorb = 3
-      end
-    end
   end
 
   ------------------------------------------------------
@@ -135,8 +119,6 @@
     if(not rBottomBarStyler.automana) then 
       rBottomBarStyler.automana = default_automana 
     end    
-    
-    set_automana()
     
   end
   
@@ -194,77 +176,29 @@
   
   local function set_the_hglows()
     hfill:SetVertexColor(orbtab[rBottomBarStyler.healthorb].r,orbtab[rBottomBarStyler.healthorb].g,orbtab[rBottomBarStyler.healthorb].b)
-    if usegalaxy == 1 then
-      hgal1.t:SetVertexColor(galaxytab[rBottomBarStyler.healthorb].r,galaxytab[rBottomBarStyler.healthorb].g,galaxytab[rBottomBarStyler.healthorb].b)
-      hgal2.t:SetVertexColor(galaxytab[rBottomBarStyler.healthorb].r,galaxytab[rBottomBarStyler.healthorb].g,galaxytab[rBottomBarStyler.healthorb].b)
-      hgal3.t:SetVertexColor(galaxytab[rBottomBarStyler.healthorb].r,galaxytab[rBottomBarStyler.healthorb].g,galaxytab[rBottomBarStyler.healthorb].b)
-    else
-      hglow1:SetModel(orbtab[rBottomBarStyler.healthorb].anim)
-      hglow1:SetModelScale(orbtab[rBottomBarStyler.healthorb].scale)
-      hglow1:SetPosition(orbtab[rBottomBarStyler.healthorb].z, orbtab[rBottomBarStyler.healthorb].x, orbtab[rBottomBarStyler.healthorb].y) 
-      hglow2:SetModel(orbtab[rBottomBarStyler.healthorb].anim)
-      hglow2:SetModelScale(orbtab[rBottomBarStyler.healthorb].scale)
-      hglow2:SetPosition(orbtab[rBottomBarStyler.healthorb].z, orbtab[rBottomBarStyler.healthorb].x, orbtab[rBottomBarStyler.healthorb].y+1) 
-    end
+    hgal1.t:SetVertexColor(galaxytab[rBottomBarStyler.healthorb].r,galaxytab[rBottomBarStyler.healthorb].g,galaxytab[rBottomBarStyler.healthorb].b)
+    hgal2.t:SetVertexColor(galaxytab[rBottomBarStyler.healthorb].r,galaxytab[rBottomBarStyler.healthorb].g,galaxytab[rBottomBarStyler.healthorb].b)
+    hgal3.t:SetVertexColor(galaxytab[rBottomBarStyler.healthorb].r,galaxytab[rBottomBarStyler.healthorb].g,galaxytab[rBottomBarStyler.healthorb].b)
   end
 
   local function set_the_mglows()
-    mfill:SetVertexColor(orbtab[rBottomBarStyler.manaorb].r,orbtab[rBottomBarStyler.manaorb].g,orbtab[rBottomBarStyler.manaorb].b)
-    if usegalaxy == 1 then
+
+    local powertype = select(2, UnitPowerType("player"))
+    local color = rBBS_PowerColor[powertype]
+    
+    if color and rBottomBarStyler.automana == 1 then
+      mfill:SetVertexColor(color.r,color.g,color.b)
+      mgal1.t:SetVertexColor(color.r,color.g,color.b)
+      mgal2.t:SetVertexColor(color.r,color.g,color.b)
+      mgal3.t:SetVertexColor(color.r,color.g,color.b)
+    else
+      mfill:SetVertexColor(orbtab[rBottomBarStyler.manaorb].r,orbtab[rBottomBarStyler.manaorb].g,orbtab[rBottomBarStyler.manaorb].b)
       mgal1.t:SetVertexColor(galaxytab[rBottomBarStyler.manaorb].r,galaxytab[rBottomBarStyler.manaorb].g,galaxytab[rBottomBarStyler.manaorb].b)
       mgal2.t:SetVertexColor(galaxytab[rBottomBarStyler.manaorb].r,galaxytab[rBottomBarStyler.manaorb].g,galaxytab[rBottomBarStyler.manaorb].b)
       mgal3.t:SetVertexColor(galaxytab[rBottomBarStyler.manaorb].r,galaxytab[rBottomBarStyler.manaorb].g,galaxytab[rBottomBarStyler.manaorb].b)
-    else
-      mglow1:SetModel(orbtab[rBottomBarStyler.manaorb].anim)
-      mglow1:SetModelScale(orbtab[rBottomBarStyler.manaorb].scale)
-      mglow1:SetPosition(orbtab[rBottomBarStyler.manaorb].z, orbtab[rBottomBarStyler.manaorb].x, orbtab[rBottomBarStyler.manaorb].y) 
-      mglow2:SetModel(orbtab[rBottomBarStyler.manaorb].anim)
-      mglow2:SetModelScale(orbtab[rBottomBarStyler.manaorb].scale)
-      mglow2:SetPosition(orbtab[rBottomBarStyler.manaorb].z, orbtab[rBottomBarStyler.manaorb].x, orbtab[rBottomBarStyler.manaorb].y+1) 
     end
   end
 
-  local function set_the_shapeshift_mglows()
-    local st = GetShapeshiftForm()
-    if st ~= save_dudustance then
-      mfill:SetVertexColor(orbtab[rBottomBarStyler.manaorb].r,orbtab[rBottomBarStyler.manaorb].g,orbtab[rBottomBarStyler.manaorb].b)
-      if usegalaxy == 1 then
-        mgal1.t:SetVertexColor(galaxytab[rBottomBarStyler.manaorb].r,galaxytab[rBottomBarStyler.manaorb].g,galaxytab[rBottomBarStyler.manaorb].b)
-        mgal2.t:SetVertexColor(galaxytab[rBottomBarStyler.manaorb].r,galaxytab[rBottomBarStyler.manaorb].g,galaxytab[rBottomBarStyler.manaorb].b)
-        mgal3.t:SetVertexColor(galaxytab[rBottomBarStyler.manaorb].r,galaxytab[rBottomBarStyler.manaorb].g,galaxytab[rBottomBarStyler.manaorb].b)
-      else
-        mglow1:SetModel(orbtab[rBottomBarStyler.manaorb].anim)
-        mglow1:SetModelScale(orbtab[rBottomBarStyler.manaorb].scale)
-        mglow1:SetPosition(orbtab[rBottomBarStyler.manaorb].z, orbtab[rBottomBarStyler.manaorb].x, orbtab[rBottomBarStyler.manaorb].y) 
-        mglow2:SetModel(orbtab[rBottomBarStyler.manaorb].anim)
-        mglow2:SetModelScale(orbtab[rBottomBarStyler.manaorb].scale)
-        mglow2:SetPosition(orbtab[rBottomBarStyler.manaorb].z, orbtab[rBottomBarStyler.manaorb].x, orbtab[rBottomBarStyler.manaorb].y+1) 
-      end
-      save_dudustance = st
-    end
-  end
-
-  ------------------------------------------------------
-  -- / CREATE ME A ORB GLOW FUNC / --
-  ------------------------------------------------------
-
-  local function create_me_a_orb_glow(f,useorb,pos)
-    local glow = CreateFrame("PlayerModel", nil, f)
-    glow:SetFrameStrata("BACKGROUND")
-    glow:SetFrameLevel(5)
-    glow:SetAllPoints(f)
-    glow:SetModel(orbtab[useorb].anim)
-    glow:SetModelScale(orbtab[useorb].scale)
-    glow:SetPosition(orbtab[useorb].z, orbtab[useorb].x, orbtab[useorb].y+pos) 
-    glow:SetAlpha(1/fog_smoother)
-    glow:SetScript("OnShow",function() 
-      glow:SetModel(orbtab[useorb].anim)
-      glow:SetModelScale(orbtab[useorb].scale)
-      glow:SetPosition(orbtab[useorb].z, orbtab[useorb].x, orbtab[useorb].y+pos) 
-    end)
-    return glow
-  end
-  
   ------------------------------------------------------
   -- / CREATE ME A ORB GALAXY FUNC / --
   ------------------------------------------------------
@@ -290,21 +224,9 @@
     a1:SetDegrees(360)
     a1:SetDuration(dur)
     h.ag.a1 = a1
-    
-    h:SetScript("OnUpdate",function(self,elapsed)
-      local t = self.total
-      if (not t) then
-        self.total = 0
-        return
-      end
-      t = t + elapsed
-      if (t<1) then
-        self.total = t
-        return
-      else
-        h.ag:Play()
-      end
-    end)
+
+    h.ag:SetLooping("REPEAT")  
+    h.ag:Play()
     
     return h
   
@@ -375,7 +297,7 @@
   -- / ORB HEALTH FUNC / --
   ------------------------------------------------------
   
-  local function orbhealth(orb1,orb1_fill,glow1,glow2,orbtext1,orbtext2)
+  local function orbhealth(orb1,orb1_fill,orbtext1,orbtext2)
     orb1:SetScript("OnEvent", function(self, event, arg1, ...)
       if event == "PLAYER_LOGIN" or event == "PLAYER_ENTERING_WORLD" or arg1 == "player" then      
         local uh, uhm = UnitHealth("player"), UnitHealthMax("player")
@@ -383,22 +305,15 @@
         local nuh = do_format(uh)
         local nuhm = do_format(uhm)
         orbtext1:SetText(perc)
-        if text_display == 1 then
-          orbtext2:SetText(nuh)
-        else
-          orbtext2:SetText(nuh.."/"..nuhm)
-        end
+        orbtext2:SetText(nuh)
         orb1_fill:SetHeight((uh/uhm) * orb1_fill:GetWidth())
         orb1_fill:SetTexCoord(0,1,  math.abs(uh/uhm - 1),1)
         
-        if usegalaxy == 1 then
-          hgal1:SetAlpha(uh / uhm)
-          hgal2:SetAlpha(uh / uhm)
-          hgal3:SetAlpha(uh / uhm)
-        else
-          glow1:SetAlpha((uh / uhm)/fog_smoother)
-          glow2:SetAlpha((uh / uhm)/fog_smoother)
-        end
+        set_the_hglows()
+        
+        hgal1:SetAlpha(uh / uhm)
+        hgal2:SetAlpha(uh / uhm)
+        hgal3:SetAlpha(uh / uhm)
       end
     end)
     orb1:RegisterEvent("UNIT_HEALTH")
@@ -410,44 +325,37 @@
   -- / ORB MANA FUNC / --
   ------------------------------------------------------
   
-  local function orbmana(orb2,orb2_fill,glow1,glow2,orbtext1,orbtext2)
+  local function orbmana(orb2,orb2_fill,orbtext1,orbtext2)
     orb2:SetScript("OnEvent", function(self, event, arg1, ...)
-      if event == "PLAYER_LOGIN" or event == "PLAYER_ENTERING_WORLD" or arg1 == "player" then      
+      if event == "PLAYER_LOGIN" or event == "PLAYER_ENTERING_WORLD" or event == "UPDATE_SHAPESHIFT_FORM" or arg1 == "player" then      
         local um, umm = UnitMana("player"), UnitManaMax("player")
         local perc = floor(um/umm*100)
         local nuh = do_format(um)
         local nuhm = do_format(umm)
-        local _, class = UnitClass("player")
-        local shape = GetShapeshiftForm()    
-        if text_display == 1 then
-          if class == "WARRIOR" or (class == "DRUID" and shape == 3) or (class == "DRUID" and shape == 1) or class == "DEATHKNIGHT" or class == "ROGUE" then
-            orbtext1:SetText(nuh)
-            orbtext2:SetText(perc)
-          else
-            orbtext1:SetText(perc)
-            orbtext2:SetText(nuh)            
-          end
+
+        local powertype = select(2, UnitPowerType("player"))
+        if powertype ~= "MANA" then
+          orbtext1:SetText(nuh)
+          orbtext2:SetText(perc)
         else
           orbtext1:SetText(perc)
-          orbtext2:SetText(nuh.."/"..nuhm)
+          orbtext2:SetText(nuh)            
         end
         orb2_fill:SetHeight((um/umm) * orb2_fill:GetWidth())
         orb2_fill:SetTexCoord(0,1,  math.abs(um/umm - 1),1)
         
-        if usegalaxy == 1 then
-          mgal1:SetAlpha(um / umm)
-          mgal2:SetAlpha(um / umm)
-          mgal3:SetAlpha(um / umm)
-        else
-          glow1:SetAlpha((um / umm)/fog_smoother)
-          glow2:SetAlpha((um / umm)/fog_smoother)
-        end
+        set_the_mglows()
+        
+        mgal1:SetAlpha(um / umm)
+        mgal2:SetAlpha(um / umm)
+        mgal3:SetAlpha(um / umm)
       end
     end)
     orb2:RegisterEvent("UNIT_POWER")
     orb2:RegisterEvent("UNIT_MAXPOWER")
     orb2:RegisterEvent("PLAYER_ENTERING_WORLD")
     orb2:RegisterEvent("PLAYER_LOGIN")
+    orb2:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
   end
   
   ------------------------------------------------------
@@ -489,34 +397,18 @@
 
     local orb1_back = create_me_a_texture(orb1,"BORDER","Interface\\AddOns\\rBottomBarStyler\\orbtex\\orb_back2")
     local orb1_fill = create_me_a_texture(orb1,"ARTWORK","Interface\\AddOns\\rBottomBarStyler\\orbtex\\"..orbfilltex,"fill")
-    --glows
-    local glow1, glow2
     if orbtype == "life" then
       orb1_fill:SetVertexColor(orbtab[useorb].r,orbtab[useorb].g,orbtab[useorb].b)
       hfill = orb1_fill
-      if usegalaxy == 1 then
-        hgal1 = create_me_a_galaxy(orb1,0,15,110,1,35,"galaxy2",useorb)
-        hgal2 = create_me_a_galaxy(orb1,0,-10,150,1,45,"galaxy",useorb)
-        hgal3 = create_me_a_galaxy(orb1,-10,-10,130,1,18,"galaxy3",useorb)
-      else
-        glow1 = create_me_a_orb_glow(orb1,useorb,0)
-        glow2 = create_me_a_orb_glow(orb1,useorb,1)
-        hglow1 = glow1
-        hglow2 = glow2
-      end
+      hgal1 = create_me_a_galaxy(orb1,0,15,110,1,35,"galaxy2",useorb)
+      hgal2 = create_me_a_galaxy(orb1,0,-10,150,1,45,"galaxy",useorb)
+      hgal3 = create_me_a_galaxy(orb1,-10,-10,130,1,18,"galaxy3",useorb)
     else
       orb1_fill:SetVertexColor(orbtab[useorb].r,orbtab[useorb].g,orbtab[useorb].b)
       mfill = orb1_fill
-      if usegalaxy == 1 then
-        mgal1 = create_me_a_galaxy(orb1,0,10,110,1,40,"galaxy2",useorb)
-        mgal2 = create_me_a_galaxy(orb1,-10,-10,150,1,50,"galaxy",useorb)
-        mgal3 = create_me_a_galaxy(orb1,10,-10,130,1,20,"galaxy3",useorb)
-      else
-        glow1 = create_me_a_orb_glow(orb1,useorb,0)
-        glow2 = create_me_a_orb_glow(orb1,useorb,1)
-        mglow1 = glow1
-        mglow2 = glow2
-      end
+      mgal1 = create_me_a_galaxy(orb1,0,10,110,1,40,"galaxy2",useorb)
+      mgal2 = create_me_a_galaxy(orb1,-10,-10,150,1,50,"galaxy",useorb)
+      mgal3 = create_me_a_galaxy(orb1,10,-10,130,1,20,"galaxy3",useorb)
     end
     local orb1_glossholder = create_me_a_frame("Frame",nil,orb1,"BACKGROUND",6,orbsize,orbsize,"BOTTOM",0,0,1)
     local orb1_gloss = create_me_a_texture(orb1_glossholder,"BACKGROUND","Interface\\AddOns\\rBottomBarStyler\\orbtex\\orb_gloss")
@@ -527,21 +419,9 @@
     orbtext2:SetPoint("CENTER", 0, -(orbsize/12))
     orbtext2:SetTextColor(0.6,0.6,0.6)
     if orbtype == "life" then
-      orbhealth(orb1,orb1_fill,glow1,glow2,orbtext1,orbtext2)
+      orbhealth(orb1,orb1_fill,orbtext1,orbtext2)
     else
-      orbmana(orb1,orb1_fill,glow1,glow2,orbtext1,orbtext2)
-      --druid mana color on stance
-      local _, pt = UnitClass("player")
-      if pt == "DRUID" then
-        orb1_glossholder:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
-        orb1_glossholder:RegisterEvent("PLAYER_LOGIN")
-        orb1_glossholder:SetScript("OnEvent", function(self,event)
-          if event == "UPDATE_SHAPESHIFT_FORM" then
-            set_automana()
-            set_the_shapeshift_mglows()
-          end
-        end)
-      end
+      orbmana(orb1,orb1_fill,orbtext1,orbtext2)
     end
   end  
   
@@ -917,7 +797,6 @@
         if tonumber(c) then
           am("Automana is set to: "..c)
           rBottomBarStyler.automana = tonumber(c)
-          set_automana()
           set_the_mglows()
         else
           am("No number value.")

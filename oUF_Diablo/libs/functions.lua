d10db42726b77e3faacd163121a7325d04fbe02c
Erik Raetz <zorker@users.noreply.github.com>
2010-10-21 07:55:36 +0000

    tabs

--- oUF_Diablo/libs/functions.lua
+++ oUF_Diablo/libs/functions.lua
@@ -54,16 +54,16 @@
     f:SetBackdropBorderColor(0,0,0,1)
   end
   
-	func.menu = function(self)
-		local unit = self.unit:sub(1, -2)
-		local cunit = self.unit:gsub("^%l", string.upper)
-		if(cunit == 'Vehicle') then	cunit = 'Pet' end
-		if(unit == "party" or unit == "partypet") then
-			ToggleDropDownMenu(1, nil, _G["PartyMemberFrame"..self.id.."DropDown"], "cursor", 0, 0)
-		elseif(_G[cunit.."FrameDropDown"]) then
-			ToggleDropDownMenu(1, nil, _G[cunit.."FrameDropDown"], "cursor", 0, 0)
-		end
-	end
+  func.menu = function(self)
+    local unit = self.unit:sub(1, -2)
+    local cunit = self.unit:gsub("^%l", string.upper)
+    if(cunit == 'Vehicle') then  cunit = 'Pet' end
+    if(unit == "party" or unit == "partypet") then
+      ToggleDropDownMenu(1, nil, _G["PartyMemberFrame"..self.id.."DropDown"], "cursor", 0, 0)
+    elseif(_G[cunit.."FrameDropDown"]) then
+      ToggleDropDownMenu(1, nil, _G[cunit.."FrameDropDown"], "cursor", 0, 0)
+    end
+  end
   
   --create castbar func
   func.createCastbar = function(f)

9d8a5408c329dd78c0a3244140c60eadc4dedac0
Erik Raetz <zorker@users.noreply.github.com>
2011-01-19 12:53:56 +0000

    - marker option added (enabled red color highlight for player statusbar) - /rthreat slash command added to move/lock frames - option added to disable title frame

---
+++ rThreat/slashcmd.lua
@@ -0,0 +1,58 @@
+ 
+  rThreat_Frames = {
+    "rThreatDragFrame", 
+  }
+  
+  local _, v
+  
+  function rThreat_unlockFrames()
+    print("rThreat: Frames unlocked")
+    for _, v in pairs(rThreat_Frames) do
+      local f = _G[v]
+      if f and f:IsUserPlaced() then
+        --print(f:GetName())
+        f.dragtexture:SetAlpha(0.2)
+        f:EnableMouse(true)
+        f:RegisterForDrag("LeftButton")
+        f:SetScript("OnEnter", function(s) 
+          GameTooltip:SetOwner(s, "ANCHOR_TOP")
+          GameTooltip:AddLine(s:GetName(), 0, 1, 0.5, 1, 1, 1)
+          GameTooltip:AddLine("Hold down ALT+SHIFT to drag!", 1, 1, 1, 1, 1, 1)
+          GameTooltip:Show()
+        end)
+        f:SetScript("OnLeave", function(s) GameTooltip:Hide() end)
+      end
+    end
+  end  
+  
+  function rThreat_lockFrames()
+    print("rThreat: frames locked")
+    for _, v in pairs(rThreat_Frames) do
+      local f = _G[v]
+      if f and f:IsUserPlaced() then
+        f.dragtexture:SetAlpha(0)
+        f:EnableMouse(nil)
+        f:RegisterForDrag(nil)
+        f:SetScript("OnEnter", nil)
+        f:SetScript("OnLeave", nil)
+      end
+    end
+  end
+  
+  local function SlashCmd(cmd)    
+    if (cmd:match"unlock") then
+      rThreat_unlockFrames()
+    elseif (cmd:match"lock") then
+      rThreat_lockFrames()
+    else
+      print("|c00FFAA00rThreat command list:|r")
+      print("|c00FFAA00\/rthreat lock|r, to lock")
+      print("|c00FFAA00\/rthreat unlock|r, to unlock")
+    end
+  end
+
+  SlashCmdList["rthreat"] = SlashCmd;
+  SLASH_rthreat1 = "/rthreat";
+  
+  print("|c00FFAA00rThreat loaded.|r")
+  print("|c00FFAA00\/rthreat|r to display the command list")

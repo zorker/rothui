09d67096a2628cf68a01f9287d5a9bc4f6afc299
Erik Raetz <zorker@users.noreply.github.com>
2011-06-11 20:04:14 +0000

    added all new animations I could find and that made sense

--- DiscoKugel/config.lua
+++ DiscoKugel/config.lua
@@ -46,7 +46,7 @@
       displayid = 17054,
     },
     [3] = {
-      color = {r=0,g=0.8,b=0,},
+      color = {r=0,g=0.4,b=0,},
       displayid = 17055,
     },
     [4] = {
@@ -80,6 +80,106 @@
     [11] = {
       color = {r=1,g=0.3,b=0,},
       displayid = 30903,
+    },
+    [12] = {
+      color = {r=0.4,g=0,b=0,},
+      displayid = 23422,
+    },
+    [13] = {
+      color = {r=0,g=0.4,b=1,},
+      displayid = 27393,
+    },
+    [14] = {
+      color = {r=0.4,g=0,b=0,},
+      displayid = 20894,
+    },
+    [15] = {
+      color = {r=0.4,g=0,b=1,},
+      displayid = 15438,
+    },
+    [16] = {
+      color = {r=0,g=0.7,b=1,},
+      displayid = 20782,
+    },
+    [17] = {
+      color = {r=1,g=0.4,b=0,},
+      displayid = 22814,
+    },
+    [18] = {
+      color = {r=0.3,g=0.3,b=0.3,},
+      displayid = 23310,
+    },
+    [19] = {
+      color = {r=0.6,g=0.6,b=0.6,},
+      displayid = 23343,
+    },
+    [20] = {
+      color = {r=0.4,g=0,b=0,},
+      displayid = 24813,
+    },
+    [21] = {
+      color = {r=0.4,g=0.6,b=0,},
+      displayid = 25392,
+    },
+    [22] = {
+      color = {r=0,g=0.6,b=1,},
+      displayid = 26320,
+    },
+    [23] = {
+      color = {r=0.4,g=0.6,b=0,},
+      displayid = 27625,
+    },
+    [24] = {
+      color = {r=0.5,g=0,b=1,},
+      displayid = 28460,
+    },
+    [25] = {
+      color = {r=0,g=0.4,b=0,},
+      displayid = 28450,
+    },
+    [26] = {
+      color = {r=0.4,g=0.6,b=0,},
+      displayid = 29133,
+    },
+    [27] = {
+      color = {r=1,g=1,b=1,},
+      displayid = 29286,
+    },
+    [28] = {
+      color = {r=0,g=0.6,b=0.9,},
+      displayid = 29561,
+    },
+    [29] = {
+      color = {r=1,g=0.5,b=0,},
+      displayid = 30660,
+    },
+    [30] = {
+      color = {r=1,g=0.2,b=0,},
+      displayid = 30998,
+    },
+    [31] = {
+      color = {r=1,g=1,b=1,},
+      displayid = 32368,
+    },
+    [32] = {
+      color = {r=1,g=0,b=0,},
+      displayid = 33853,
+    },
+    [33] = {
+      color = {r=0,g=0,b=0.4,},
+      displayid = 34319,
+    },
+    [34] = {
+      color = {r=0,g=1,b=1,},
+      displayid = 34404,
+    },
+    [35] = {
+      color = {r=0.3,g=0,b=0.3,},
+      displayid = 34645,
+    },
+    [36] = {
+      color = {r=0.8,g=0.2,b=0,},
+      displayid = 35058,
     },
   }
 

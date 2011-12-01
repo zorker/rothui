9e854eba0d33936762dbb5f54a840046099bd40b
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2011-12-01 21:02:43 +0000

    First set of fixes for patch 4.3

--- trunk/rActionBarStyler/bars/extra.lua
+++ trunk/rActionBarStyler/bars/extra.lua
@@ -1,51 +1,51 @@
+    --get the addon namespace
+      local addon, ns = ...
 
-  --get the addon namespace
-  local addon, ns = ...
+      --get the config values
+      local cfg = ns.cfg
+      local barcfg = cfg.bars.extrabar
 
-  --get the config values
-  local cfg = ns.cfg
-  local barcfg = cfg.bars.extrabar
+      if not barcfg.disable then
 
-  if not barcfg.disable then
+        local num = 1
 
-    local num = 1
+        local bar = CreateFrame("Frame","rABS_ExtraActionBar",UIParent,"SecureHandlerStateTemplate")
+        bar:SetWidth(barcfg.buttonsize*num+barcfg.buttonspacing*(num-1))
+        bar:SetHeight(barcfg.buttonsize)
+        bar:SetPoint(barcfg.pos.a1,barcfg.pos.af,barcfg.pos.a2,barcfg.pos.x,barcfg.pos.y)
+        bar:SetHitRectInsets(-cfg.barinset, -cfg.barinset, -cfg.barinset, -cfg.barinset)
+        bar:SetScale(barcfg.barscale)
 
-    local bar = CreateFrame("Frame","rABS_ExtraActionBar",UIParent,"SecureHandlerStateTemplate")
-    bar:SetWidth(barcfg.buttonsize*num+barcfg.buttonspacing*(num-1))
-    bar:SetHeight(barcfg.buttonsize)
-    bar:SetPoint(barcfg.pos.a1,barcfg.pos.af,barcfg.pos.a2,barcfg.pos.x,barcfg.pos.y)
-    bar:SetHitRectInsets(-cfg.barinset, -cfg.barinset, -cfg.barinset, -cfg.barinset)
+        cfg.applyDragFunctionality(bar,barcfg.userplaced,barcfg.locked)
 
-    if barcfg.testmode then
-      bar:SetBackdrop(cfg.backdrop)
-      bar:SetBackdropColor(1,0.8,1,0.6)
-    end
-    bar:SetScale(barcfg.barscale)
+        ExtraActionBarFrame:SetParent(bar)
+        ExtraActionBarFrame:EnableMouse(false)
 
-    cfg.applyDragFunctionality(bar,barcfg.userplaced,barcfg.locked)
+        --local txt = "Interface\Addons" --debug
+        --print(string.sub(txt,1,9)) --debug
 
-    for i=1, num do
-      local button = _G["ExtraActionButton"..i]
-      if not button then return end
-      button:SetSize(barcfg.buttonsize, barcfg.buttonsize)
-      button:ClearAllPoints()
-      if i == 1 then
-        button:SetPoint("BOTTOMLEFT", bar, 0,0)
-      else
-        local previous = _G["ExtraActionButton"..i-1]
-        button:SetPoint("LEFT", previous, "RIGHT", barcfg.buttonspacing, 0)
-      end
-    end
-
-    bar:RegisterEvent("UPDATE_EXTRA_ACTIONBAR")
-    bar:SetScript("OnEvent", function(self, event, ...)
-      if (event == "UPDATE_EXTRA_ACTIONBAR") then
-        if (HasExtraActionBar()) then
-          self:Show()
-        elseif(self:IsShown()) then
-          self:Hide()
+        local disableTexture = function(style, texture)
+          if string.sub(texture,1,9) == "Interface" then
+            style:SetTexture("")--disable button style texture
+          end
         end
-      end
-    end)
 
-  end --disable
+        for i=1, num do
+          local button = _G["ExtraActionButton"..i]
+          if not button then return end
+          if button.style then
+            --print(button.style:GetTexture()) --debug
+            button.style:SetTexture("")
+            hooksecurefunc(button.style, "SetTexture", disableTexture)
+          end
+          button:SetSize(barcfg.buttonsize, barcfg.buttonsize)
+          button:ClearAllPoints()
+          if i == 1 then
+            button:SetPoint("BOTTOMLEFT", bar, 0,0)
+          else
+            local previous = _G["ExtraActionButton"..i-1]
+            button:SetPoint("LEFT", previous, "RIGHT", barcfg.buttonspacing, 0)
+          end
+        end
+
+      end --disable

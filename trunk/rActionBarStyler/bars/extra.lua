bedd5b0989926927e677cf528083801dbb273183
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2011-12-06 17:32:25 +0000

    Final tweaks for the extraActionbutton and small adjustments to the unit highlights in the oUF_Diablo layout

--- trunk/rActionBarStyler/bars/extra.lua
+++ trunk/rActionBarStyler/bars/extra.lua
@@ -4,34 +4,48 @@ local addon, ns = ...
 local cfg = ns.cfg
 local barcfg = cfg.bars.extrabar
 if barcfg and not barcfg.disable then
+
+  --holder frame
   local bar = CreateFrame("Frame","rABS_ExtraActionBar",UIParent,"SecureHandlerStateTemplate")
   bar:SetSize(barcfg.buttonsize,barcfg.buttonsize)
   bar:SetPoint(barcfg.pos.a1,barcfg.pos.af,barcfg.pos.a2,barcfg.pos.x,barcfg.pos.y)
   bar:SetHitRectInsets(-cfg.barinset, -cfg.barinset, -cfg.barinset, -cfg.barinset)
   bar:SetScale(barcfg.barscale)
   cfg.applyDragFunctionality(bar,barcfg.userplaced,barcfg.locked)
+
+  --the frame
+  local f = ExtraActionBarFrame
+  f:SetParent(bar)
+  f:ClearAllPoints()
+  f:SetPoint("CENTER", 0, 0)
+  f.ignoreFramePositionManager = true
+
+  --the button
+  local b = ExtraActionButton1
+  b:SetSize(barcfg.buttonsize,barcfg.buttonsize)
+  bar.button = b
+
+  --style texture
+  local s = b.style
+  s:SetTexture(nil)
   local disableTexture = function(style, texture)
+    if not texture then return end
     if string.sub(texture,1,9) == "Interface" then
       style:SetTexture(nil) --bzzzzzzzz
     end
   end
-  local b = ExtraActionButton1
-  b:SetParent(bar)
-  b:SetSize(barcfg.buttonsize,barcfg.buttonsize)
-  b:ClearAllPoints()
-  b:SetPoint("CENTER",0,0)
-  b:Show()
-  local s = b.style
-  s:SetTexture(nil)
   hooksecurefunc(s, "SetTexture", disableTexture)
+
+  --register the event, make sure the damn button shows up
   bar:RegisterEvent("UPDATE_EXTRA_ACTIONBAR")
+  bar:RegisterEvent("PLAYER_REGEN_ENABLED")
   bar:SetScript("OnEvent", function(self, event, ...)
-    if (event == "UPDATE_EXTRA_ACTIONBAR") then
-      if (HasExtraActionBar()) then
-        b:Show()
-      elseif(self:IsShown()) then
-        b:Hide()
-      end
+    if (HasExtraActionBar()) then
+      self:Show()
+      self.button:Show()
+    else
+      self:Hide()
     end
   end)
+
 end --disable

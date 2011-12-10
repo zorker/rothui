e28494abd1f4b609263ac04e30a3e5e99583bab8
Erik Raetz <zorker@users.noreply.github.com>
2011-12-10 14:32:56 +0000

    Way better raid display settings Added Improved Totem Frame functionality by phanx to rABS If you swap form as rogue/warlock now you ALWAYS get the new bar. Just put buttons in. Done.

--- rActionBarStyler/bars/bar1.lua
+++ rActionBarStyler/bars/bar1.lua
@@ -35,8 +35,8 @@
     ["DRUID"] = "[bonusbar:1,nostealth] 7; [bonusbar:1,stealth] 8; [bonusbar:2] 8; [bonusbar:3] 9; [bonusbar:4] 10;",
     ["WARRIOR"] = "[bonusbar:1] 7; [bonusbar:2] 8; [bonusbar:3] 9;",
     ["PRIEST"] = "[bonusbar:1] 7;",
-    ["ROGUE"] = "[bonusbar:1] 7; [form:3] 7;",
-    --["WARLOCK"] = "[form:2] 7;",
+    ["ROGUE"] = "[bonusbar:1] 7; [form:3] 10;",
+    ["WARLOCK"] = "[form:2] 7;",
     ["DEFAULT"] = "[bonusbar:5] 11; [bar:2] 2; [bar:3] 3; [bar:4] 4; [bar:5] 5; [bar:6] 6;",
   }
 

48d57f2c00706751ed1dffcbb96aab06adc78652
Erik Raetz <zorker@users.noreply.github.com>
2012-03-12 10:20:05 +0000

    remove stuff

--- blog/01_simple_mod/myFirstMod/core.lua
+++
@@ -1,3 +0,0 @@
-
-  --say Hello World!
-  DEFAULT_CHAT_FRAME:AddMessage("core.lua: Hello World!")
--- blog/01_simple_mod/myFirstMod/core2.lua
+++
@@ -1,8 +0,0 @@
-
-  -- Hello World function
-  local function am(text)
-    DEFAULT_CHAT_FRAME:AddMessage(text)
-  end
-  
-  -- Call the Hello World function
-  am("core2.lua: Hello World!")

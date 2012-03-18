54c595d373c326a6db5fbb135f5b9cfd44598b87
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2012-03-18 09:52:44 +0000

    Remove outdated druid settings >_<

--- trunk/oUF_Diablo/charspecific.lua
+++ trunk/oUF_Diablo/charspecific.lua
@@ -10,10 +10,10 @@
 
   --example: my healer Loral should use the raidframes for party aswell
   if cfg.playername == "Loral" and cfg.playerclass == "DRUID" then
-    cfg.units.party.show = false
+    --cfg.units.party.show = false
     --cfg.units.raid.pos = { a1 = "TOPLEFT", a2 = "TOPLEFT", af = "UIParent", x = 10, y = -10 },
-    cfg.units.raid.attributes.visibility = "custom [group:party][group:raid] show;hide"
-    cfg.units.raid.attributes.showPlayer = true
-    cfg.units.raid.attributes.showParty = true
+    --cfg.units.raid.attributes.visibility = "custom [group:party][group:raid] show;hide"
+    --cfg.units.raid.attributes.showPlayer = true
+    --cfg.units.raid.attributes.showParty = true
     cfg.units.focus.auras.showBuffs = false
   end

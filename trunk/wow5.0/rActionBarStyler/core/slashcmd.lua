0f114a2138278d1d326a34bf2284f7d1ed0f58e2
erik.raetz@googlemail.com <erik.raetz@googlemail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2012-07-03 09:28:59 +0000

    - added global function rCreateSlashCmdFunction

--- trunk/wow5.0/rActionBarStyler/core/slashcmd.lua
+++ trunk/wow5.0/rActionBarStyler/core/slashcmd.lua
@@ -6,32 +6,22 @@
   --addon namespace
   local addon, ns = ...
 
-  --create a list of dragable frames for the addon
+  --variables
   local dragFrameList = {}
-  --make the list (object reference) available in the namespace for later usage
-  ns.dragFrameList = dragFrameList
+  local color         = "0000FF00"
+  local shortcut      = "rabs"
+
+  --make variables available in the namespace
+  ns.dragFrameList    = dragFrameList
+  ns.addonColor       = color
+  ns.addonShortcut    = shortcut
 
   -----------------------------
   -- FUNCTIONS
   -----------------------------
 
-  local function SlashCmd(cmd)
-    if (cmd:match"unlock") then
-      rUnlockAllFrames(dragFrameList, "rABS: frames unlocked")
-    elseif (cmd:match"lock") then
-      rLockAllFrames(dragFrameList, "rABS: frames locked")
-    elseif (cmd:match"reset") then
-      rResetAllFramesToDefault(dragFrameList, "rABS: frames reseted")
-    else
-      print("|c0000FF00rActionBarStyler command list:|r")
-      print("|c0000FF00\/rabs lock|r, to lock all bars")
-      print("|c0000FF00\/rabs unlock|r, to unlock all bars")
-      print("|c0000FF00\/rabs reset|r, to reset all bars")
-    end
-  end
+  SlashCmdList[shortcut] = rCreateSlashCmdFunction(addon, shortcut, dragFrameList, color)
+  SLASH_rabs1 = "/"..shortcut; --the value in the between SLASH_ and NUMBER has to match the value of shortcut
 
-  SlashCmdList["rabs"] = SlashCmd;
-  SLASH_rabs1 = "/rabs";
-
-  print("|c0000FF00rActionBarStyler loaded.|r")
-  print("|c0000FF00\/rabs|r to display the command list")
+  print("|c"..color..addon.." loaded.|r")
+  print("|c"..color.."\/"..shortcut.."|r to display the command list")

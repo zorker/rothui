5665bab6eddd468bb3c096f63cd0c40ac00e712b
Erik Raetz <zorker@users.noreply.github.com>
2012-07-03 09:28:59 +0000

    - added global function rCreateSlashCmdFunction

---
+++ wow5.0/rLib/core/slashcmd.lua
@@ -0,0 +1,31 @@
+
+  -- // slashcmd functionality
+  -- // zork - 2012
+
+  -----------------------------
+  -- GLOBAL FUNCTIONS
+  -----------------------------
+
+  --add some global functions
+
+  --rCreateSlashCmdFunction func
+  function rCreateSlashCmdFunction(addon, shortcut, dragFrameList, color)
+    if not addon or not shortcut or not dragFrameList then return end
+    local defaultColor = "00FFFFFF"
+    local slashCmdFunction = function(cmd)
+      if (cmd:match"unlock") then
+        rUnlockAllFrames(dragFrameList, addon..": frames unlocked")
+      elseif (cmd:match"lock") then
+        rLockAllFrames(dragFrameList, addon..": frames locked")
+      elseif (cmd:match"reset") then
+        rResetAllFramesToDefault(dragFrameList, addon..": frames reseted")
+      else
+        print("|c"..color or defaultColor..addon.." command list:|r")
+        print("|c"..color or defaultColor.."\/"..shortcut.." lock|r, to lock all frames")
+        print("|c"..color or defaultColor.."\/"..shortcut.." unlock|r, to unlock all frames")
+        print("|c"..color or defaultColor.."\/"..shortcut.." reset|r, to reset all frames")
+      end
+    end
+    return slashCmdFunction
+  end
+

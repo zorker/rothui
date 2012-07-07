fdf74833ee22e70cb6343d2fe3b34d4f357b7e29
Erik Raetz <zorker@users.noreply.github.com>
2012-07-07 14:44:34 +0000

    - Copies some fader functionally to local. Blizzard is using :Show in the default fader which is prohibited in combat and actually not needed

--- wow5.0/rLib/core/fader.lua
+++ wow5.0/rLib/core/fader.lua
@@ -7,12 +7,77 @@
   -----------------------------
 
   --add some global functions
-
   local defaultFadeIn   = {time = 0.4, alpha = 1}
   local defaultFadeOut  = {time = 0.3, alpha = 0}
 
+  local frameFadeManager = CreateFrame("FRAME");
+
+  -- Generic fade function
+  local function UIFrameFade(frame, fadeInfo)
+    if (not frame) then
+      return;
+    end
+    if ( not fadeInfo.mode ) then
+      fadeInfo.mode = "IN";
+    end
+    local alpha;
+    if ( fadeInfo.mode == "IN" ) then
+      if ( not fadeInfo.startAlpha ) then
+        fadeInfo.startAlpha = 0;
+      end
+      if ( not fadeInfo.endAlpha ) then
+        fadeInfo.endAlpha = 1.0;
+      end
+      alpha = 0;
+    elseif ( fadeInfo.mode == "OUT" ) then
+      if ( not fadeInfo.startAlpha ) then
+        fadeInfo.startAlpha = 1.0;
+      end
+      if ( not fadeInfo.endAlpha ) then
+        fadeInfo.endAlpha = 0;
+      end
+      alpha = 1.0;
+    end
+    frame:SetAlpha(fadeInfo.startAlpha);
+
+    frame.fadeInfo = fadeInfo;
+
+    local index = 1;
+    while FADEFRAMES[index] do
+      -- If frame is already set to fade then return
+      if ( FADEFRAMES[index] == frame ) then
+        return;
+      end
+      index = index + 1;
+    end
+    tinsert(FADEFRAMES, frame);
+    frameFadeManager:SetScript("OnUpdate", UIFrameFade_OnUpdate);
+  end
+
+  -- Convenience function to do a simple fade in
+  local function UIFrameFadeIn(frame, timeToFade, startAlpha, endAlpha)
+    local fadeInfo = {};
+    fadeInfo.mode = "IN";
+    fadeInfo.timeToFade = timeToFade;
+    fadeInfo.startAlpha = startAlpha;
+    fadeInfo.endAlpha = endAlpha;
+    UIFrameFade(frame, fadeInfo);
+  end
+
+  -- Convenience function to do a simple fade out
+  local function UIFrameFadeOut(frame, timeToFade, startAlpha, endAlpha)
+    local fadeInfo = {};
+    fadeInfo.mode = "OUT";
+    fadeInfo.timeToFade = timeToFade;
+    fadeInfo.startAlpha = startAlpha;
+    fadeInfo.endAlpha = endAlpha;
+    UIFrameFade(frame, fadeInfo);
+  end
+
+
   --rButtonBarFader func
   function rButtonBarFader(frame,buttonList,fadeIn,fadeOut)
+    --if 1 == 1 then return end
     if not frame or not buttonList then return end
     if not fadeIn then fadeIn = defaultFadeIn end
     if not fadeOut then fadeOut = defaultFadeOut end
@@ -30,6 +95,7 @@
 
   --rFrameFader func
   function rFrameFader(frame,fadeIn,fadeOut)
+    --if 1 == 1 then return end
     if not frame then return end
     if not fadeIn then fadeIn = defaultFadeIn end
     if not fadeOut then fadeOut = defaultFadeOut end

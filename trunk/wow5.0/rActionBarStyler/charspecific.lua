49eb4f2720bce0e8ed33d7df30a7fac5a34c930b
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2012-07-08 12:55:15 +0000

    personal settings

--- trunk/wow5.0/rActionBarStyler/charspecific.lua
+++ trunk/wow5.0/rActionBarStyler/charspecific.lua
@@ -14,6 +14,12 @@
   local playername, _ = UnitName("player")
   local _, playerclass = UnitClass("player")
 
-  if playername == "Rothar" or playername == "Wolowizard" or playername == "Loral" then
-    cfg.bars.stancebar.enable = false
+  if playername == "Rothor" or playername == "Wolowizard" or playername == "Loral" then
+    cfg.bars.stancebar.enable = true --disable stance bar completly
+    cfg.bars.bar2.mouseover.enable = false
+    --cfg.bars.bar3.mouseover.enable = false
+    cfg.bars.bar4.mouseover.fadeOut.alpha = 0.5
+    cfg.bars.bar4.mouseover.fadeOut.alpha = 0
+    cfg.bars.bags.mouseover.fadeOut.alpha = 0
+    cfg.bars.micromenu.mouseover.fadeOut.alpha = 0
   end

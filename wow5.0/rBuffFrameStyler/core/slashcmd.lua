9d570bf5759013df9ac726010bac1d3a63246a44
Erik Raetz <zorker@users.noreply.github.com>
2012-08-05 16:21:47 +0000

    Updated the version and changed the slash command function call to bring it more in line with the other addons

--- wow5.0/rBuffFrameStyler/core/slashcmd.lua
+++ wow5.0/rBuffFrameStyler/core/slashcmd.lua
@@ -9,7 +9,7 @@
   --variables
   local dragFrameList = {}
   local color         = "0000FFFF"
-  local shortcut      = "rbuff"
+  local shortcut      = "rbfs"
 
   --make variables available in the namespace
   ns.dragFrameList    = dragFrameList
@@ -21,7 +21,7 @@
   -----------------------------
 
   SlashCmdList[shortcut] = rCreateSlashCmdFunction(addon, shortcut, dragFrameList, color)
-  SLASH_rbuff1 = "/"..shortcut; --the value in the between SLASH_ and NUMBER has to match the value of shortcut
+  SLASH_rbfs1 = "/"..shortcut; --the value in the between SLASH_ and NUMBER has to match the value of shortcut
 
   print("|c"..color..addon.." loaded.|r")
   print("|c"..color.."\/"..shortcut.."|r to display the command list")

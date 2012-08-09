5cc3a10b3d771a63ed165d667f866f07576b45b1
erik.raetz@googlemail.com <erik.raetz@googlemail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2012-08-09 13:23:04 +0000

    Some corrections for later

--- trunk/wow5.0/DiscoKugel/core.lua
+++ trunk/wow5.0/DiscoKugel/core.lua
@@ -3,29 +3,28 @@
   -- // zork - 2010
 
   --get the addon namespace
-  local addon, ns = ...  
-  
+  local addon, ns = ...
+
   --get the config
   local cfg = ns.cfg
-  
+
   local list = cfg.displayIdList
   local numList = # list
 
   local texList = cfg.textureList
   local numTexList = # texList
-  
+
   -----------------------------
   -- FUNCTIONS
-  ----------------------------- 
-  
+  -----------------------------
+
   --round some stuff
-  local function rIMV_roundNumber(n)
-    return floor((n)*10)/10
+  local function roundNumber(v)
+    return floor((v)*1000)/1000
   end
 
-   
   --change portraitZoom func
-  local function rIMV_changeModelPortraitZoom(self, delta)
+  local function changeModelPortraitZoom(self, delta)
     local maxzoom = 1
     local minzoom = -0.5
     self.zoomLevel = self.zoomLevel + delta*0.15
@@ -35,69 +34,69 @@
     if (minzoom > self.zoomLevel) then
         self.zoomLevel = minzoom
     end
-    self.zoomLeve = rIMV_roundNumber(self.zoomLevel)
+    self.zoomLevel = roundNumber(self.zoomLevel)
     self:SetPortraitZoom(self.zoomLevel)
   end
-    
+
   --change camDistanceScale func
-  local function rIMV_changeModelDistanceScale(self, delta)
-    local maxscale = 10
+  local function changeModelDistanceScale(self, delta)
+    local maxscale = 20
     local minscale = 0.1
-    self.scaleLevel = self.scaleLevel + delta*0.15
+    self.scaleLevel = self.scaleLevel + delta*0.05
     if (self.scaleLevel > maxscale) then
         self.scaleLevel = maxscale
     end
     if (minscale > self.scaleLevel) then
         self.scaleLevel = minscale
     end
-    self.scaleLevel = rIMV_roundNumber(self.scaleLevel)
+    self.scaleLevel = roundNumber(self.scaleLevel)
     self:SetCamDistanceScale(self.scaleLevel)
   end
-  
+
   --move model left right func
-  local function rIMV_moveModelLeftRight(self, delta)
-    local max = 10
-    local min = -10
-    self.posX = self.posX + delta*0.15
+  local function moveModelLeftRight(self, delta)
+    local max = 20
+    local min = -20
+    self.posX = self.posX + delta*0.05
     if (self.posX > max) then
         self.posX = max
     end
     if (min > self.posX) then
         self.posX = min
     end
-    self.posX = rIMV_roundNumber(self.posX)
+    self.posX = roundNumber(self.posX)
     self:SetPosition(0,self.posX,self.posY)
   end
-  
+
   --move model top bottom func
-  local function rIMV_moveModelTopBottom(self, delta)
-    local max = 10
-    local min = -10
-    self.posY = self.posY + delta*0.15
+  local function moveModelTopBottom(self, delta)
+    local max = 20
+    local min = -20
+    self.posY = self.posY + delta*0.05
     if (self.posY > max) then
         self.posY = max
     end
     if (min > self.posY) then
         self.posY = min
     end
-    self.posY = rIMV_roundNumber(self.posY)
+    self.posY = roundNumber(self.posY)
     self:SetPosition(0,self.posX,self.posY)
   end
-  
+
   --model rotation func
-  local function rIMV_rotateModel(self,button)
+  local function rotateModel(self,button)
     local rotationIncrement = 0.2
     if button == "LeftButton" then
       self.rotation = self.rotation - rotationIncrement
     else
       self.rotation = self.rotation + rotationIncrement
     end
-    self.rotation = rIMV_roundNumber(self.rotation)
+    self.rotation = roundNumber(self.rotation)
     self:SetRotation(self.rotation)
   end
-  
+
   --tooltip for model func
-  local function rIMV_showModelTooltip(self,theatre)
+  local function showModelTooltip(self,theatre)
     GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
     --GameTooltip:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -90, 90)
     if not theatre then
@@ -106,158 +105,144 @@
       GameTooltip:AddLine("Theatre View", 0, 1, 0.5, 1, 1, 1)
     end
     local r,g,b = self:GetParent().fill:GetVertexColor()
-    r = math.floor(r*100)/100
-    g = math.floor(g*100)/100
-    b = math.floor(b*100)/100
-    local colorStr = "R "..r.." G "..g.." B "..b
+    local colorStr = "R "..roundNumber(r).." G "..roundNumber(g).." B "..roundNumber(b)
 
-    GameTooltip:AddLine("~ model ~~~~~")
+    GameTooltip:AddLine("-- MODEL --------------------------------------------")
     GameTooltip:AddDoubleLine("DisplayID", self.id, 1, 1, 1, 1, 1, 1)
     GameTooltip:AddDoubleLine("SetCamDistanceScale", self.scaleLevel, 1, 1, 1, 1, 1, 1)
-    GameTooltip:AddDoubleLine("SetPortraitZoom", self.zoomLevel, 1, 1, 1, 1, 1, 1)    
+    GameTooltip:AddDoubleLine("SetPortraitZoom", self.zoomLevel, 1, 1, 1, 1, 1, 1)
     GameTooltip:AddDoubleLine("SetPosition", "(0,"..self.posX..","..self.posY..")", 1, 1, 1, 1, 1, 1)
     GameTooltip:AddDoubleLine("SetRotation", self.rotation, 1, 1, 1, 1, 1, 1)
     GameTooltip:AddDoubleLine("GetModel", self:GetModel(), 1, 1, 1, 1, 1, 1)
-    GameTooltip:AddLine("~ texture ~~~~~")
+    GameTooltip:AddLine("-- FILLING TEXTURE ----------------------------------")
     GameTooltip:AddDoubleLine("FillingColor", colorStr, 1, 1, 1, 1, 1, 1)
     GameTooltip:AddDoubleLine("FillingTexture", self:GetParent().fill:GetTexture(), 1, 1, 1, 1, 1, 1)
-    GameTooltip:AddLine("~ left mouse ~~~~~")
-    GameTooltip:AddLine("LeftMouse = NEW COLOR")
-    GameTooltip:AddLine("SHIFT + LeftMouse = RESET")
-    GameTooltip:AddLine("ALT + LeftMouse = ROTATE MODEL LEFT")
-    GameTooltip:AddLine("~ right mouse ~~~~~")
-    GameTooltip:AddLine("RightMouse = CYCLE FILLING TEXTURES")
-    GameTooltip:AddLine("SHIFT + RightMouse + DRAG = MOVE")
-    GameTooltip:AddLine("ALT + RightMouse + DRAG = RESIZE")
-    GameTooltip:AddLine("~ buttons ~~~~~")
-    GameTooltip:AddLine("MouseButton4 = NEXT MODEL ANIMATION")
-    GameTooltip:AddLine("MouseButton5 = PREVIOUS MODEL ANIMATION")
-    GameTooltip:AddLine("~ wheel ~~~~~")
-    GameTooltip:AddLine("MouseWheel = SetCamDistanceScale")
-    GameTooltip:AddLine("ALT + MouseWheel Y-AXIS")
-    GameTooltip:AddLine("SHIFT + MouseWheel = X-AXIS")
-    GameTooltip:AddLine("ALT + SHIFT + MouseWheel = SetPortraitZoom")
+    GameTooltip:AddLine("-- LEFT MOUSE ---------------------------------------")
+    GameTooltip:AddLine("LeftMouse                  = COLOR PICKER -----------")
+    GameTooltip:AddLine("SHIFT + LeftMouse          = RESET ------------------")
+    GameTooltip:AddLine("ALT + LeftMouse            = ROTATE MODEL LEFT ------")
+    GameTooltip:AddLine("-- RIGHT MOUSE --------------------------------------")
+    GameTooltip:AddLine("RightMouse                 = CYCLE FILLING TEXTURES -")
+    GameTooltip:AddLine("SHIFT + RightMouse + DRAG  = MOVE -------------------")
+    GameTooltip:AddLine("ALT + RightMouse + DRAG    = RESIZE -----------------")
+    GameTooltip:AddLine("-- OTHER BUTTONS ------------------------------------")
+    GameTooltip:AddLine("MouseButton4               = NEXT MODEL ANIMATION ---")
+    GameTooltip:AddLine("MouseButton5               = PREVIOUS MODEL ANIMATION")
+    GameTooltip:AddLine("-- MOUSE WHEEL --------------------------------------")
+    GameTooltip:AddLine("MouseWheel                 = SetCamDistanceScale ----")
+    GameTooltip:AddLine("ALT + MouseWheel           = Y-AXIS -----------------")
+    GameTooltip:AddLine("SHIFT + MouseWheel         = X-AXIS -----------------")
+    GameTooltip:AddLine("ALT + SHIFT + MouseWheel   = SetPortraitZoom --------")
     GameTooltip:Show()
   end
-  
-  local function rIMV_setModelValues(self)
-  
+
+  local function loadDefaultModelValues(self)
     self.scaleLevel = 1
     self.zoomLevel = 1
     self.posX = 0
     self.posY = 0
     self.rotation = 0
-    
     self:SetPortraitZoom(self.zoomLevel)
     self:SetCamDistanceScale(self.scaleLevel)
     self:SetPosition(0,self.posX,self.posY)
     self:SetRotation(self.rotation)
-    self:ClearModel()
+    self:ClearModel() --clearmodel to make sure model values reset correctly
+    self:SetModel("interface\\buttons\\talktomequestionmark.m2") --in case setdisplayinfo fails
     self:SetDisplayInfo(self.id)
   end
 
-  local function rIMV_setFillingTexture(self)
+  local function setFillingTexture(self)
     self.fill:SetTexture(texList[self.texIndex])
   end
 
-  local function rIMV_changeModel(self)  
-    --local f = self.fill
-    --f:SetVertexColor(listentry.color.r,listentry.color.g,listentry.color.b)    
+  local function changeModel(self)
     local m = self.m
-    
-    m:SetModel("interface\\buttons\\talktomequestionmark.m2") --in case setdisplayinfo fails 
     local listentry = list[self.index]
-    print("changing model displayid to: "..listentry)
-    --m:SetDisplayInfo(listentry)
     m.id = listentry
-    rIMV_setModelValues(m)  
+    loadDefaultModelValues(m)
   end
 
-  function ShowColorPicker(r, g, b, a, changedCallback)
-    ColorPickerFrame:SetColorRGB(r,g,b);
-    ColorPickerFrame.hasOpacity, ColorPickerFrame.opacity = (a ~= nil), a;
-    ColorPickerFrame.previousValues = {r,g,b,a};
-    ColorPickerFrame.func, ColorPickerFrame.opacityFunc, ColorPickerFrame.cancelFunc = 
-    changedCallback, changedCallback, changedCallback;
-    ColorPickerFrame:Hide(); -- Need to run the OnShow handler.
-    ColorPickerFrame:Show();
+  function showColorPicker(r, g, b, a, callback)
+    ColorPickerFrame:SetColorRGB(r,g,b)
+    ColorPickerFrame.hasOpacity, ColorPickerFrame.opacity = (a ~= nil), a
+    ColorPickerFrame.previousValues = {r,g,b,a}
+    ColorPickerFrame.func, ColorPickerFrame.opacityFunc, ColorPickerFrame.cancelFunc = callback, callback, callback
+    ColorPickerFrame:Hide()
+    ColorPickerFrame:Show()
   end
 
   local createOrb = function(listentry, index)
-    
+
     local f = CreateFrame("FRAME",nil,UIParent)
     f:SetFrameLevel(1)
     f:SetSize(150,150)
     f:SetPoint("CENTER",0,0)
-    
+
     f.index = index
-    
+    f.texIndex = index
+
     local bg = f:CreateTexture(nil,"BACKGROUND",nil,-8)
     bg:SetAllPoints(f)
     bg:SetTexture("Interface\\AddOns\\DiscoKugel\\media\\orb_back")
-    
+
     local fill = f:CreateTexture(nil,"BACKGROUND",nil,-7)
     fill:SetAllPoints(f)
-    f.texIndex = 1
     fill:SetTexture(texList[f.texIndex])
     fill:SetVertexColor(1,0,0)
-    
     f.fill = fill
 
-    local function myColorCallback(restore)
-      --local newR, newG, newB, newA;
-      local color = {}
-      if restore then
-        -- The user bailed, we extract the old color from the table created by ShowColorPicker.
-        color.r, color.g, color.b, color.a = unpack(restore)
-      else
-        -- Something changed
-        color.a, color.r, color.g, color.b = OpacitySliderFrame:GetValue(), ColorPickerFrame:GetColorRGB()
-      end     
-      f.fill:SetVertexColor(color.r, color.g, color.b)
-    end
-    
     local m = CreateFrame("PlayerModel", nil,f)
     m:SetAllPoints(f)
-    m:SetModel("interface\\buttons\\talktomequestionmark.m2") --in case setdisplayinfo fails 
+    m:SetModel("interface\\buttons\\talktomequestionmark.m2") --in case setdisplayinfo fails
     m:SetDisplayInfo(listentry)
     m.id = listentry
-    rIMV_setModelValues(m)
-
+    loadDefaultModelValues(m)
     f.m = m
-    
+
+    f.recolorFillingTexture = function(color)
+      local r,g,b,a
+      if color then
+        r,g,b,a = unpack(color)
+      else
+        r,g,b = ColorPickerFrame:GetColorRGB()
+        a = OpacitySliderFrame:GetValue()
+      end
+      f.fill:SetVertexColor(r,g,b)
+      f.m:SetAlpha(a)
+    end
+
     local g = CreateFrame("Frame",nil,m)
     g:SetAllPoints(f)
-    
+
     local gloss = g:CreateTexture(nil,"BACKGROUND",nil,-6)
     gloss:SetAllPoints(f)
     gloss:SetTexture("Interface\\AddOns\\DiscoKugel\\media\\orb_gloss")
-    
+
     f:SetMovable(true)
     f:SetResizable(true)
     f:SetUserPlaced(true)
     f:EnableMouse(true)
     f:RegisterForDrag("RightButton")
-    f:SetScript("OnDragStart", function(s) 
-      if IsAltKeyDown() then 
-        s:StartSizing() 
+    f:SetScript("OnDragStart", function(s)
+      if IsAltKeyDown() then
+        s:StartSizing()
       elseif IsShiftKeyDown() then
         s:StartMoving()
-      end 
+      end
     end)
-    f:SetScript("OnSizeChanged", function(s) 
+    f:SetScript("OnSizeChanged", function(s)
       if s:GetWidth() < 50 then
         s:SetWidth(50)
         s:SetHeight(50)
       end
       s:SetHeight(s:GetWidth())
     end)
-    f:SetScript("OnDragStop", function(s) 
+    f:SetScript("OnDragStop", function(s)
       if s:GetWidth() < 50 then
         s:SetWidth(50)
       end
       s:SetHeight(s:GetWidth())
-      s:StopMovingOrSizing() 
+      s:StopMovingOrSizing()
     end)
     f:SetScript("OnMouseDown", function(s,bu,...)
       if bu == "Button4" then
@@ -265,63 +250,60 @@
         if not list[s.index] then
           s.index = 1
         end
-        rIMV_changeModel(s)
+        changeModel(s)
       end
       if bu == "Button5" then
         s.index = s.index - 1
         if not list[s.index] then
           s.index = numList
         end
-        rIMV_changeModel(s)
+        changeModel(s)
       end
       if bu == "RightButton" then
         if IsAltKeyDown() or IsShiftKeyDown() then return end
-        --rIMV_changeModel(s)
+        --changeModel(s)
         --change Texture func
         s.texIndex = s.texIndex + 1
         if not texList[s.texIndex] then
           s.texIndex = 1
         end
-        rIMV_setFillingTexture(s)
+        setFillingTexture(s)
       end
       if bu == "LeftButton" then
         if IsShiftKeyDown() then
-          rIMV_setModelValues(s.m)
+          loadDefaultModelValues(s.m)
         elseif IsAltKeyDown() then
-          rIMV_rotateModel(s.m,bu)
+          rotateModel(s.m,bu)
         else
-          local color = {}
-          color.r, color.g, color.b = s.fill:GetVertexColor()
-          ShowColorPicker(color.r, color.g, color.b, 1, myColorCallback);
-          --rIMV_changeModel(s)
+          local r,g,b = s.fill:GetVertexColor()
+          local a = s.m:GetAlpha()
+          showColorPicker(r,g,b,a,s.recolorFillingTexture)
         end
       end
-    end)    
+    end)
     f:SetScript("OnMouseWheel", function(s,d,...)
       if IsShiftKeyDown() and IsAltKeyDown() then
-        rIMV_changeModelPortraitZoom(s.m,d)        
+        changeModelPortraitZoom(s.m,d)
       elseif IsAltKeyDown() then
-        rIMV_moveModelTopBottom(s.m,d)
+        moveModelTopBottom(s.m,d)
       elseif IsShiftKeyDown() then
-        rIMV_moveModelLeftRight(s.m,d)
+        moveModelLeftRight(s.m,d)
       else
-        rIMV_changeModelDistanceScale(s.m,d)
+        changeModelDistanceScale(s.m,d)
       end
     end)
-    
-    f:SetScript("OnEnter", function(s) rIMV_showModelTooltip(s.m) end)
+    f:SetScript("OnEnter", function(s) showModelTooltip(s.m) end)
     f:SetScript("OnLeave", function(s) GameTooltip:Hide() end)
-  
   end
 
-  
-  
+
+
   -----------------------------
   -- CALL
-  ----------------------------- 
+  -----------------------------
 
   if list[1] then
     createOrb(list[1],1)
   end
 
-  
+

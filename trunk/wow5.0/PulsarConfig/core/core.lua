afacbfbfa044d938ce445ebc3d93a72bce279a53
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2012-08-10 17:32:14 +0000

    Added Pulsar This mod has only one purpose. Teaching myself how to create a GUI config for orb unit frames.

---
+++ trunk/wow5.0/PulsarConfig/core/core.lua
@@ -0,0 +1,11 @@
+
+  -- // PulsarGUI
+  -- // zork - 2012
+
+  --get the addon namespace
+  local addon, ns = ...
+  --config
+  local cfg = ns.cfg
+  --PulsarGUI
+  ns.PulsarGUI = {}
+  local PulsarGUI = ns.PulsarGUI

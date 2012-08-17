fc40d36887cbcae821086360472eb3c45f091754
Erik Raetz <zorker@users.noreply.github.com>
2012-08-17 18:47:37 +0000

    Added loader for the config menu Added a first config panel Added a slash command to open the config panel Config loads on demand

--- wow5.0/PulsarConfig/core/core.lua
+++ wow5.0/PulsarConfig/core/core.lua
@@ -1,11 +1,20 @@
 
-  -- // PulsarGUI
+  -- // Pulsar GUI config
   -- // zork - 2012
 
   --get the addon namespace
   local addon, ns = ...
-  --config
-  local cfg = ns.cfg
-  --PulsarGUI
-  ns.PulsarGUI = {}
-  local PulsarGUI = ns.PulsarGUI
+
+  local PulsarConfig = PulsarConfig or CreateFrame("Frame", "PulsarConfig", UIParent)
+  PulsarConfig:Hide()
+
+
+  local function initPanel()
+    local mainpanel = CreateFrame("FRAME")
+    mainpanel.name = "Pulsar"
+    InterfaceOptions_AddCategory(mainpanel)
+    print(addon.." loaded")
+    return mainpanel
+  end
+
+  PulsarConfig.mainpanel = initPanel()

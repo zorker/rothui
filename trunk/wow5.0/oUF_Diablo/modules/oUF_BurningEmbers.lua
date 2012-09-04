ac2e70d61ae2e0e4d6aa2915c8ab1b8b2d6faa95
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2012-09-04 21:06:16 +0000

    Added ComboBarFader to BurningEmber, SoulShard and DemonicFury (disabled by default) Hide BurningEmber, SoulShard and DemonicFury when the OverrideBar is present

--- trunk/wow5.0/oUF_Diablo/modules/oUF_BurningEmbers.lua
+++ trunk/wow5.0/oUF_Diablo/modules/oUF_BurningEmbers.lua
@@ -18,12 +18,14 @@ local Update = function(self, event, unit, powerType)
   local max2 = UnitPowerMax(unit, SPELL_POWER_BURNING_EMBERS, true)
   local val = cur2-cur*MAX_POWER_PER_EMBER
 
+  --[[ --do not hide the bar when the value is empty, keep it visible
   if cur2 < 1 then
     if bar:IsShown() then bar:Hide() end
     return
   else
     if not bar:IsShown() then bar:Show() end
   end
+  ]]
 
   --adjust the width of the burning ember power frame
   local w = 64*(max+2)
@@ -45,8 +47,10 @@ local Update = function(self, event, unit, powerType)
         orb:SetStatusBarColor(1,0,0)
         orb.glow:SetVertexColor(1,0,0)
       else
-        orb:SetStatusBarColor(bar.color.r,bar.color.g,bar.color.b)
-        orb.glow:SetVertexColor(bar.color.r,bar.color.g,bar.color.b)
+        orb:SetStatusBarColor(0,1,0)
+        orb.glow:SetVertexColor(0,1,0)
+        --orb:SetStatusBarColor(bar.color.r,bar.color.g,bar.color.b)
+        --orb.glow:SetVertexColor(bar.color.r,bar.color.g,bar.color.b)
       end
       orb.glow:Show()
       orb:SetValue(MAX_POWER_PER_EMBER)
@@ -67,7 +71,12 @@ end
 local Visibility = function(self, event, unit)
   local element = self.BurningEmbers
   local bar = self.BurningEmberPowerBar
-  if(GetSpecialization() == SPEC_WARLOCK_DESTRUCTION) then
+  if UnitHasVehicleUI("player")
+    or ((HasVehicleActionBar() and UnitVehicleSkin("player") and UnitVehicleSkin("player") ~= "")
+    or (HasOverrideActionBar() and GetOverrideBarSkin() and GetOverrideBarSkin() ~= ""))
+  then
+    bar:Hide()
+  elseif(GetSpecialization() == SPEC_WARLOCK_DESTRUCTION) then
     bar:Show()
     element.ForceUpdate(element)
   else
@@ -93,6 +102,11 @@ local function Enable(self)
     self:RegisterEvent('UNIT_DISPLAYPOWER', Path, true)
     self:RegisterEvent('PLAYER_TALENT_UPDATE', Visibility, true)
     self:RegisterEvent("SPELLS_CHANGED", Visibility, true)
+    self:RegisterEvent("UPDATE_OVERRIDE_ACTIONBAR", Visibility, true)
+
+    local helper = CreateFrame("Frame") --this is needed...adding player_login to the visivility events doesn't do anything
+    helper:RegisterEvent("PLAYER_LOGIN")
+    helper:SetScript("OnEvent", function() Visibility(self) end)
 
     return true
   end
@@ -104,7 +118,9 @@ local function Disable(self)
     self:UnregisterEvent('UNIT_POWER', Path)
     self:UnregisterEvent('UNIT_DISPLAYPOWER', Path)
     self:UnregisterEvent('PLAYER_TALENT_UPDATE', Visibility)
+    self:UnregisterEvent('PLAYER_LOGIN', Visibility)
     self:UnregisterEvent('SPELLS_CHANGED', Visibility)
+    self:UnregisterEvent('UPDATE_OVERRIDE_ACTIONBAR', Visibility)
   end
 end
 

502ebcd02b694afd1c208d284836b786f5edea89
Erik Raetz <zorker@users.noreply.github.com>
2012-09-19 18:57:19 +0000

    rChat update

--- wow5.0/rChat/config.lua
+++ wow5.0/rChat/config.lua
@@ -15,3 +15,8 @@
   -- CONFIG
   -----------------------------
 
+  cfg.hideChatTabBackgrounds  = true
+  cfg.selectedTabColor        = {1,0.75,0}
+  cfg.selectedTabAlpha        = 1
+  cfg.notSelectedTabColor     = {0.5,0.5,0.5}
+  cfg.notSelectedTabAlpha     = 0.3

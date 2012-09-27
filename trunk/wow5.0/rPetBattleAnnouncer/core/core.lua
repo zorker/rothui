7183d4c7d99009958b2140dae1092672745b3264
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2012-09-27 06:56:41 +0000

    - added pet tooltip - added icons and strings for health, power, speed and pettype

--- trunk/wow5.0/rPetBattleAnnouncer/core/core.lua
+++ trunk/wow5.0/rPetBattleAnnouncer/core/core.lua
@@ -21,16 +21,15 @@ local qualityColorRGB = {
 }
 
 local qualityText = {
-  [1] = "poor", --gray, poor
-  [2] = "common", --white, common
-  [3] = "uncommon", --green, uncommon
-  [4] = "rare", --blue, rare
-  [5] = "epic", --purple, epic
-  [6] = "legendary", --orange, legendary
+  [1] = "Poor", --gray, poor
+  [2] = "Common", --white, common
+  [3] = "Uncommon", --green, uncommon
+  [4] = "Rare", --blue, rare
+  [5] = "Epic", --purple, epic
+  [6] = "Legend", --orange, legendary
 }
 
 local unpack = unpack
-local font = ""
 
 --calc hex color from rgb
 local function RGBPercToHex(color)
@@ -135,7 +134,7 @@ fight:SetScript("OnMouseDown", function(...)
   canvas.ag2:Play()
 end)
 fight:SetScript("OnEnter", function(...)
-  PlaySound("INTERFACESOUND_LOSTTARGETUNIT")
+  PlaySound("igCreatureAggroSelect")
 end)
 fight:SetScript("OnLeave", function(...)
   PlaySound("INTERFACESOUND_LOSTTARGETUNIT")
@@ -157,7 +156,7 @@ exit:HookScript("OnMouseDown", function(...)
   canvas.ag2:Play()
 end)
 exit:SetScript("OnEnter", function(...)
-  PlaySound("INTERFACESOUND_LOSTTARGETUNIT")
+  PlaySound("igCreatureAggroSelect")
 end)
 exit:SetScript("OnLeave", function(...)
   PlaySound("INTERFACESOUND_LOSTTARGETUNIT")
@@ -178,65 +177,187 @@ end
 local function createPortraitFrame(name,type)
   local f = CreateFrame("Frame",name,scaler)
   f:SetSize(256,256)
+  f:EnableMouse(true)
+  f:SetScript("OnEnter", function(self)
+    if not self.ownerId or not self.petIndex then return end
+    if type == "player" then
+      PetBattleUnitTooltip_Attach(PetBattlePrimaryUnitTooltip, "LEFT", self, "RIGHT", 0, 0)
+    else
+      PetBattleUnitTooltip_Attach(PetBattlePrimaryUnitTooltip, "RIGHT", self, "LEFT", 0, 0)
+    end
+    PetBattleUnitTooltip_UpdateForUnit(PetBattlePrimaryUnitTooltip, self.ownerId, self.petIndex)
+    PetBattlePrimaryUnitTooltip:Show()
+  end)
+  f:SetScript("OnLeave", function(self)
+    if(PetBattlePrimaryUnitTooltip:GetParent() == self) then
+      PetBattlePrimaryUnitTooltip:Hide()
+    end
+  end)
+
   local helper = CreateFrame("Frame",nil,f)
   helper:SetPoint("CENTER")
   helper:SetSize(256,256)
   helper:SetScale(1.1) --help rescaling the textures but keep everything centered
+  --background
   local bg = helper:CreateTexture(nil, "BACKGROUND",nil,-8)
   bg:SetSize(256,256)
   bg:SetPoint("CENTER")
   bg:SetTexture("Interface\\AddOns\\rPetBattleAnnouncer\\media\\raid_back")
+  --modell
   local model = CreateFrame("PlayerModel",nil,helper)
   model:SetSize(142,142)
   model:SetPoint("CENTER")
+
+  --gloss
   local gloss = CreateFrame("Frame",nil,model)
   gloss:SetSize(256,256)
   gloss:SetPoint("CENTER")
+  --portrait border texture
   local border = gloss:CreateTexture(nil, "BACKGROUND",nil,-8)
   border:SetSize(256,256)
   border:SetPoint("CENTER")
   border:SetTexture("Interface\\AddOns\\rPetBattleAnnouncer\\media\\raid_border")
-
+  --level bg texture
   local levelBg = gloss:CreateTexture(nil, "BACKGROUND",nil,-7)
   levelBg:SetSize(90,90)
   levelBg:SetPoint("BOTTOMLEFT",18,18)
   levelBg:SetTexture("Interface\\AddOns\\rPetBattleAnnouncer\\media\\combo_orb_bg")
+  --level border texture
   local levelBorder = gloss:CreateTexture(nil, "BACKGROUND",nil,-6)
   levelBorder:SetAllPoints(levelBg)
   levelBorder:SetTexture("Interface\\AddOns\\rPetBattleAnnouncer\\media\\combo_orb_border")
 
-  local level = gloss:CreateFontString(nil, "BACKGROUND")
-  level:SetFont("Interface\\AddOns\\rPetBattleAnnouncer\\media\\pokemon.ttf", 20, "THICKOUTLINE")
-  level:SetPoint("CENTER", levelBg, "CENTER", 2, -5)
-  level:SetJustifyH("CENTER")
+  --pet power frame
+  local petPowerFrame = CreateFrame("Frame",nil,gloss)
+  petPowerFrame:SetSize(40,40)
+  local powericon = petPowerFrame:CreateTexture(nil, "BACKGROUND",nil,-6)
+  powericon:SetSize(30,30)
+  powericon:SetTexture("Interface\\AddOns\\rPetBattleAnnouncer\\media\\sword")
+  if type == "player" then
+    petPowerFrame:SetPoint("LEFT", -10, 0)
+    powericon:SetPoint("RIGHT")
+  else
+    petPowerFrame:SetPoint("RIGHT", 10, 0)
+    powericon:SetPoint("LEFT")
+  end
+  --power fontstring
+  local power = petPowerFrame:CreateFontString(nil, "BACKGROUND")
+  power:SetFont("Interface\\AddOns\\rPetBattleAnnouncer\\media\\pokemon.ttf", 40, "THICKOUTLINE")
+  if type == "player" then
+    power:SetPoint("RIGHT", -40, -5)
+    power:SetJustifyH("RIGHT")
+  else
+    power:SetPoint("LEFT", 40, -5)
+    power:SetJustifyH("LEFT")
+  end
+  power:SetTextColor(1,0.3,0)
 
-  local name = gloss:CreateFontString(nil, "BACKGROUND")
-  name:SetFont("Interface\\AddOns\\rPetBattleAnnouncer\\media\\pokemon.ttf", 20, "THICKOUTLINE")
-  name:SetPoint("TOP", 0, -20)
-  name:SetPoint("LEFT", -40, 0)
-  name:SetPoint("RIGHT", 40, 0)
-  name:SetJustifyH("CENTER")
-
-  local health = gloss:CreateFontString(nil, "BACKGROUND")
+  --pet health frame
+  local petHealthFrame = CreateFrame("Frame",nil,gloss)
+  petHealthFrame:SetSize(40,40)
+  local healthicon = petHealthFrame:CreateTexture(nil, "BACKGROUND",nil,-6)
+  healthicon:SetSize(30,30)
+  healthicon:SetTexture("Interface\\AddOns\\rPetBattleAnnouncer\\media\\heart")
+  if type == "player" then
+    petHealthFrame:SetPoint("LEFT", -10, 55)
+    healthicon:SetPoint("RIGHT")
+  else
+    petHealthFrame:SetPoint("RIGHT", 10, 55)
+    healthicon:SetPoint("LEFT")
+  end
+  --health fontstring
+  local health = petHealthFrame:CreateFontString(nil, "BACKGROUND")
   health:SetFont("Interface\\AddOns\\rPetBattleAnnouncer\\media\\pokemon.ttf", 40, "THICKOUTLINE")
   if type == "player" then
-    health:SetPoint("LEFT", -35, 0)
+    health:SetPoint("RIGHT", -40, -5)
     health:SetJustifyH("RIGHT")
   else
-    health:SetPoint("RIGHT", 35, 0)
+    health:SetPoint("LEFT", 40, -5)
     health:SetJustifyH("LEFT")
   end
   health:SetTextColor(0,0.7,0)
 
+  --pet speed frame
+  local petSpeedFrame = CreateFrame("Frame",nil,gloss)
+  petSpeedFrame:SetSize(40,40)
+  local speedicon = petSpeedFrame:CreateTexture(nil, "BACKGROUND",nil,-6)
+  speedicon:SetSize(30,30)
+  speedicon:SetTexture("Interface\\AddOns\\rPetBattleAnnouncer\\media\\speed")
+  if type == "player" then
+    petSpeedFrame:SetPoint("LEFT", -10, -55)
+    speedicon:SetPoint("RIGHT")
+  else
+    petSpeedFrame:SetPoint("RIGHT", 10, -55)
+    speedicon:SetPoint("LEFT")
+  end
+  --speed fontstring
+  local speed = petSpeedFrame:CreateFontString(nil, "BACKGROUND")
+  speed:SetFont("Interface\\AddOns\\rPetBattleAnnouncer\\media\\pokemon.ttf", 40, "THICKOUTLINE")
+  if type == "player" then
+    speed:SetPoint("RIGHT", -40, -5)
+    speed:SetJustifyH("RIGHT")
+  else
+    speed:SetPoint("LEFT", 40, -5)
+    speed:SetJustifyH("LEFT")
+  end
+  speed:SetTextColor(1,1,0)
+
+  --pettype textures
+  --local pettypeBg = gloss:CreateTexture(nil, "BACKGROUND",nil,-7)
+  --pettypeBg:SetSize(90,90)
+  --pettypeBg:SetPoint("BOTTOMRIGHT",-18,18)
+  --pettypeBg:SetTexture("Interface\\AddOns\\rPetBattleAnnouncer\\media\\combo_orb_bg")
+  local pettypeIcon = gloss:CreateTexture(nil, "BACKGROUND",nil,-6)
+  pettypeIcon:SetSize(40,40)
+  pettypeIcon:SetPoint("TOPLEFT",60,-60)
+  pettypeIcon:SetTexture("Interface\\PetBattles\\PetIcon-Mechanical")
+  pettypeIcon:SetDesaturated(true)
+  pettypeIcon:SetAlpha(0.3)
+  pettypeIcon:SetTexCoord(0.79687500,0.49218750,0.50390625,0.65625000)
+
+  --level fontstring
+  local level = gloss:CreateFontString(nil, "BACKGROUND")
+  level:SetFont("Interface\\AddOns\\rPetBattleAnnouncer\\media\\pokemon.ttf", 20, "THICKOUTLINE")
+  level:SetPoint("CENTER", levelBg, "CENTER", 2, -5)
+  level:SetJustifyH("CENTER")
+  --name fontstring
+  local name = gloss:CreateFontString(nil, "BACKGROUND")
+  name:SetFont("Interface\\AddOns\\rPetBattleAnnouncer\\media\\pokemon.ttf", 20, "THICKOUTLINE")
+  name:SetPoint("TOP", 0, 0)
+  name:SetPoint("LEFT", -40, 0)
+  name:SetPoint("RIGHT", 40, 0)
+  name:SetJustifyH("CENTER")
+
+  local class = gloss:CreateFontString(nil, "BACKGROUND")
+  class:SetFont("Interface\\AddOns\\rPetBattleAnnouncer\\media\\pokemon.ttf", 19, "THICKOUTLINE")
+  class:SetPoint("TOP", name, "BOTTOM", 0, -5)
+  class:SetPoint("LEFT", -40, 0)
+  class:SetPoint("RIGHT", 40, 0)
+  class:SetJustifyH("CENTER")
+  class:SetText("Poor")
+
+  local empty = gloss:CreateFontString(nil, "BACKGROUND")
+  empty:SetFont("Interface\\AddOns\\rPetBattleAnnouncer\\media\\pokemon.ttf", 20, "THICKOUTLINE")
+  empty:SetPoint("CENTER")
+  empty:SetText("Empty\nslot")
+  empty:SetAlpha(0.2)
 
   f.model = model
   f.gloss = gloss
   f.border = border
   f.name = name
+  f.class = class
+  f.empty = empty
   f.levelBg = levelBg
   f.levelBorder = levelBorder
-  f.health = health
   f.level = level
+  f.health = health
+  f.healthicon = healthicon
+  f.power = power
+  f.powericon = powericon
+  f.speed = speed
+  f.speedicon = speedicon
+  f.typeicon = pettypeIcon
   f.bg = bg
   return f
 end
@@ -276,22 +397,37 @@ end
 
 local data = {
   name = nil,
+  species = nil,
+  petTypeId = nil,
+  petTypeSuffix = nil,
+  petTypeName = nil,
+  petTypeTexture = nil,
   quality = nil,
   colorRGB = nil,
   colorHEX = nil,
   displayId = nil,
   health = nil,
+  power = nil,
+  speed = nil,
   icon = nil,
   level = nil,
 }
 
 local function fillData(ownerId, i)
-  data["name"]        = C_PetBattles.GetName(ownerId, i)
+  local name, species = C_PetBattles.GetName(ownerId, i)
+  data["name"]        = name
+  data["species"]     = species
+  data["petTypeId"]   = C_PetBattles.GetPetType(ownerId, i)
+  data["petTypeSuffix"] = PET_TYPE_SUFFIX[data["petTypeId"]]
+  data["petTypeName"] = _G["BATTLE_PET_NAME_"..data["petTypeId"]]
+  data["petTypeTexture"] = "Interface\\PetBattles\\PetIcon-"..data["petTypeSuffix"]
   data["quality"]     = C_PetBattles.GetBreedQuality(ownerId, i)
   data["colorRGB"]    = qualityColorRGB[data["quality"]]
   data["colorHEX"]    = RGBPercToHex(data["colorRGB"])
   data["displayId"]   = C_PetBattles.GetDisplayID(ownerId, i)
   data["health"]      = C_PetBattles.GetHealth(ownerId, i)
+  data["power"]       = C_PetBattles.GetPower(ownerId, i)
+  data["speed"]       = C_PetBattles.GetSpeed(ownerId, i)
   data["icon"]        = C_PetBattles.GetIcon(ownerId, i)
   data["level"]       = C_PetBattles.GetLevel(ownerId, i)
 end
@@ -299,12 +435,22 @@ end
 local function loadEmptyPetPortraitFrame(frame)
     frame.bg:SetVertexColor(0.6,0.6,0.6)
     frame.border:SetVertexColor(0.6,0.6,0.6)
-    frame.name:SetText("|cffffffffEmpty slot|r")
+    frame.name:SetText("")
+    frame.class:SetText("")
     frame.health:SetText("")
+    frame.power:SetText("")
+    frame.speed:SetText("")
+    frame.empty:Show()
+    frame.healthicon:Hide()
+    frame.powericon:Hide()
+    frame.speedicon:Hide()
     frame.level:SetText("")
+    frame.typeicon:SetTexture("")
     frame.levelBorder:SetVertexColor(0.6,0.6,0.6)
     frame.levelBg:SetVertexColor(0.6,0.6,0.6)
     frame.model:ClearModel()
+    frame.ownerId = nil
+    frame.petIndex = nil
 end
 
 local function loadEmptyPetPortraitFrames()
@@ -323,11 +469,25 @@ local function getPetData(prefix,ownerId)
     fillData(ownerId, i)
     --apply data to the portrait frame
     local frame = _G[prefix..i]
+    frame.ownerId = ownerId
+    frame.petIndex = i
     frame.bg:SetVertexColor(unpack(data["colorRGB"]))
     frame.border:SetVertexColor(unpack(data["colorRGB"]))
-    frame.name:SetText(data["name"].."\n|cffffffff("..qualityText[data["quality"]]..")|r")
+    --frame.name:SetText(data["name"].."\n|cffffffff"..data["petTypeName"]..","..qualityText[data["quality"]].."|r")
+    --frame.name:SetText(data["name"].."\n|cffffffff("..qualityText[data["quality"]]..")|r")
+    frame.name:SetText(data["name"])
+    frame.class:SetText(data["petTypeName"]..", |cff"..RGBPercToHex(data["colorRGB"])..qualityText[data["quality"]].."|r")
     frame.name:SetTextColor(unpack(data["colorRGB"]))
-    frame.health:SetText(numFormat(data["health"] or 0).."\n|cff99cc99Life|r")
+    --frame.health:SetText(numFormat(data["health"] or 0).."\n|cff99cc99Life|r")
+    frame.typeicon:SetTexture(data["petTypeTexture"])
+    frame.typeicon:SetDesaturated(true)
+    frame.health:SetText(numFormat(data["health"] or 0))
+    frame.power:SetText(numFormat(data["power"] or 0))
+    frame.speed:SetText(numFormat(data["speed"] or 0))
+    frame.empty:Hide()
+    frame.healthicon:Show()
+    frame.powericon:Show()
+    frame.speedicon:Show()
     frame.level:SetText("|cffffcc00"..data["level"].."|r")
     frame.levelBorder:SetVertexColor(unpack(data["colorRGB"]))
     frame.levelBg:SetVertexColor(unpack(data["colorRGB"]))

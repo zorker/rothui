0bf57e526216927e15415eb6797edb5cbaf9e300
Erik Raetz <zorker@users.noreply.github.com>
2012-10-20 14:54:02 +0000

    Adding the new buffs/debuffs

--- wow5.0/rFilter3/charspecific.lua
+++ wow5.0/rFilter3/charspecific.lua
@@ -24,12 +24,13 @@
         spellid = 112048, --shield barrier
         spec = 3,
         size = 32,
-        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 130, y = 107 },
+        pos = { a1 = "CENTER", a2 = "CENTER", af = "UIParent", x = -160, y = 0 },
         unit = "player",
         validate_unit   = true,
         ismine          = true,
         desaturate      = true,
         move_ingame     = true,
+        hide_ooc        = false,
         show_value      = 1,
         alpha = {
           found = {
@@ -38,7 +39,7 @@
           },
           not_found = {
             frame = 0.4,
-            icon = 1,
+            icon = 0.6,
           },
         },
       },
@@ -46,12 +47,13 @@
         spellid = 132365, --vengeance
         spec = 3,
         size = 32,
-        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 130, y = 107 },
+        pos = { a1 = "CENTER", a2 = "CENTER", af = "UIParent", x = -160, y = 45 },
         unit = "player",
         validate_unit   = true,
         ismine          = true,
         desaturate      = true,
         move_ingame     = true,
+        hide_ooc        = false,
         show_value      = 1,
         alpha = {
           found = {
@@ -60,7 +62,51 @@
           },
           not_found = {
             frame = 0.4,
+            icon = 0.6,
+          },
+        },
+      },
+      [3] = {
+        spellid = 132404, --shield block
+        spec = 3,
+        size = 32,
+        pos = { a1 = "CENTER", a2 = "CENTER", af = "UIParent", x = -160, y = -45 },
+        unit = "player",
+        validate_unit   = true,
+        ismine          = true,
+        desaturate      = true,
+        move_ingame     = true,
+        hide_ooc        = false,
+        alpha = {
+          found = {
+            frame = 1,
             icon = 1,
+          },
+          not_found = {
+            frame = 0.4,
+            icon = 0.6,
+          },
+        },
+      },
+      [4] = {
+        spellid = 12880, --enrage
+        spec = 3,
+        size = 32,
+        pos = { a1 = "CENTER", a2 = "CENTER", af = "UIParent", x = -205, y = 0 },
+        unit = "player",
+        validate_unit   = true,
+        ismine          = true,
+        desaturate      = true,
+        move_ingame     = true,
+        hide_ooc        = false,
+        alpha = {
+          found = {
+            frame = 1,
+            icon = 1,
+          },
+          not_found = {
+            frame = 0.4,
+            icon = 0.6,
           },
         },
       },
@@ -68,6 +114,72 @@
 
     --Rothars Debuff List
     cfg.rf3_DebuffList = {
+      [1] = {
+        spellid = 115798, --weakened blows
+        spec = nil,
+        size = 22,
+        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = -140, y = 110 },
+        unit = "target",
+        validate_unit   = true,
+        ismine          = false,
+        desaturate      = true,
+        move_ingame     = true,
+        hide_ooc        = false,
+        alpha = {
+          found = {
+            frame = 1,
+            icon = 1,
+          },
+          not_found = {
+            frame = 0.4,
+            icon = 0.6,
+          },
+        },
+      },
+      [2] = {
+        spellid = 113746, --weakened armor
+        spec = nil,
+        size = 22,
+        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = -110, y = 110 },
+        unit = "target",
+        validate_unit   = true,
+        ismine          = false,
+        desaturate      = true,
+        move_ingame     = true,
+        hide_ooc        = false,
+        alpha = {
+          found = {
+            frame = 1,
+            icon = 1,
+          },
+          not_found = {
+            frame = 0.4,
+            icon = 0.6,
+          },
+        },
+      },
+      [3] = {
+        spellid = 81326, --physical vulnerability
+        spec = nil,
+        size = 22,
+        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = -80, y = 110 },
+        unit = "target",
+        validate_unit   = true,
+        ismine          = false,
+        desaturate      = true,
+        move_ingame     = true,
+        hide_ooc        = false,
+        alpha = {
+          found = {
+            frame = 1,
+            icon = 1,
+          },
+          not_found = {
+            frame = 0.4,
+            icon = 0.6,
+          },
+        },
+      },
     }
 
   --Rothars Cooldown List

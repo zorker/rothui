9170fdc8f380b0b7bf97df80f2d733080f371aac
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2012-12-08 13:57:04 +0000

    Haste added a petbattle handler. No need for this anymore.

--- trunk/wow5.0/oUF_Diablo/core/units.lua
+++ trunk/wow5.0/oUF_Diablo/core/units.lua
@@ -19,20 +19,4 @@
   --object container to addon namespace
   ns.unit = unit
 
-  unit:RegisterEvent("PET_BATTLE_OPENING_START")
-  unit:RegisterEvent("PET_BATTLE_CLOSE")
-  --event
-  unit:SetScript("OnEvent", function(...)
-    local self, event, arg1 = ...
-    if event == "PET_BATTLE_OPENING_START" then
-      for _, v in pairs(oUF_Diablo_Units) do
-        local f = _G[v]
-        if f then f:SetParent(unit) end
-      end
-    elseif event == "PET_BATTLE_CLOSE" then
-      for _, v in pairs(oUF_Diablo_Units) do
-        local f = _G[v]
-        if f then f:SetParent(UIParent) end
-      end
-    end
-  end)
+  

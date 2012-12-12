d509668fe4b1a8659fceb970e52755ab394b04e9
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2012-12-12 23:42:29 +0000

    My latest filter settings

--- trunk/wow5.0/rFilter3/charspecific.lua
+++ trunk/wow5.0/rFilter3/charspecific.lua
@@ -21,78 +21,10 @@
     --Rothars Buff List
     cfg.rf3_BuffList = {
       [1] = {
-        spellid = 112048, --shield barrier
-        spec = 3,
-        size = 30,
-        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 0, y = 150 },
-        unit = "player",
-        validate_unit   = true,
-        ismine          = true,
-        desaturate      = true,
-        move_ingame     = true,
-        hide_ooc        = true,
-        show_value      = 1,
-        alpha = {
-          found = {
-            frame = 1,
-            icon = 1,
-          },
-          not_found = {
-            frame = 0.2,
-            icon = 0.6,
-          },
-        },
-      },
-      [2] = {
-        spellid = 132365, --vengeance
-        spec = 3,
-        size = 30,
-        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = -40, y = 150 },
-        unit = "player",
-        validate_unit   = true,
-        ismine          = true,
-        desaturate      = true,
-        move_ingame     = true,
-        hide_ooc        = true,
-        show_value      = 1,
-        alpha = {
-          found = {
-            frame = 1,
-            icon = 1,
-          },
-          not_found = {
-            frame = 0.2,
-            icon = 0.6,
-          },
-        },
-      },
-      [3] = {
-        spellid = 132404, --shield block
-        spec = 3,
-        size = 30,
-        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 40, y = 150 },
-        unit = "player",
-        validate_unit   = true,
-        ismine          = true,
-        desaturate      = true,
-        move_ingame     = true,
-        hide_ooc        = true,
-        alpha = {
-          found = {
-            frame = 1,
-            icon = 1,
-          },
-          not_found = {
-            frame = 0.2,
-            icon = 0.6,
-          },
-        },
-      },
-      [4] = {
         spellid = 12880, --enrage
         spec = nil,
         size = 22,
-        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = -140, y = 140 },
+        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 80, y = 110 },
         unit = "player",
         validate_unit   = true,
         ismine          = true,
@@ -110,9 +42,9 @@
           },
         },
       },
-      [5] = {
-        spellid = 1126, -- +5% STR, AGI, INT
-        spelllist = { 1126, 115921, 20217, 90363, },
+      [2] = {
+        spellid = 469, -- +10% STA
+        spelllist = { 469, 90364, 6307, 21562, 103127, },
         spec = nil,
         size = 22,
         pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 140, y = 110 },
@@ -133,9 +65,9 @@
           },
         },
       },
-      [6] = {
-        spellid = 469, -- +10% STA
-        spelllist = { 469, 90364, 6307, 21562, 103127, },
+      [3] = {
+        spellid = 6673, -- +10% AP / RAP
+        spelllist = { 6673, 57330, 19506, },
         spec = nil,
         size = 22,
         pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 110, y = 110 },
@@ -156,9 +88,78 @@
           },
         },
       },
+      [4] = {
+        spellid = 112048, --shield barrier
+        spec = 3,
+        size = 30,
+        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 44, y = 150 },
+        unit = "player",
+        validate_unit   = true,
+        ismine          = true,
+        desaturate      = true,
+        move_ingame     = true,
+        hide_ooc        = true,
+        show_value      = 1,
+        alpha = {
+          found = {
+            frame = 1,
+            icon = 1,
+          },
+          not_found = {
+            frame = 0,
+            icon = 0.6,
+          },
+        },
+      },
+      [5] = {
+        spellid = 132365, --vengeance
+        spec = 3,
+        size = 30,
+        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = -44, y = 150 },
+        unit = "player",
+        validate_unit   = true,
+        ismine          = true,
+        desaturate      = true,
+        move_ingame     = true,
+        hide_ooc        = true,
+        show_value      = 1,
+        alpha = {
+          found = {
+            frame = 1,
+            icon = 1,
+          },
+          not_found = {
+            frame = 0,
+            icon = 0.6,
+          },
+        },
+      },
+      [6] = {
+        spellid = 132404, --shield block
+        spec = 3,
+        size = 36,
+        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 0, y = 147 },
+        unit = "player",
+        validate_unit   = true,
+        ismine          = true,
+        desaturate      = true,
+        move_ingame     = true,
+        hide_ooc        = true,
+        alpha = {
+          found = {
+            frame = 1,
+            icon = 1,
+          },
+          not_found = {
+            frame = 0,
+            icon = 0.6,
+          },
+        },
+      },
+      --[[
       [7] = {
-        spellid = 6673, -- +10% AP / RAP
-        spelllist = { 6673, 57330, 19506, },
+        spellid = 1126, -- +5% STR, AGI, INT
+        spelllist = { 1126, 115921, 20217, 90363, },
         spec = nil,
         size = 22,
         pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 80, y = 110 },
@@ -225,7 +226,8 @@
           },
         },
       },
-      [10] = {
+      
+      [4] = {
         spellid = 131116, --raging blow stack
         spec = 2,
         size = 40,
@@ -248,8 +250,8 @@
           },
         },
       },
-      [11] = {
-        spellid = 85739, --raging blow stack
+      [5] = {
+        spellid = 85739, --metzger
         spec = 2,
         size = 40,
         framestrata = "BACKGROUND",
@@ -271,7 +273,9 @@
           },
         },
       },
-      [12] = {
+      ]]--
+      --[[
+      [6] = {
         spellid = 125831, --taste for blood
         spec = 1,
         size = 50,
@@ -294,7 +298,7 @@
           },
         },
       },
-      [13] = {
+      [7] = {
         spellid = 85730, --deadly calm
         spec = nil,
         size = 40,
@@ -317,6 +321,7 @@
           },
         },
       },
+      ]]--
     }
 
     --Rothars Debuff List
@@ -438,10 +443,10 @@
     cfg.rf3_CooldownList = {
       [1] = {
         spellid = 118000, --dragon roar
-        --spec = 2,
+        --spec = 1,
         size = 50,
         framestrata = "LOW",
-        pos = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 0, y = 150 },
+        pos = { a1 = "CENTER", a2 = "CENTER", af = "UIParent", x = 0, y = 0 },
         desaturate      = true,
         move_ingame     = true,
         hide_ooc        = true,

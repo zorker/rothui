a98d4a0df4aa35c2a52b4549d6d1b703d309c547
erik.raetz@googlemail.com <erik.raetz@googlemail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2013-02-20 15:46:30 +0000

    Adding a templating system Uniforming the update/save/load functions

--- trunk/wow5.0/oUF_Diablo/core/panel.lua
+++ trunk/wow5.0/oUF_Diablo/core/panel.lua
@@ -17,12 +17,38 @@
 
   local db = ns.db
 
-  local unpack = unpack
-
   ---------------------------------------------
-  --FUNCTIONS
+  --PANEL SETUP
   ---------------------------------------------
 
+  --setup panel
+  do
+    --size/point
+    panel:SetSize(600,500)
+    panel:SetPoint("CENTER")
+    --title
+    panel.title = _G[addon.."ConfigPanelTitleText"]
+    panel.title:SetText(addon.." - Orb config panel")
+    --icon
+    local icon = panel:CreateTexture("$parentIcon", "OVERLAY", nil, -8)
+    icon:SetSize(60,60)
+    icon:SetPoint("TOPLEFT",-5,7)
+    --icon:SetTexture("Interface\\FriendsFrame\\Battlenet-Portrait")
+    SetPortraitTexture(icon, "player")
+    icon:SetTexCoord(0,1,0,1)
+    panel.icon = icon
+    --mouse/drag stuff
+    panel:EnableMouse(true)
+    panel:SetClampedToScreen(true)
+    panel:SetMovable(true)
+    panel:SetUserPlaced(true)
+  end
+
+  ---------------------------------------------
+  --TEMPLATE ELEMENT FUNCTIONS
+  ---------------------------------------------
+
+  --basic slider function
   local createBasicSlider = function(name, parent, title)
     local slider = CreateFrame("Slider", name, parent, "OptionsSliderTemplate")
     local editbox = CreateFrame("EditBox", "$parentEditBox", slider, "InputBoxTemplate")
@@ -51,28 +77,34 @@
   end
 
   ---------------------------------------------
-  --CREATE PANEL ELEMENTS
+  --PANEL ELEMENT FUNCTIONS
   ---------------------------------------------
 
-   local createSliderHealthOrbAnimationAlpha = function(parent)
+  --create element health orb animation alpha
+  local createSliderHealthOrbAnimationAlpha = function(parent)
     local slider = createBasicSlider(addon.."PanelHealthOrbAnimationAlpha", parent, "Animation Alpha")
-    slider:SetPoint("TOPLEFT", parent, "TOPLEFT", 20, -25)
     slider:HookScript("OnValueChanged", function(self,value)
+      --save value
       panel.saveHealthOrbAnimationAlpha(value)
+      --update orb view
+      panel.updateHealthOrbAnimationAlpha()
     end)
     return slider
   end
 
-   local createSliderPowerOrbAnimationAlpha = function(parent)
+  --create element power orb animation alpha
+  local createSliderPowerOrbAnimationAlpha = function(parent)
     local slider = createBasicSlider(addon.."PanelPowerOrbAnimationAlpha", parent, "Animation Alpha")
-    slider:SetPoint("TOPLEFT", parent, "TOPLEFT", 300, -25)
     slider:HookScript("OnValueChanged", function(self,value)
+      --save value
       panel.savePowerOrbAnimationAlpha(value)
+      --update orb view
+      panel.updatePowerOrbAnimationAlpha()
     end)
     return slider
   end
 
-  --panel dragFrame
+  --create panel drag frame
   local createPanelDragFrame = function()
     local frame = CreateFrame("Frame", "$parentDragFrame", panel)
     frame:SetHeight(22)
@@ -97,13 +129,12 @@
     return frame
   end
 
+  --create panel scroll frame
   local createPanelScrollFrame = function()
-
-    --create a scrollframe inside
     local scrollFrame = CreateFrame("ScrollFrame", "$parentScrollFrame", panel, "UIPanelScrollFrameTemplate")
     scrollFrame:SetPoint("TOPLEFT",10,-65)
     scrollFrame:SetPoint("BOTTOMRIGHT",-37,30)
-
+    --add scrollbar background textures
     local t = scrollFrame:CreateTexture(nil,"BACKGROUND",nil,-6)
     t:SetPoint("TOP",scrollFrame)
     t:SetPoint("RIGHT",scrollFrame,25.5,0)
@@ -116,82 +147,114 @@
     t2:SetTexture(1,1,1)
     t2:SetVertexColor(0,0,0,0.3)
     t2:SetAllPoints(t)
-
+    --create panel scroll child
     local scrollChild = CreateFrame("Frame",nil,ScrollFrame)
     scrollChild:SetWidth(scrollFrame:GetWidth())
     scrollChild:SetHeight(1000)
-
     scrollFrame:SetScrollChild(scrollChild)
-
     scrollFrame.scrollChild = scrollChild
     return scrollFrame
-
   end
 
-
-
   ---------------------------------------------
-  --CREATE PANEL
+  --SPAWN PANEL ELEMENTS
   ---------------------------------------------
-
-  panel:SetSize(600,500)
-  panel:SetPoint("CENTER")
-
-  --title
-  panel.title = _G[addon.."ConfigPanelTitleText"]
-  panel.title:SetText(addon.." - Orb config panel")
-
-  --icon
-  local icon = panel:CreateTexture("$parentIcon", "OVERLAY", nil, -8)
-  icon:SetSize(60,60)
-  icon:SetPoint("TOPLEFT",-5,7)
-  --icon:SetTexture("Interface\\FriendsFrame\\Battlenet-Portrait")
-  SetPortraitTexture(icon, "player")
-  icon:SetTexCoord(0,1,0,1)
-  panel.icon = icon
-
-  --make sure you cannot click through the frame
-  panel:EnableMouse(true)
-  --make sure you cannot move the panel out of the screen
-  panel:SetClampedToScreen(true)
-  --make the panel draggable
-  panel:SetMovable(true)
-  panel:SetUserPlaced(true)
 
   --panel drag frame
   panel.dragFrame = createPanelDragFrame()
   --the scroll frame
   panel.scrollFrame = createPanelScrollFrame()
 
-  --put elements on the scrollframe
-  panel.sliderHealthOrbAnimationAlpha = createSliderHealthOrbAnimationAlpha(panel.scrollFrame.scrollChild)
-  panel.sliderPowerOrbAnimationAlpha = createSliderPowerOrbAnimationAlpha(panel.scrollFrame.scrollChild)
+  --create all panel elements
+  panel.elementHealthOrbAnimationAlpha = createSliderHealthOrbAnimationAlpha(panel.scrollFrame.scrollChild)
+  panel.elementPowerOrbAnimationAlpha = createSliderPowerOrbAnimationAlpha(panel.scrollFrame.scrollChild)
 
+  --positioon all panel elements
+  panel.elementHealthOrbAnimationAlpha:SetPoint("TOPLEFT", panel.scrollFrame.scrollChild, "TOPLEFT", 20, -25)
+  panel.elementPowerOrbAnimationAlpha:SetPoint("TOPLEFT", panel.scrollFrame.scrollChild, "TOPLEFT", 300, -25)
 
   ---------------------------------------------
-  --SAVE
+  --UPDATE ORB ELEMENT VALUES
   ---------------------------------------------
 
+  --update health orb animation alpha
+  panel.updateHealthOrbAnimationAlpha = function()
+    ns.HealthOrb.model:SetAlpha(panel.loadHealthOrbAnimationAlpha())
+  end
+
+  --update power orb animation alpha
+  panel.updatePowerOrbAnimationAlpha = function()
+    ns.PowerOrb.model:SetAlpha(panel.loadPowerOrbAnimationAlpha())
+  end
+
+  --update health orb animation enable
+  panel.updateHealthOrbAnimationEnable = function()
+    if panel.loadHealthOrbAnimationEnable() then
+      ns.HealthOrb.model:Show()
+    else
+      ns.HealthOrb.model:Hide()
+    end
+  end
+
+  --update power orb animation enable
+  panel.updatePowerOrbAnimationEnable = function()
+    if panel.loadPowerOrbAnimationEnable() then
+      ns.PowerOrb.model:Show()
+    else
+      ns.PowerOrb.model:Hide()
+    end
+  end
+
+  ---------------------------------------------
+  --UPDATE PANEL ELEMENT VALUES
+  ---------------------------------------------
+
+  --update element health orb animation alpha
+  panel.updateElementHealthOrbAnimationAlpha = function()
+    panel.elementHealthOrbAnimationAlpha:SetValue(panel.loadHealthOrbAnimationAlpha())
+  end
+
+  --update element power orb animation alpha
+  panel.updateElementPowerOrbAnimationAlpha = function()
+    panel.elementPowerOrbAnimationAlpha:SetValue(panel.loadPowerOrbAnimationAlpha())
+  end
+
+  ---------------------------------------------
+  --SAVE DATA TO DATABASE
+  ---------------------------------------------
+
+  --save health orb animation alpha
   panel.saveHealthOrbAnimationAlpha = function(value)
-    ns.HealthOrb.model:SetAlpha(value)
     db.char["HEALTH"].animation.alpha = value
   end
 
+  --save power orb animation alpha
   panel.savePowerOrbAnimationAlpha = function(value)
-    ns.PowerOrb.model:SetAlpha(value)
     db.char["POWER"].animation.alpha = value
   end
 
   ---------------------------------------------
-  --LOAD
+  --LOAD DATA FROM DATABASE
   ---------------------------------------------
 
+  --load health orb animation alpha
   panel.loadHealthOrbAnimationAlpha = function()
     return db.char["HEALTH"].animation.alpha
   end
 
+  --load power orb animation alpha
   panel.loadPowerOrbAnimationAlpha = function()
     return db.char["POWER"].animation.alpha
+  end
+
+  --load health orb animation enable
+  panel.loadHealthOrbAnimationEnable = function()
+    return db.char["HEALTH"].animation.enable
+  end
+
+  --load power orb animation enable
+  panel.loadPowerOrbAnimationEnable = function()
+    return db.char["POWER"].animation.enable
   end
 
   ---------------------------------------------
@@ -199,9 +262,16 @@
   ---------------------------------------------
 
   panel.updatePanelView = function()
-    --update all panels
-    panel.sliderHealthOrbAnimationAlpha:SetValue(panel.loadHealthOrbAnimationAlpha())
-    panel.sliderPowerOrbAnimationAlpha:SetValue(panel.loadPowerOrbAnimationAlpha())
+
+    if InCombatLockdown() then return end
+
+    --update all panel elements
+
+    --update element health orb animation alpha
+    panel.updateElementHealthOrbAnimationAlpha()
+    --update element power orb animation alpha
+    panel.updateElementPowerOrbAnimationAlpha()
+
   end
 
   ---------------------------------------------
@@ -210,28 +280,19 @@
 
   panel.updateOrbView = function()
 
-    --apply all config values to the orbs
-    print("updateOrbView")
+    if InCombatLockdown() then return end
 
-    local healthOrb = ns.HealthOrb
-    local powerOrb = ns.PowerOrb
-    local healthOrbCfg = db.char["HEALTH"]
-    local powerOrbCfg = db.char["POWER"]
+    --update all orb elements
 
-    if healthOrbCfg.animation.enable then
-      healthOrb.model:Show()
-    else
-      healthOrb.model:Hide()
-    end
+    --update health orb animation alpha
+    panel.updateHealthOrbAnimationAlpha()
+    --update power orb animation alpha
+    panel.updatePowerOrbAnimationAlpha()
 
-    if powerOrbCfg.animation.enable then
-      powerOrb.model:Show()
-    else
-      powerOrb.model:Hide()
-    end
-
-    healthOrb.model:SetAlpha(panel.loadHealthOrbAnimationAlpha())
-    powerOrb.model:SetAlpha(panel.loadPowerOrbAnimationAlpha())
+    --update health orb animation enable
+    panel.updateHealthOrbAnimationEnable()
+    --update power orb animation enable
+    panel.updatePowerOrbAnimationEnable()
 
     --update panel view
     panel.updatePanelView()

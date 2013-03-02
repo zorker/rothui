cf774c90ed372e10f2fd9469cad7357aa4bff91f
Erik Raetz <zorker@users.noreply.github.com>
2013-03-02 01:47:02 +0000

    Adding some of my templates

---
+++ wow5.0/WTF/SavedVariables/oUF_Diablo.lua
@@ -0,0 +1,561 @@
+
+OUF_DIABLO_DB_GLOB = {
+	["artifact"] = {
+		["spark"] = {
+			["alpha"] = 0.9,
+		},
+		["model"] = {
+			["portraitZoom"] = 0,
+			["rotation"] = 0,
+			["displayInfo"] = 38699,
+			["camDistanceScale"] = 0.2700000107288361,
+			["enable"] = true,
+			["pos_y"] = 0,
+			["alpha"] = 1,
+			["pos_x"] = 0,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+			["hideOnFull"] = false,
+			["hideOnEmpty"] = true,
+			["alpha"] = 1,
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["color"] = {
+				["r"] = 1,
+				["g"] = 0.2549019607843137,
+				["b"] = 0,
+			},
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling16",
+		},
+		["highlight"] = {
+			["alpha"] = 1,
+		},
+	},
+	["d2mana"] = {
+		["spark"] = {
+			["alpha"] = 0.9000000357627869,
+		},
+		["model"] = {
+			["portraitZoom"] = 0,
+			["displayInfo"] = 32368,
+			["rotation"] = 0,
+			["camDistanceScale"] = 0.9500000476837158,
+			["pos_y"] = 0.1000003814697266,
+			["alpha"] = 1,
+			["pos_x"] = 0,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+			["hideOnFull"] = false,
+			["hideOnEmpty"] = true,
+			["alpha"] = 1,
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["colorAuto"] = false,
+			["color"] = {
+				["r"] = 0,
+				["g"] = 0,
+				["b"] = 1,
+			},
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling16",
+		},
+		["highlight"] = {
+			["alpha"] = 0.300000011920929,
+		},
+	},
+	["TEMPLATE_LIST"] = {
+		{
+			["value"] = "pearl",
+			["keepShownOnClick"] = false,
+			["key"] = "pearl",
+			["notCheckable"] = true,
+		}, -- [1]
+		{
+			["value"] = "d2life",
+			["keepShownOnClick"] = false,
+			["key"] = "d2life",
+			["notCheckable"] = true,
+		}, -- [2]
+		{
+			["value"] = "d2mana",
+			["keepShownOnClick"] = false,
+			["key"] = "d2mana",
+			["notCheckable"] = true,
+		}, -- [3]
+		{
+			["value"] = "planetWow",
+			["keepShownOnClick"] = false,
+			["key"] = "planetWow",
+			["notCheckable"] = true,
+		}, -- [4]
+		{
+			["value"] = "artifact",
+			["keepShownOnClick"] = false,
+			["key"] = "artifact",
+			["notCheckable"] = true,
+		}, -- [5]
+		{
+			["value"] = "sonar",
+			["keepShownOnClick"] = false,
+			["key"] = "sonar",
+			["notCheckable"] = true,
+		}, -- [6]
+		{
+			["value"] = "yellowFog",
+			["keepShownOnClick"] = false,
+			["key"] = "yellowFog",
+			["notCheckable"] = true,
+		}, -- [7]
+		{
+			["value"] = "magic",
+			["keepShownOnClick"] = false,
+			["key"] = "magic",
+			["notCheckable"] = true,
+		}, -- [8]
+		{
+			["value"] = "deepSea",
+			["keepShownOnClick"] = false,
+			["key"] = "deepSea",
+			["notCheckable"] = true,
+		}, -- [9]
+		{
+			["value"] = "rage",
+			["keepShownOnClick"] = false,
+			["key"] = "rage",
+			["notCheckable"] = true,
+		}, -- [10]
+	},
+	["magic"] = {
+		["spark"] = {
+			["alpha"] = 0.9,
+		},
+		["model"] = {
+			["portraitZoom"] = 0,
+			["rotation"] = 0,
+			["displayInfo"] = 34645,
+			["camDistanceScale"] = 1.403000116348267,
+			["enable"] = true,
+			["pos_y"] = 0.8980002403259277,
+			["alpha"] = 0.4280000329017639,
+			["pos_x"] = 0,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+			["hideOnFull"] = false,
+			["hideOnEmpty"] = true,
+			["alpha"] = 1,
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["colorAuto"] = false,
+			["color"] = {
+				["r"] = 0.8,
+				["g"] = 0.03137254901960784,
+				["b"] = 0.5607843137254902,
+			},
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling16",
+		},
+		["highlight"] = {
+			["alpha"] = 0.784000039100647,
+		},
+	},
+	["sonar"] = {
+		["spark"] = {
+			["alpha"] = 0.9,
+		},
+		["model"] = {
+			["portraitZoom"] = 0,
+			["rotation"] = 0,
+			["displayInfo"] = 42486,
+			["camDistanceScale"] = 0.8210000395774841,
+			["enable"] = true,
+			["pos_y"] = 0.1700000762939453,
+			["alpha"] = 0.331000030040741,
+			["pos_x"] = 0.008000373840332031,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+			["hideOnFull"] = false,
+			["hideOnEmpty"] = true,
+			["alpha"] = 1,
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["colorAuto"] = false,
+			["color"] = {
+				["r"] = 0.7058823529411764,
+				["g"] = 1,
+				["b"] = 0.9254901960784314,
+			},
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling14",
+		},
+		["highlight"] = {
+			["alpha"] = 0,
+		},
+	},
+	["planetWow"] = {
+		["spark"] = {
+			["alpha"] = 0.9,
+		},
+		["model"] = {
+			["portraitZoom"] = 0,
+			["rotation"] = 0,
+			["displayInfo"] = 44652,
+			["camDistanceScale"] = 0.6200000047683716,
+			["enable"] = true,
+			["pos_y"] = -0.01999998092651367,
+			["alpha"] = 0.4770000278949738,
+			["pos_x"] = 0.07000017166137695,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+			["hideOnFull"] = false,
+			["hideOnEmpty"] = true,
+			["alpha"] = 1,
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["colorAuto"] = false,
+			["color"] = {
+				["r"] = 0.5529411764705883,
+				["g"] = 0.7647058823529411,
+				["b"] = 1,
+			},
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling16",
+		},
+		["highlight"] = {
+			["alpha"] = 0.3,
+		},
+	},
+	["deepSea"] = {
+		["spark"] = {
+			["alpha"] = 0.9,
+		},
+		["model"] = {
+			["portraitZoom"] = 0,
+			["rotation"] = 0,
+			["displayInfo"] = 27393,
+			["camDistanceScale"] = 3.101999998092651,
+			["enable"] = true,
+			["pos_y"] = 1.222000122070313,
+			["alpha"] = 0.5980000495910645,
+			["pos_x"] = 0.08900022506713867,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+			["hideOnFull"] = false,
+			["hideOnEmpty"] = true,
+			["alpha"] = 1,
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["colorAuto"] = false,
+			["color"] = {
+				["r"] = 0.04705882352941176,
+				["g"] = 0.2941176470588235,
+				["b"] = 0.8,
+			},
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling16",
+		},
+		["highlight"] = {
+			["alpha"] = 0.784000039100647,
+		},
+	},
+	["d2life"] = {
+		["spark"] = {
+			["alpha"] = 0.9000000357627869,
+		},
+		["model"] = {
+			["portraitZoom"] = 0,
+			["displayInfo"] = 32368,
+			["rotation"] = 0,
+			["camDistanceScale"] = 0.9500000476837158,
+			["pos_y"] = 0.1000003814697266,
+			["alpha"] = 1,
+			["pos_x"] = 0,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+			["hideOnFull"] = false,
+			["hideOnEmpty"] = true,
+			["alpha"] = 1,
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["colorAuto"] = false,
+			["color"] = {
+				["r"] = 1,
+				["g"] = 0,
+				["b"] = 0,
+			},
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling16",
+		},
+		["highlight"] = {
+			["alpha"] = 0.300000011920929,
+		},
+	},
+	["rage"] = {
+		["spark"] = {
+			["alpha"] = 1,
+		},
+		["model"] = {
+			["portraitZoom"] = 0,
+			["rotation"] = 0,
+			["displayInfo"] = 33853,
+			["camDistanceScale"] = 0.7900000214576721,
+			["enable"] = true,
+			["pos_y"] = 0,
+			["alpha"] = 0.6010000109672546,
+			["pos_x"] = 0,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["alpha"] = 1,
+			["hideOnEmpty"] = true,
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["colorAuto"] = false,
+			["color"] = {
+				["r"] = 0.8,
+				["g"] = 0,
+				["b"] = 0,
+			},
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling16",
+		},
+		["highlight"] = {
+			["alpha"] = 0.784000039100647,
+		},
+	},
+	["pearl"] = {
+		["spark"] = {
+			["alpha"] = 0.9,
+		},
+		["model"] = {
+			["rotation"] = 0,
+			["portraitZoom"] = 0,
+			["displayInfo"] = 32368,
+			["camDistanceScale"] = 0.95,
+			["enable"] = true,
+			["pos_y"] = 0.1,
+			["alpha"] = 1,
+			["pos_x"] = 0,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+			["hideOnFull"] = false,
+			["hideOnEmpty"] = true,
+			["alpha"] = 1,
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["color"] = {
+				["r"] = 0.8,
+				["g"] = 0.8,
+				["b"] = 1,
+			},
+			["colorAuto"] = false,
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling15",
+		},
+		["highlight"] = {
+			["alpha"] = 0.3,
+		},
+	},
+	["yellowFog"] = {
+		["spark"] = {
+			["alpha"] = 0.9,
+		},
+		["model"] = {
+			["portraitZoom"] = 0,
+			["rotation"] = 0,
+			["displayInfo"] = 17286,
+			["camDistanceScale"] = 1.986000180244446,
+			["enable"] = true,
+			["pos_y"] = -0.4779996871948242,
+			["alpha"] = 1,
+			["pos_x"] = 0.008000373840332031,
+		},
+		["value"] = {
+			["top"] = {
+				["color"] = {
+					["r"] = 1,
+					["g"] = 1,
+					["b"] = 1,
+				},
+				["tag"] = "topdef",
+			},
+			["bottom"] = {
+				["color"] = {
+					["r"] = 0.8,
+					["g"] = 0.8,
+					["b"] = 0.8,
+				},
+				["tag"] = "botdef",
+			},
+			["hideOnFull"] = false,
+			["hideOnEmpty"] = true,
+			["alpha"] = 1,
+		},
+		["galaxies"] = {
+		},
+		["filling"] = {
+			["colorAuto"] = false,
+			["color"] = {
+				["r"] = 1,
+				["g"] = 0.6901960784313725,
+				["b"] = 0,
+			},
+			["texture"] = "Interface\\AddOns\\oUF_Diablo\\media\\orb_filling16",
+		},
+		["highlight"] = {
+			["alpha"] = 0.7680000066757202,
+		},
+	},
+}

0bcaead20006f817f263e65d8ae929071c4d8789
erik.raetz@googlemail.com <erik.raetz@googlemail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2013-04-10 12:40:47 +0000

    - Code cleanup - Merged the world marker set/clear buttons

--- trunk/wow5.0/rRaidManager/core.lua
+++ trunk/wow5.0/rRaidManager/core.lua
@@ -1,7 +1,20 @@
 
+  ---------------------------------------------
+  --  rRaidManager
+  ---------------------------------------------
+
+  --  A simple raid manager
+  --  zork - 2013
+
+  ---------------------------
+  -- NAMESPACE
+  ---------------------------
+
   local addon, ns = ...
 
-  --stuff
+  ---------------------------
+  -- VARIABLES
+  ---------------------------
 
   local tinsert = tinsert
   local format = format
@@ -22,16 +35,21 @@
     insets = { left = 4, right = 4, top = 4, bottom = 4 },
   }
 
-  local manager = CF("Frame", addon, UIP)
-  manager:SetSize(200,270)
-  manager:SetPoint("TOPLEFT", -185, -180)
-  manager:SetAlpha(0.4)
-  manager:SetBackdrop(backdrop)
-  manager:SetBackdropColor(0.1,0.1,0.1,0.9)
-  manager:SetBackdropBorderColor(0.7,0.7,0.7)
+  local TEX_WORLD_RAID_MARKERS = {}
+  tinsert(TEX_WORLD_RAID_MARKERS, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_6:14:14|t")
+  tinsert(TEX_WORLD_RAID_MARKERS, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_4:14:14|t")
+  tinsert(TEX_WORLD_RAID_MARKERS, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_3:14:14|t")
+  tinsert(TEX_WORLD_RAID_MARKERS, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_7:14:14|t")
+  tinsert(TEX_WORLD_RAID_MARKERS, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_1:14:14|t")
+
+  local previousButton
+
+  ---------------------------
+  -- FUNCTIONS
+  ---------------------------
 
   --basic button func
-  local createBasicButton = function(parent, name, text, tooltipText)
+  local function CreateBasicButton(parent, name, text, tooltipText)
     local button = CF("Button", name, parent, "SecureActionButtonTemplate, UIPanelButtonTemplate")
     button.text = _G[button:GetName().."Text"]
     button.text:SetText(text)
@@ -46,88 +64,14 @@
     return button
   end
 
-  local world_marker_textures = {}
-  tinsert(world_marker_textures, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_6:14:14|t")
-  tinsert(world_marker_textures, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_4:14:14|t")
-  tinsert(world_marker_textures, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_3:14:14|t")
-  tinsert(world_marker_textures, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_7:14:14|t")
-  tinsert(world_marker_textures, "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_1:14:14|t")
-
-  local previousButton
-  for i=1, NUM_WORLD_RAID_MARKERS do
-    local text = world_marker_textures[i]
-    local button = createBasicButton(manager, addon.."Button"..i, text, "WorldMarker"..i)
-    button:SetAttribute("type", "macro")
-    button:SetAttribute("macrotext", format("/wm %d", i))
-    if not previousButton then
-      button:SetPoint("TOPRIGHT", manager, -25, -10)
+  --world raid mark onclick func
+  local function WorldRaidMarkerClick(self)
+    if IsRaidMarkerActive(self.id) then
+      ClearRaidMarker(self.id)
     else
-      button:SetPoint("TOP", previousButton, "BOTTOM", 0, 0)
+      PlaceRaidMarker(self.id)
     end
-    local cancelButton = createBasicButton(manager, addon.."Button"..i.."Cancel", "|TInterface\\Buttons\\UI-GroupLoot-Pass-Up:14:14:0:0|t", "Clear WorldMarker"..i)
-    cancelButton:SetAttribute("type", "macro")
-    cancelButton:SetAttribute("macrotext", format("/cwm %d", i))
-    cancelButton:SetPoint("RIGHT", button, "LEFT", 0, 0)
-    previousButton = button
   end
-
-  --cancel button
-  local button = createBasicButton(manager, addon.."ButtonCancel", "|TInterface\\Buttons\\UI-GroupLoot-Pass-Up:14:14:0:0|t", "Clear All WorldMarkers")
-  button:SetScript("OnClick", ClearRaidMarker)
-  button:SetPoint("TOP", previousButton, "BOTTOM", 0, 0)
-  previousButton = button
-
-  --ready button
-  local button = createBasicButton(manager, addon.."ButtonReady", "|TInterface\\RaidFrame\\ReadyCheck-Ready:14:14:0:0|t", "Ready check")
-  button:SetScript("OnClick", DoReadyCheck)
-  button:SetPoint("TOP", previousButton, "BOTTOM", 0, -10)
-  previousButton = button
-
-  --rolecheck button
-  local button = createBasicButton(manager, addon.."ButtonRoleCheck", "|TInterface\\LFGFrame\\LFGRole:14:14:0:0:64:16:32:48:0:16|t", "Role check")
-  button:SetScript("OnClick", InitiateRolePoll)
-  button:SetPoint("TOP", previousButton, "BOTTOM", 0, 0)
-  previousButton = button
-
-  --hover frame
-  local hoverFrame = CF("BUTTON", addon.."HoverFrame", manager, "SecureHandlerClickTemplate")
-  hoverFrame:SetPoint("TOPRIGHT",-3,-3)
-  hoverFrame:SetPoint("BOTTOMRIGHT",-3,3)
-  hoverFrame:SetWidth(15)
-  local tex = hoverFrame:CreateTexture(nil, "BACKGROUND", nil, -8)
-  tex:SetAllPoints()
-  tex:SetTexture(1,1,1)
-  tex:SetVertexColor(1,1,1)
-  tex:SetAlpha(0.05)
-  hoverFrame.tex = tex
-  manager.hover = hoverFrame
-
-  hoverFrame:SetScript("OnEnter", function(self)
-    GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
-    GameTooltip:AddLine("Open or close the raid manager", 0, 1, 0.5, 1, 1, 1)
-    GameTooltip:Show()
-  end)
-  hoverFrame:SetScript("OnLeave", function(self)
-    GameTooltip:Hide()
-  end)
-
-  hoverFrame:SetAttribute("_onclick", [=[
-    local ref = self:GetFrameRef("manager")
-    if not ref:GetAttribute("state") then
-      ref:SetAttribute("state","closed")
-    end
-    local state = ref:GetAttribute("state")
-    if state == "closed" then
-      ref:SetAlpha(1)
-      ref:SetWidth(275)
-      ref:SetAttribute("state","open")
-    else
-      ref:SetAlpha(0.4)
-      ref:SetWidth(200)
-      ref:SetAttribute("state","closed")
-    end
-  ]=])
-  hoverFrame:SetFrameRef("manager", manager)
 
   --remove the default raidframe manager
   local function BlizzardRaidFrameManagerDisable()
@@ -141,5 +85,98 @@
     CRFC:UnregisterAllEvents()
   end
 
+  ---------------------------
+  -- INIT
+  ---------------------------
+
+  --create manager frame
+  local manager = CF("Frame", addon, UIP)
+  manager:SetSize(200,270)
+  manager:SetPoint("TOPLEFT", -185, -180)
+  manager:SetAlpha(0.4)
+  manager:SetBackdrop(backdrop)
+  manager:SetBackdropColor(0.1,0.1,0.1,0.9)
+  manager:SetBackdropBorderColor(0.7,0.7,0.7)
   manager:RegisterEvent("PLAYER_LOGIN")
   manager:SetScript("OnEvent", BlizzardRaidFrameManagerDisable)
+
+  --create world marker buttons
+  for i=1, NUM_WORLD_RAID_MARKERS do
+    local text = TEX_WORLD_RAID_MARKERS[i]
+    local button = CreateBasicButton(manager, addon.."Button"..i, text, "WorldMarker"..i)
+    button.id = i
+    button:SetScript("OnClick", WorldRaidMarkerClick)
+    if not previousButton then
+      button:SetPoint("TOPRIGHT", manager, -25, -10)
+    else
+      button:SetPoint("TOP", previousButton, "BOTTOM", 0, 0)
+    end
+    previousButton = button
+  end
+
+  --cancel all world markers button
+  local button = CreateBasicButton(manager, addon.."ButtonCancel", "|TInterface\\Buttons\\UI-GroupLoot-Pass-Up:14:14:0:0|t", "Clear All WorldMarkers")
+  button:SetScript("OnClick", ClearRaidMarker)
+  button:SetPoint("TOP", previousButton, "BOTTOM", 0, 0)
+  previousButton = button
+
+  --readycheck button
+  local button = CreateBasicButton(manager, addon.."ButtonReady", "|TInterface\\RaidFrame\\ReadyCheck-Ready:14:14:0:0|t", "Ready check")
+  button:SetScript("OnClick", DoReadyCheck)
+  button:SetPoint("TOP", previousButton, "BOTTOM", 0, -10)
+  previousButton = button
+
+  --rolecheck button
+  local button = CreateBasicButton(manager, addon.."ButtonRoleCheck", "|TInterface\\LFGFrame\\LFGRole:14:14:0:0:64:16:32:48:0:16|t", "Role check")
+  button:SetScript("OnClick", InitiateRolePoll)
+  button:SetPoint("TOP", previousButton, "BOTTOM", 0, 0)
+  previousButton = button
+
+  --state frame
+  local stateFrame = CF("BUTTON", addon.."stateFrame", manager, "SecureHandlerClickTemplate")
+  stateFrame:SetPoint("TOPRIGHT",-3,-3)
+  stateFrame:SetPoint("BOTTOMRIGHT",-3,3)
+  stateFrame:SetWidth(15)
+
+  --state frame texture
+  local bg = stateFrame:CreateTexture(nil, "BACKGROUND", nil, -8)
+  bg:SetAllPoints()
+  bg:SetTexture(1,1,1)
+  bg:SetVertexColor(1,1,1)
+  bg:SetAlpha(0.05)
+  stateFrame.bg = bg
+
+  --state frame onenter
+  stateFrame:SetScript("OnEnter", function(self)
+    self.bg:SetVertexColor(0,0.5,1)
+    GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
+    GameTooltip:AddLine("Open or close the raid manager", 0, 1, 0.5, 1, 1, 1)
+    GameTooltip:Show()
+  end)
+
+  --state frame onleave
+  stateFrame:SetScript("OnLeave", function(self)
+    self.bg:SetVertexColor(1,1,1)
+    GameTooltip:Hide()
+  end)
+
+  --state frame attribute state
+  stateFrame:SetAttribute("state","closed")
+
+  --state frame attribute secure onclick
+  stateFrame:SetAttribute("_onclick", [=[
+    local ref = self:GetFrameRef("manager")
+    local state = ref:GetAttribute("state")
+    if state == "closed" then
+      ref:SetAlpha(1)
+      ref:SetWidth(275)
+      ref:SetAttribute("state","open")
+    else
+      ref:SetAlpha(0.4)
+      ref:SetWidth(200)
+      ref:SetAttribute("state","closed")
+    end
+  ]=])
+
+  --frame reference
+  stateFrame:SetFrameRef("manager", manager)

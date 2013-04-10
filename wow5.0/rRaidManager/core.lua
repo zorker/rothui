272b7655577fb592c7c1277f8d08c4e7e9f0b612
Erik Raetz <zorker@users.noreply.github.com>
2013-04-10 17:26:10 +0000

    Fixing all the taint / block issues. Hide manager when soloing.

--- wow5.0/rRaidManager/core.lua
+++ wow5.0/rRaidManager/core.lua
@@ -50,8 +50,8 @@
 
   --basic button func
   local function CreateBasicButton(parent, name, text, tooltipText)
-    --local button = CF("Button", name, parent, "SecureActionButtonTemplate, UIPanelButtonTemplate")
-    local button = CF("Button", name, parent, "UIPanelButtonTemplate")
+    local button = CF("Button", name, parent, "SecureActionButtonTemplate, UIPanelButtonTemplate")
+    --local button = CF("Button", name, parent, "UIPanelButtonTemplate")
     button.text = _G[button:GetName().."Text"]
     button.text:SetText(text)
     button:SetWidth(30)
@@ -86,24 +86,12 @@
     CRFC:UnregisterAllEvents()
   end
 
-  --manager onevent
-  local function ManagerOnEvent(self, event)
-    if event == "PLAYER_LOGIN" then
-      BlizzardRaidFrameManagerDisable()
-    end
-    if GetNumGroupMembers() > 0 and not self:IsShown() then
-      self:Show()
-    elseif self:IsShown() then
-      self:Hide()
-    end
-  end
-
   ---------------------------
   -- INIT
   ---------------------------
 
   --create manager frame
-  local manager = CF("Frame", addon, UIP)
+  local manager = CF("Frame", addon, UIP, "SecureHandlerStateTemplate")
   manager:SetSize(200,270)
   manager:SetPoint("TOPLEFT", -185, -180)
   manager:SetAlpha(0.4)
@@ -112,20 +100,25 @@
   manager:SetBackdropBorderColor(0.7,0.7,0.7)
   manager.state = "closed"
   manager:RegisterEvent("PLAYER_LOGIN")
-  manager:RegisterEvent("PARTY_MEMBERS_CHANGED")
-  manager:SetScript("OnEvent", ManagerOnEvent)
+  manager:SetScript("OnEvent", BlizzardRaidFrameManagerDisable)
+  RegisterStateDriver(manager, "visibility", "[group:party][group:raid] show; hide")
+
 
   --create world marker buttons
   for i=1, NUM_WORLD_RAID_MARKERS do
     local text = TEX_WORLD_RAID_MARKERS[i]
-    local button = CreateBasicButton(manager, addon.."ButtonWM"..i, text, "Set or clear world marker "..i)
-    button.id = i
-    button:SetScript("OnClick", WorldRaidMarkerOnClick)
+    local button = CreateBasicButton(manager, addon.."Button"..i, text, "WorldMarker"..i)
+    button:SetAttribute("type", "macro")
+    button:SetAttribute("macrotext", format("/wm %d", i))
     if not previousButton then
       button:SetPoint("TOPRIGHT", manager, -25, -10)
     else
       button:SetPoint("TOP", previousButton, "BOTTOM", 0, 0)
     end
+    local cancelButton = CreateBasicButton(manager, addon.."Button"..i.."Cancel", "|TInterface\\Buttons\\UI-GroupLoot-Pass-Up:14:14:0:0|t", "Clear WorldMarker"..i)
+    cancelButton:SetAttribute("type", "macro")
+    cancelButton:SetAttribute("macrotext", format("/cwm %d", i))
+    cancelButton:SetPoint("RIGHT", button, "LEFT", 0, 0)
     previousButton = button
   end
 
@@ -142,7 +135,7 @@
   previousButton = button
 
   --raid to party button
-  local buttonLeft = createBasicButton(manager, addon.."ButtonRaidToParty", "|TInterface\\GroupFrame\\UI-Group-AssistantIcon:14:14:0:0|t", "Raid to party")
+  local buttonLeft = CreateBasicButton(manager, addon.."ButtonRaidToParty", "|TInterface\\GroupFrame\\UI-Group-AssistantIcon:14:14:0:0|t", "Raid to party")
   buttonLeft:SetScript("OnClick", ConvertToParty)
   buttonLeft:SetPoint("RIGHT", button, "LEFT", 0, 0)
 
@@ -153,13 +146,13 @@
   previousButton = button
 
   --party to raid button
-  local buttonLeft = createBasicButton(manager, addon.."ButtonPartyToRaid", "|TInterface\\GroupFrame\\UI-Group-LeaderIcon:14:14:0:0|t", "Party to raid")
+  local buttonLeft = CreateBasicButton(manager, addon.."ButtonPartyToRaid", "|TInterface\\GroupFrame\\UI-Group-LeaderIcon:14:14:0:0|t", "Party to raid")
   buttonLeft:SetScript("OnClick", ConvertToRaid)
   buttonLeft:SetPoint("RIGHT", button, "LEFT", 0, 0)
 
   --state frame
-  --local stateFrame = CF("BUTTON", addon.."stateFrame", manager, "SecureHandlerClickTemplate")
-  local stateFrame = CF("BUTTON", addon.."stateFrame", manager)
+  local stateFrame = CF("BUTTON", addon.."stateFrame", manager, "SecureHandlerClickTemplate")
+  --local stateFrame = CF("BUTTON", addon.."stateFrame", manager)
   stateFrame:SetPoint("TOPRIGHT",-3,-3)
   stateFrame:SetPoint("BOTTOMRIGHT",-3,3)
   stateFrame:SetWidth(15)
@@ -174,7 +167,6 @@
 
   --state frame onenter
   stateFrame:SetScript("OnEnter", function(self)
-    self.bg:SetVertexColor(0,0.5,1)
     GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
     GameTooltip:AddLine("Open or close the raid manager", 0, 1, 0.5, 1, 1, 1)
     GameTooltip:Show()
@@ -182,31 +174,15 @@
 
   --state frame onleave
   stateFrame:SetScript("OnLeave", function(self)
-    self.bg:SetVertexColor(1,1,1)
     GameTooltip:Hide()
   end)
-
-  --state frame onclick
-  stateFrame:SetScript("OnClick", function(self)
-    if manager.state == "closed" then
-      manager:SetAlpha(1)
-      manager:SetWidth(275)
-      manager.state = "open"
-    else
-      manager:SetAlpha(0.4)
-      manager:SetWidth(200)
-      manager.state = "closed"
-    end
-  end)
-
-  --[[
-
-  --state frame attribute state
-  stateFrame:SetAttribute("state","closed")
 
   --state frame attribute secure onclick
   stateFrame:SetAttribute("_onclick", [=[
     local ref = self:GetFrameRef("manager")
+    if not ref:GetAttribute("state") then
+      ref:SetAttribute("state","closed")
+    end
     local state = ref:GetAttribute("state")
     if state == "closed" then
       ref:SetAlpha(1)
@@ -221,5 +197,3 @@
 
   --frame reference
   stateFrame:SetFrameRef("manager", manager)
-
-  ]]

5632fb68225be3817406c32aef1090282901e6cd
erik.raetz@googlemail.com <erik.raetz@googlemail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2013-04-18 12:00:42 +0000

    Work work...

--- trunk/wow5.0/oUF_Donut/config.lua
+++ trunk/wow5.0/oUF_Donut/config.lua
@@ -15,31 +15,142 @@
   -- config
   ----------------------------------------
 
+  --    ring segment layout
+  --     ____ ____
+  --    /    |    \
+  --    |  4 | 1  |
+  --     ----+----
+  --    |  3 | 2  |
+  --    \____|____/
+  --
+
   cfg.units = {
+    --PLAYER
     player = {
       enable = true,
       scale = 0.5,
-      castbar = {
-        scale = 0.9,
+      --castring
+      castring = {
+        radius = 0.9,     -- range 0-1
+        textures = {
+          bg    = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          fill  = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          spark = "Interface\\AddOns\\oUF_Donut\\media\\ring_spark",
+        },
+        colors = {
+          bg        = { r=0, g=0, b=0, a=0.8, },
+          fill      = { r=1, g=0.7, b=0, a=1, },
+          shielded  = { r=0.5, g=0.5, b=0.5, a=1, },
+          latency   = { r=1, g=0, b=0, a=0.8, },
+          colorShielded = true,
+        },
+        numSegmentsUsed = 4,  -- how many sements are affected? (see ring layout)
+        startSegment  = 4,    -- in which segment should the ring begin it's journey? (see ring layout)
+        fillDirection = 0,    -- how should the ring fill up? 1 = clock-wise, 0 = counter-clock-wise
       },
-      powerbar = {
-        scale = 0.7,
+      --powerring
+      powerring = {
+        radius = 0.7,
+        textures = {
+          bg    = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          fill  = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          spark = "Interface\\AddOns\\oUF_Donut\\media\\ring_spark",
+        },
+        colors = {
+          bg    = { r=0, g=0, b=0, a=0.8, },
+          fill  = { r=0, g=0.7, b=1, a=1, },
+          colorClass        = false,
+          colorPower        = true,
+        },
+        numSegmentsUsed = 4,  -- how many sements are affected? (see ring layout)
+        startSegment  = 4,    -- in which segment should the ring begin it's journey? (see ring layout)
+        fillDirection = 0,    -- how should the ring fill up? 1 = clock-wise, 0 = counter-clock-wise
       },
-      healthbar = {
-        scale = 0.5,
+      --healthring
+      healthring = {
+        radius = 0.5,
+        textures = {
+          bg    = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          fill  = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          spark = "Interface\\AddOns\\oUF_Donut\\media\\ring_spark",
+        },
+        colors = {
+          bg    = { r=0, g=0, b=0, a=0.8, },
+          fill  = { r=0, g=1, b=0, a=1, },
+          colorTapping      = true,
+          colorDisconnected = true,
+          colorThreat       = true,
+          colorClass        = true,
+          colorReaction     = true,
+          colorHealth       = true,
+        },
+        numSegmentsUsed = 4,  -- how many sements are affected? (see ring layout)
+        startSegment  = 4,    -- in which segment should the ring begin it's journey? (see ring layout)
+        fillDirection = 0,    -- how should the ring fill up? 1 = clock-wise, 0 = counter-clock-wise
       },
     },
+
+    --TARGET
     target = {
       enable = true,
       scale = 1,
-      castbar = {
-        scale = 0.9,
+      --castring
+      castring = {
+        radius = 0.9,     -- range 0-1
+        textures = {
+          bg    = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          fill  = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          spark = "Interface\\AddOns\\oUF_Donut\\media\\ring_spark",
+        },
+        colors = {
+          bg        = { r=0, g=0, b=0, a=0.8, },
+          fill      = { r=1, g=0.7, b=0, a=1, },
+          shielded  = { r=0.5, g=0.5, b=0.5, a=1, },
+          colorShielded = true,
+        },
+        numSegmentsUsed = 4,  -- how many sements are affected? (see ring layout)
+        startSegment  = 4,    -- in which segment should the ring begin it's journey? (see ring layout)
+        fillDirection = 0,    -- how should the ring fill up? 1 = clock-wise, 0 = counter-clock-wise
       },
-      powerbar = {
-        scale = 0.7,
+      --powerring
+      powerring = {
+        radius = 0.7,
+        textures = {
+          bg    = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          fill  = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          spark = "Interface\\AddOns\\oUF_Donut\\media\\ring_spark",
+        },
+        colors = {
+          bg    = { r=0, g=0, b=0, a=0.8, },
+          fill  = { r=0, g=0.7, b=1, a=1, },
+          colorClass        = false,
+          colorPower        = true,
+        },
+        numSegmentsUsed = 4,  -- how many sements are affected? (see ring layout)
+        startSegment  = 4,    -- in which segment should the ring begin it's journey? (see ring layout)
+        fillDirection = 0,    -- how should the ring fill up? 1 = clock-wise, 0 = counter-clock-wise
       },
-      healthbar = {
-        scale = 0.5,
+      --healthring
+      healthring = {
+        radius = 0.5,
+        textures = {
+          bg    = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          fill  = "Interface\\AddOns\\oUF_Donut\\media\\ring_quarter",
+          spark = "Interface\\AddOns\\oUF_Donut\\media\\ring_spark",
+        },
+        colors = {
+          bg    = { r=0, g=0, b=0, a=0.8, },
+          fill  = { r=0, g=1, b=0, a=1, },
+          colorTapping      = true,
+          colorDisconnected = true,
+          colorThreat       = true,
+          colorClass        = true,
+          colorReaction     = true,
+          colorHealth       = true,
+        },
+        numSegmentsUsed = 4,  -- how many sements are affected? (see ring layout)
+        startSegment  = 4,    -- in which segment should the ring begin it's journey? (see ring layout)
+        fillDirection = 0,    -- how should the ring fill up? 1 = clock-wise, 0 = counter-clock-wise
       },
     },
   }

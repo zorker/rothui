4f8555b6278dae9f049f473bd28fd32f81a40603
Erik Raetz <zorker@users.noreply.github.com>
2013-05-17 14:15:39 +0000

    Added rActiveSpellAlert

---
+++ wow5.0/rActiveSpellAlert/core/init.lua
@@ -0,0 +1,35 @@
+
+  --get the addon namespace
+  local addon, ns = ...
+
+  -----------------------------
+  -- VARIABLES
+  -----------------------------
+
+  local rASA = CreateFrame("Frame")
+  ns.rASA = rASA
+  rASA.auraList = {}
+
+  local tinsert = tinsert
+
+  -----------------------------
+  -- FUNCTIONS
+  -----------------------------
+
+  --add aura func
+  function rASA:AddAura(data,type) {
+    if not GetSpellInfo(data.spellid) then
+      print(addon..": No spell info found for spellid: "..data.spellid)
+      return
+    end
+    data.auraFilter = "HELPFUL"
+    if type == "debuff" then
+      data.auraFilter = "HARMFUL"
+    end
+    data.isOverlayShown = false
+    data.spellName, data.spellRank, data.spellIcon = GetSpellInfo(data.spellid)
+    if data.useSpellIconAsTexture then
+      data.texture = data.spellIcon
+    end
+    tinsert(self.auraList,data)
+  }

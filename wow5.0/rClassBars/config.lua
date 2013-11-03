5d46cd86866dd4a483cb3696f88fdf2f72ffae79
Erik Raetz <zorker@users.noreply.github.com>
2013-11-03 12:29:57 +0000

    Added holypower.lua Unified the orb bar creation process

--- wow5.0/rClassBars/config.lua
+++ wow5.0/rClassBars/config.lua
@@ -21,12 +21,24 @@
 
   --modules
   cfg.modules = {
+    --soul shards
     soulshards = {
       enable    = true,
-      scale     = 0.35,
+      scale     = 0.32,
       color     = {r = 200/255, g = 0/255, b = 255/255, },
       combat          = { --fade the bar in/out in combat/out of combat
         enable          = false,
+        fadeIn          = {time = 0.4, alpha = 1},
+        fadeOut         = {time = 0.3, alpha = 0.2},
+      },
+    },
+    --holy power
+    holypower = {
+      enable    = true,
+      scale     = 0.32,
+      color = {r = 200/255, g = 135/255, b = 190/255, },
+      combat          = { --fade the bar in/out in combat/out of combat
+        enable          = true,
         fadeIn          = {time = 0.4, alpha = 1},
         fadeOut         = {time = 0.3, alpha = 0.2},
       },

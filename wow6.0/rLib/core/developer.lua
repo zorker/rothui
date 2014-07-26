4520b64ab797c219a297dd9842c99a8b50aeaa21
Erik Raetz <zorker@users.noreply.github.com>
2014-07-26 14:57:38 +0000

    Number and point tests

--- wow6.0/rLib/core/developer.lua
+++ wow6.0/rLib/core/developer.lua
@@ -1,27 +1,31 @@
 
-  -- addon data
-  local an, at = ... --addon name, addon table
+  -------------------------------------
+  -- VARIABLES
+  -------------------------------------
 
-  --local variables
+  -- addon data
+  local an, at = ... -- addon name, addon table
+
+  -- local variables
   local G, L, C = at.G, at.L, at.C
 
-  --log func
-  function G:Log(s)
-    print(s)
+  local print = print
+
+  -------------------------------------
+  -- FUNCTIONS
+  -------------------------------------
+
+  -- log func
+  function G:Log(...)
+    print(...)
   end
 
-  --local log func
-  function L:Log(s)
-    print(L.name..": "..s)
+  -- local log func
+  function L:Log(...)
+    print(L.name..":",...)
   end
 
-  L:Log("hello world")
-  L:Log(L.name)
-  L:Log(L.version)
-  L:Log(L.versionNumber)
-  L:Log(L.locale)
-
-  --get mouse focus func
+  -- get mouse focus func
   function G:GetMouseFocus()
     local f = GetMouseFocus()
     local fn = f:GetName()
@@ -40,3 +44,11 @@
       G:Log("Frame: "..fn)
     end
   end
+
+  -------------------------------------
+  -- CALLS
+  -------------------------------------
+
+  local point = {ActionButton1:GetPoint(1)}
+  if point[2] and point[2]:GetName() then point[2] = point[2]:GetName() end
+  L:Log("|cffff00ffPoint of ActionButton1:|r",point[1],point[2],point[3],point[4],point[5])

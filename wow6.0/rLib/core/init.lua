06ae4531a163f3c1e50ef971bed8686570ec11f1
Erik Raetz <zorker@users.noreply.github.com>
2014-07-26 11:49:24 +0000

    Creating addon folder for wow api 6.0
    Creating rLib

---
+++ wow6.0/rLib/core/init.lua
@@ -0,0 +1,23 @@
+
+  -- addon data
+  local an, at = ... --addon name, addon table
+  
+  --global data table
+  at.G = {}
+  --make global data table available as rLib
+  rLib = at.G
+
+  --local data table
+  at.L = {}
+
+  --config data table
+  at.C = {}
+  
+  --local variables
+  local G, L, C = at.G, at.L, at.C
+  
+  --stuff  
+  L.name = an
+  L.version = GetAddOnMetadata(L.name, "Version")
+  L.versionNumber = tonumber(L.version)
+

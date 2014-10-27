235e319b2f145a8b5a8fb8c131f471fad2db7a9f
erik.raetz@gmail.com <erik.raetz@gmail.com@4fa8daaa-f645-0410-a1c5-6dfb7eab8bc1>
2014-10-27 18:01:38 +0000

    Delaying the first setpoint call until PLAYER_LOGIN.

--- trunk/wow6.0/rObjectiveTrackerMover/core.lua
+++ trunk/wow6.0/rObjectiveTrackerMover/core.lua
@@ -3,34 +3,46 @@
   -- // zork - 2014
 
   -----------------------------
-  -- FUNCTIONS
+  -- VARIABLES
   -----------------------------
   
   local an, at = ...
   local unpack = unpack  
   local ObjectiveTrackerFrame = ObjectiveTrackerFrame
-  
-  local frame = CreateFrame("Frame")
 
-  frame:SetScript("OnEvent", function(self,event)
-    self:UnregisterEvent(event)
-    if not InCombatLockdown() then
-      --print(an,"out of combat now, adjusting setpoint now")
-      ObjectiveTrackerFrame:SetPoint(unpack(self.point))
-    end
-  end)
+  local frame = CreateFrame("Frame")
+  
+  local cfg = {}
+  cfg.y = -75
+ 
+  -----------------------------
+  -- FUNCTIONS
+  -----------------------------
 
   local function AdjustSetPoint(self,...)
     local a1,af,a2,x,y = ...
     if a1 and af == "MinimapCluster" then    
       if not InCombatLockdown() then
-        self:SetPoint(a1,af,a2,x,-75)
+        self:SetPoint(a1,af,a2,x,cfg.y)
       else
         --print(an,"delaying setpoint because of combat")
-        frame.point = {a1,af,a2,x,-75}
+        frame.point = {a1,af,a2,x,cfg.y}
         frame:RegisterEvent("PLAYER_REGEN_ENABLED")
       end      
     end
-  end
+  end 
+
+  frame:SetScript("OnEvent", function(self,event)
+    self:UnregisterEvent(event)
+    if event == "PLAYER_LOGIN" then
+      self.point = {ObjectiveTrackerFrame:GetPoint()}      
+      hooksecurefunc(ObjectiveTrackerFrame, "SetPoint", AdjustSetPoint)    
+    end
+    if not InCombatLockdown() then
+      --print(an,"out of combat now, adjusting setpoint now")
+      ObjectiveTrackerFrame:SetPoint(unpack(self.point))
+    end
+  end)
   
-  hooksecurefunc(ObjectiveTrackerFrame, "SetPoint", AdjustSetPoint)
+  frame:RegisterEvent("PLAYER_LOGIN")
+

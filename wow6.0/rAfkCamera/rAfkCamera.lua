35b949e5b0b11da2860e7808e5fb4a5961e80aa3
Erik Raetz <erik.raetz@googlemail.com>
2016-03-05 11:58:06 +0100

    Updated rAfkCamera

--- wow6.0/rAfkCamera/rAfkCamera.lua
+++ wow6.0/rAfkCamera/rAfkCamera.lua
@@ -1,6 +1,6 @@
 
   -- // rAfkCamera
-  -- // zork - 2014
+  -- // zork - 2016
 
   -----------------------------
   -- VARIABLES
@@ -11,68 +11,72 @@
   -----------------------------
   -- FUNCTIONS
   -----------------------------
+    
+  -- canvas enable func
+  local function Enable(self)
+    self.isActive = true
+    self.title:SetText(UnitName("player").."-"..GetRealmName())
+    self.server:SetText("<"..(GetGuildInfo("player") or "No Guild")..">")
+    self:Show()
+    self.fadeIn:Play()
+    self.model:SetUnit("player")
+    self.model:SetRotation(math.rad(-30))
+    UIParent:Hide()
+    SetUIVisibility(false)
+    MoveViewRightStart(0.025)
+  end
+
+  -- canvas disable func
+  local function Disable(self)
+    self.isActive = false
+    self.fadeOut:Play()
+    UIParent:Show()
+    SetUIVisibility(true)
+    MoveViewRightStop()
+  end
 
   local function OnEvent(self)
-    self.title:SetText(UnitName("player").."-"..GetRealmName())
-    self.server:SetText("<"..GetGuildInfo("player")..">")
-    if UIParent:IsShown() and self:IsShown() then
-      self:Disable()
-      return
-    end
     if UnitIsAFK("player") then
-      self:Enable()
-    end
+      if self.isActive then return end
+      Enable(self)
+    else
+      if not self.isActive then return end
+      Disable(self)
+    end    
   end
 
   --canvas frame
   local f = CreateFrame("Frame",nil,WorldFrame)
   f:SetAllPoints()
   f:EnableMouse(true)
+  f:Hide()
   f:SetAlpha(0)
-  --f:Hide()
+  f.isActive = false
   f.w,f.h = f:GetSize()
+  
+  --print(GetGameTime())
 
   --canvas background
   f.bg = f:CreateTexture(nil,"BACKGROUND",nil,-8)
   f.bg:SetTexture(1,1,1)
-  f.bg:SetVertexColor(0.15,0.15,0.15,0.7)
+  f.bg:SetVertexColor(0,0,0,0.85)
   f.bg:SetPoint("BOTTOMLEFT")
   f.bg:SetPoint("BOTTOMRIGHT")
-  f.bg:SetHeight(f.h*0.13)
+  f.bg:SetHeight(100)
 
   --canvas background
   f.bg2 = f:CreateTexture(nil,"BACKGROUND",nil,-8)
   f.bg2:SetTexture(1,1,1)
-  f.bg2:SetVertexColor(0.15,0.15,0.15,0.7)
+  f.bg2:SetVertexColor(0,0,0,0.85)
   f.bg2:SetPoint("TOPLEFT")
   f.bg2:SetPoint("TOPRIGHT")
-  f.bg2:SetHeight(f.h*0.065)
+  f.bg2:SetHeight(50)
   
-  --model
-  f.model = CreateFrame("PlayerModel",nil,f)
-  f.model:SetSize(f.h*0.75,f.h*1.5)
-  f.model:SetPoint("BOTTOMRIGHT",0,-f.h*0.5)
-
-  function f:UpdateModel()
-    self.model:SetUnit("player")
-    self.model:SetRotation(math.rad(-30))
-  end
-  
-  f.server = f:CreateFontString(nil, "BACKGROUND")
-  f.server:SetFont(STANDARD_TEXT_FONT, 18, "OUTLINE")
-  f.server:SetPoint("BOTTOMLEFT", 20, 20)  
-  f.server:SetTextColor(0.6,0.6,0.6)
-  
-  f.title = f:CreateFontString(nil, "BACKGROUND")
-  f.title:SetFont(STANDARD_TEXT_FONT, 32, "OUTLINE")
-  f.title:SetPoint("BOTTOMLEFT", f.server, "TOPLEFT", 0, 0)
-  
-
   --fade in anim
   f.fadeIn = f:CreateAnimationGroup()
   f.fadeIn.anim = f.fadeIn:CreateAnimation("Alpha")
-  f.fadeIn.anim:SetDuration(0.8)
-  f.fadeIn.anim:SetSmoothing("IN")
+  f.fadeIn.anim:SetDuration(1)
+  f.fadeIn.anim:SetSmoothing("OUT")
   f.fadeIn.anim:SetChange(1)
   f.fadeIn:HookScript("OnFinished", function(self)
     self:GetParent():SetAlpha(1)
@@ -81,34 +85,31 @@
   --fade out anim
   f.fadeOut = f:CreateAnimationGroup()
   f.fadeOut.anim = f.fadeOut:CreateAnimation("Alpha")
-  f.fadeOut.anim:SetDuration(0.8)
+  f.fadeOut.anim:SetDuration(1)
   f.fadeOut.anim:SetSmoothing("OUT")
   f.fadeOut.anim:SetChange(-1)
   f.fadeOut:HookScript("OnFinished", function(self)
     self:GetParent():SetAlpha(0)
     self:GetParent():Hide()
   end)
-
-  -- canvas enable func
-  function f:Enable()
-    self:Show()
-    self:UpdateModel()
-    UIParent:Hide()
-    MoveViewRightStart(0.1)
-    self.fadeIn:Play()
-  end
-
-  -- canvas disable func
-  function f:Disable()
-    UIParent:Show()
-    MoveViewRightStop()
-    self.fadeOut:Play()
-  end
-
+  
+  --model
+  f.model = CreateFrame("PlayerModel",nil,f)
+  f.model:SetSize(f.h*0.75,f.h*1.5)
+  f.model:SetPoint("BOTTOMRIGHT",0,-f.h*0.5)
+  
+  f.server = f:CreateFontString(nil, "BACKGROUND")
+  f.server:SetFont(STANDARD_TEXT_FONT, 18, "OUTLINE")
+  f.server:SetPoint("BOTTOMLEFT", 25, 25)  
+  f.server:SetTextColor(0.6,0.6,0.6)
+  
+  f.title = f:CreateFontString(nil, "BACKGROUND")
+  f.title:SetFont(STANDARD_TEXT_FONT, 32, "OUTLINE")
+  f.title:SetPoint("BOTTOMLEFT", f.server, "TOPLEFT", 0, 5)
+  
   f:SetScript("OnEvent",OnEvent)
 
   f:RegisterEvent("PLAYER_FLAGS_CHANGED")
   f:RegisterEvent("PLAYER_ENTERING_WORLD")
   f:RegisterEvent("PLAYER_LEAVING_WORLD")
   
-  UIParent:HookScript("OnShow", function() OnEvent(f) end)

131abe3b40d1012abdcd7e0707d7e18e3156e103
Erik Raetz <erik.raetz@googlemail.com>
2016-05-22 13:41:34 +0200

    rIngameModelViewer updated for WoW Legion Beta

---
+++ wow7.0/rIngameModelViewer/config.lua
@@ -0,0 +1,33 @@
+
+  -------------------------------------
+  -- ADDON TABLES
+  -------------------------------------
+
+  local an, at = ...
+
+  -------------------------------------
+  -- VARIABLES
+  -------------------------------------
+
+  -- local variables
+  local G, L, C = at.G, at.L, at.C
+
+  -------------------------------------
+  -- CONFIG
+  -------------------------------------
+
+  C.sound = {}
+  C.sound.select =  "igcreatureaggroselect"
+  C.sound.swap    = "interfacesound_losttargetunit"
+  C.sound.click   = "igmainmenuoption"
+  C.sound.clack   = "igmainmenulogout"
+
+  C.backdrop = {
+    bgFile = "",
+    edgeFile = "interface\\tooltips\\ui-tooltip-border",
+    tile = false,
+    tileSize = 16,
+    edgeSize = 16,
+    insets = { left = 4, right = 4, top = 4, bottom = 4 },
+  }
+

e094cc866e0c3735c2dd73b00c7d0ff2b17059b3
Erik Raetz <erik.raetz@googlemail.com>
2016-05-26 22:01:59 +0200

    Fix animation

--- wow7.0/rLib/framefader.lua
+++ wow7.0/rLib/framefader.lua
@@ -12,50 +12,44 @@ local A, L = ...
 -- Functions
 -----------------------------
 
-local function FadeInOnFinished(self)
-  local frame = self.__owner
-  frame:SetAlpha(frame.faderConfig.fadeInAlpha)
+local function FaderOnFinished(self)
+  self.__owner:SetAlpha(self.finAlpha)
 end
 
-local function FadeOutOnFinished(self)
-  local frame = self.__owner
-  frame:SetAlpha(frame.faderConfig.fadeOutAlpha)
-end
-
-local function CreateFaderAnimations(frame)
-  if frame.fadeIn then return end
-  --fade in anim
-  frame.fadeIn = frame:CreateAnimationGroup()
-  frame.fadeIn.__owner = frame
-  frame.fadeIn.anim = frame.fadeIn:CreateAnimation("Alpha")
-  frame.fadeIn.anim:SetDuration(frame.faderConfig.fadeInDuration)
-  frame.fadeIn.anim:SetSmoothing(frame.faderConfig.fadeInSmooth)
-  frame.fadeIn.anim:SetFromAlpha(frame.faderConfig.fadeOutAlpha)
-  frame.fadeIn.anim:SetToAlpha(frame.faderConfig.fadeInAlpha)
-  frame.fadeIn:HookScript("OnFinished", FadeInOnFinished)
-  --fade out anim
-  frame.fadeOut = frame:CreateAnimationGroup()
-  frame.fadeOut.__owner = frame
-  frame.fadeOut.anim = frame.fadeOut:CreateAnimation("Alpha")
-  frame.fadeOut.anim:SetDuration(frame.faderConfig.fadeOutDuration)
-  frame.fadeOut.anim:SetSmoothing(frame.faderConfig.fadeOutSmooth)
-  frame.fadeOut.anim:SetFromAlpha(frame.faderConfig.fadeInAlpha)
-  frame.fadeOut.anim:SetToAlpha(frame.faderConfig.fadeOutAlpha)
-  frame.fadeOut:HookScript("OnFinished", FadeOutOnFinished)
+local function CreateFaderAnimation(frame)
+  if frame.fader then return end
+  --fader
+  frame.fader = frame:CreateAnimationGroup()
+  frame.fader.__owner = frame
+  frame.fader.direction = nil
+  frame.fader.anim = frame.fader:CreateAnimation("Alpha")
+  frame.fader:HookScript("OnFinished", FaderOnFinished)
 end
 
 --enable func
 local function StartFadeIn(frame)
-  frame.fadeIn.anim:SetFromAlpha(frame:GetAlpha())
-  frame.fadeOut:Stop()
-  frame.fadeIn:Play()
+  if frame.fader.direction == "in" or not MouseIsOver(frame) then return end
+  frame.fader:Pause()
+  frame.fader.anim:SetFromAlpha(frame:GetAlpha())
+  frame.fader.anim:SetToAlpha(frame.faderConfig.fadeInAlpha)
+  frame.fader.anim:SetDuration(frame.faderConfig.fadeInDuration)
+  frame.fader.anim:SetSmoothing(frame.faderConfig.fadeInSmooth)
+  frame.fader.finAlpha = frame.faderConfig.fadeInAlpha
+  frame.fader:Play()
+  frame.fader.direction = "in"
 end
 
 --disable func
 local function StartFadeOut(frame)
-  frame.fadeOut.anim:SetFromAlpha(frame:GetAlpha())
-  frame.fadeIn:Stop()
-  frame.fadeOut:Play()
+  if frame.fader.direction == "out" or MouseIsOver(frame) then return end
+  frame.fader:Pause()
+  frame.fader.anim:SetFromAlpha(frame:GetAlpha())
+  frame.fader.anim:SetToAlpha(frame.faderConfig.fadeOutAlpha)
+  frame.fader.anim:SetDuration(frame.faderConfig.fadeOutDuration)
+  frame.fader.anim:SetSmoothing(frame.faderConfig.fadeOutSmooth)
+  frame.fader.finAlpha = frame.faderConfig.fadeOutAlpha
+  frame.fader:Play()
+  frame.fader.direction = "out"
 end
 
 local function StartFadeInFromButton(button)
@@ -68,23 +62,25 @@ end
 
 local function StartFadeOutFromButton(button)
   if button:GetParent().faderConfig then
-    StartFadeIn(button:GetParent())
+    StartFadeOut(button:GetParent())
   elseif button:GetParent():GetParent().faderConfig then
-    StartFadeIn(button:GetParent():GetParent())
+    StartFadeOut(button:GetParent():GetParent())
   end
 end
 
 function rLib:CreateButtonFrameFader(frame, buttonList, faderConfig)
   frame.faderConfig = faderConfig
   frame:EnableMouse(true)
-  CreateFaderAnimations(frame)
+  CreateFaderAnimation(frame)
   frame:HookScript("OnEnter", StartFadeIn)
   frame:HookScript("OnLeave", StartFadeOut)
-  if not MouseIsOver(frame) then
-    StartFadeOut(frame)
-  end
   for i, button in next, buttonList do
     button:HookScript("OnEnter", StartFadeInFromButton)
     button:HookScript("OnLeave", StartFadeOutFromButton)
+  end
+  if not MouseIsOver(frame) then
+    StartFadeOut(frame)
+  else
+    StartFadeIn(frame)
   end
 end

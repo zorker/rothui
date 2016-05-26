ba4bd529707dfe1e2549c58ddb9372a2471bdd0a
Erik Raetz <erik.raetz@googlemail.com>
2016-05-26 12:06:49 +0200

    Fix index

--- wow7.0/rActionBar/core/bars.lua
+++ wow7.0/rActionBar/core/bars.lua
@@ -84,7 +84,7 @@ do
   local buttonList = L:GetButtonList(cfg.buttonName, cfg.numButtons)
   local frame = L:CreateButtonFrame(cfg,buttonList)
   --special
-  for idx, button in next, buttonList do
+  for i, button in next, buttonList do
     _G[cfg.buttonName..i.."Cooldown"]:SetAllPoints(button) --fix cooldown display
   end
 end

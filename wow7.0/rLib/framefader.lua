7b6bc7a725b01c8f2c26096346af90d70fed5153
Erik Raetz <erik.raetz@googlemail.com>
2016-05-27 00:08:42 +0200

    Added SpellFlyout Check, Code cleanup

--- wow7.0/rLib/framefader.lua
+++ wow7.0/rLib/framefader.lua
@@ -18,7 +18,6 @@ end
 
 local function CreateFaderAnimation(frame)
   if frame.fader then return end
-  --fader
   frame.fader = frame:CreateAnimationGroup()
   frame.fader.__owner = frame
   frame.fader.direction = nil
@@ -26,30 +25,36 @@ local function CreateFaderAnimation(frame)
   frame.fader:HookScript("OnFinished", FaderOnFinished)
 end
 
---enable func
-local function StartFadeIn(frame)
-  if frame.fader.direction == "in" or not MouseIsOver(frame) then return end
-  frame.fader:Pause()
-  frame.fader.anim:SetFromAlpha(frame:GetAlpha())
-  frame.fader.anim:SetToAlpha(frame.faderConfig.fadeInAlpha)
-  frame.fader.anim:SetDuration(frame.faderConfig.fadeInDuration)
-  frame.fader.anim:SetSmoothing(frame.faderConfig.fadeInSmooth)
-  frame.fader.finAlpha = frame.faderConfig.fadeInAlpha
-  frame.fader:Play()
-  frame.fader.direction = "in"
+local function IsMouseOverFlyout(frame)
+  if not SpellFlyout:IsShown() then return false end
+  if SpellFlyout:GetParent():GetParent():GetParent() == frame and MouseIsOver(SpellFlyout) then return true end
+  return false
 end
 
---disable func
+local function StartFadeIn(frame)
+  if frame.fader.direction == "in" then return end
+  if MouseIsOver(frame) or IsMouseOverFlyout(frame) then
+    frame.fader:Pause()
+    frame.fader.anim:SetFromAlpha(frame:GetAlpha())
+    frame.fader.anim:SetToAlpha(frame.faderConfig.fadeInAlpha)
+    frame.fader.anim:SetDuration(frame.faderConfig.fadeInDuration)
+    frame.fader.anim:SetSmoothing(frame.faderConfig.fadeInSmooth)
+    frame.fader.finAlpha = frame.faderConfig.fadeInAlpha
+    frame.fader.direction = "in"
+    frame.fader:Play()
+  end
+end
+
 local function StartFadeOut(frame)
-  if frame.fader.direction == "out" or MouseIsOver(frame) then return end
+  if frame.fader.direction == "out" or MouseIsOver(frame) or IsMouseOverFlyout(frame) then return end
   frame.fader:Pause()
   frame.fader.anim:SetFromAlpha(frame:GetAlpha())
   frame.fader.anim:SetToAlpha(frame.faderConfig.fadeOutAlpha)
   frame.fader.anim:SetDuration(frame.faderConfig.fadeOutDuration)
   frame.fader.anim:SetSmoothing(frame.faderConfig.fadeOutSmooth)
   frame.fader.finAlpha = frame.faderConfig.fadeOutAlpha
-  frame.fader:Play()
   frame.fader.direction = "out"
+  frame.fader:Play()
 end
 
 local function StartFadeInFromButton(button)
@@ -68,19 +73,23 @@ local function StartFadeOutFromButton(button)
   end
 end
 
-function rLib:CreateButtonFrameFader(frame, buttonList, faderConfig)
+function rLib:CreateFrameFader(frame, faderConfig)
   frame.faderConfig = faderConfig
   frame:EnableMouse(true)
   CreateFaderAnimation(frame)
   frame:HookScript("OnEnter", StartFadeIn)
   frame:HookScript("OnLeave", StartFadeOut)
-  for i, button in next, buttonList do
-    button:HookScript("OnEnter", StartFadeInFromButton)
-    button:HookScript("OnLeave", StartFadeOutFromButton)
-  end
   if not MouseIsOver(frame) then
     StartFadeOut(frame)
   else
     StartFadeIn(frame)
+  end
+end
+
+function rLib:CreateButtonFrameFader(frame, buttonList, faderConfig)
+  rLib:CreateFrameFader(frame, faderConfig)
+  for i, button in next, buttonList do
+    button:HookScript("OnEnter", StartFadeInFromButton)
+    button:HookScript("OnLeave", StartFadeOutFromButton)
   end
 end

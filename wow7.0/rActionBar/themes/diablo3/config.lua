90e92c1f00c914f701482bfc7993bad0206feb4d
Erik Raetz <erik.raetz@googlemail.com>
2016-05-26 14:59:41 +0200

    Theme examples

---
+++ wow7.0/rActionBar/themes/diablo3/config.lua
@@ -0,0 +1,15 @@
+
+-- rActionBar: themes\diablo3\config
+-- zork, 2016
+
+-----------------------------
+-- Variables
+-----------------------------
+
+local A, L = ...
+
+-----------------------------
+-- Init
+-----------------------------
+
+print('themes\diablo3\config.lua')

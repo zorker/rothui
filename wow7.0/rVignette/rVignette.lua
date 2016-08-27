893b159968423493c7ea06517c9caf6b680fa2b9
Erik Raetz <erik.raetz@googlemail.com>
2016-08-27 11:23:07 +0200

    Soundfile is OGG now

--- wow7.0/rVignette/rVignette.lua
+++ wow7.0/rVignette/rVignette.lua
@@ -18,7 +18,7 @@ local function OnVignetteAdded(self,event,id)
   if self.vignettes[id] then return end
   local x, y, name, icon = C_Vignettes.GetVignetteInfoFromInstanceID(id)
   local left, right, top, bottom = GetObjectIconTextureCoords(icon)
-  PlaySoundFile("Sound\\Interface\\RaidWarning.wav")
+  PlaySoundFile("Sound\\Interface\\RaidWarning.ogg")
   local str = "|TInterface\\MINIMAP\\ObjectIconsAtlas:0:0:0:0:256:256:"..(left*256)..":"..(right*256)..":"..(top*256)..":"..(bottom*256).."|t"
   RaidNotice_AddMessage(RaidWarningFrame, str..name.." spotted!", ChatTypeInfo["RAID_WARNING"])
   print(str..name,"spotted!")

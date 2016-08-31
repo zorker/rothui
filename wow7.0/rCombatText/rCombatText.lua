d0eb0d9abf04d5cf07ab8cf6e68ef5b00d9a11d8
Erik Raetz <erik.raetz@googlemail.com>
2016-08-31 23:13:54 +0200

    Added rCombatText

---
+++ wow7.0/rCombatText/rCombatText.lua
@@ -0,0 +1,17 @@
+SystemFont_Shadow_Huge3:SetFont(STANDARD_TEXT_FONT, 16, "OUTLINE")
+SystemFont_Shadow_Huge3:SetShadowOffset(1,-1)
+SystemFont_Shadow_Huge3:SetShadowColor(0,0,0,0.6)
+
+COMBAT_TEXT_HEIGHT = 16
+COMBAT_TEXT_CRIT_MAXHEIGHT = 24
+COMBAT_TEXT_CRIT_MINHEIGHT = 16
+COMBAT_TEXT_SCROLLSPEED = 3
+
+local function UpdateDisplayMessages()
+  if COMBAT_TEXT_FLOAT_MODE == "1" then
+    COMBAT_TEXT_LOCATIONS.startY = 484
+    COMBAT_TEXT_LOCATIONS.endY = 709
+  end
+end
+
+hooksecurefunc("CombatText_UpdateDisplayedMessages", UpdateDisplayMessages)

8afca3cc21931195f8b9aae6ec31791f11a5e5a3
Erik Raetz <erik.raetz@googlemail.com>
2016-09-30 19:35:44 +0200

    Some more tests

--- wow7.0/rClipThing/core.lua
+++ wow7.0/rClipThing/core.lua
@@ -19,16 +19,16 @@ f:SetPoint("CENTER")
 
 local t = f:CreateTexture(nil,"BACKGROUND",nil,-8)
 t:SetAllPoints()
-t:SetColorTexture(1,0,0)
+t:SetColorTexture(1,0.7,0)
 
 local sc = CreateFrame("Frame")
 f:SetScrollChild(sc)
 sc:SetSize(256,32)
 
-
 local t = sc:CreateTexture(nil,"BACKGROUND",nil,-8)
 t:SetSize(512,512)
-t:SetPoint("CENTER",-128,0)
+--t:SetSize(sqrt(2)*512,sqrt(2)*512)
+t:SetPoint("CENTER",f,-120,0)
 t:SetBlendMode("MOD")
 t:SetAlpha(1)
 
@@ -37,6 +37,6 @@ t:SetTexture("interface\\addons\\"..A.."\\cloudsphere")
 t.ag = t:CreateAnimationGroup()
 t.ag.anim = t.ag:CreateAnimation("Rotation")
 t.ag.anim:SetDegrees(360)
-t.ag.anim:SetDuration(20)
+t.ag.anim:SetDuration(30)
 t.ag:SetLooping("REPEAT")
 t.ag:Play()

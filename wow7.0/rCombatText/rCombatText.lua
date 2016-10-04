498912942490742cc259dc50c56bc5badd368f16
Erik Raetz <erik.raetz@googlemail.com>
2016-10-04 20:06:28 +0200

    Values adjusted

--- wow7.0/rCombatText/rCombatText.lua
+++ wow7.0/rCombatText/rCombatText.lua
@@ -1,16 +1,18 @@
-SystemFont_Shadow_Huge3:SetFont(STANDARD_TEXT_FONT, 16, "OUTLINE")
+local size = 18
+
+SystemFont_Shadow_Huge3:SetFont(STANDARD_TEXT_FONT, size, "OUTLINE")
 SystemFont_Shadow_Huge3:SetShadowOffset(1,-1)
 SystemFont_Shadow_Huge3:SetShadowColor(0,0,0,0.6)
 
-COMBAT_TEXT_HEIGHT = 16
-COMBAT_TEXT_CRIT_MAXHEIGHT = 24
-COMBAT_TEXT_CRIT_MINHEIGHT = 16
-COMBAT_TEXT_SCROLLSPEED = 3
+COMBAT_TEXT_HEIGHT = size
+COMBAT_TEXT_CRIT_MAXHEIGHT = size*1.8
+COMBAT_TEXT_CRIT_MINHEIGHT = size*1.4
+COMBAT_TEXT_SCROLLSPEED = 3.5
 
 local function UpdateDisplayMessages()
   if COMBAT_TEXT_FLOAT_MODE == "1" then
-    COMBAT_TEXT_LOCATIONS.startY = 484
-    COMBAT_TEXT_LOCATIONS.endY = 709
+    COMBAT_TEXT_LOCATIONS.startY = 350
+    COMBAT_TEXT_LOCATIONS.endY = 550
   end
 end
 

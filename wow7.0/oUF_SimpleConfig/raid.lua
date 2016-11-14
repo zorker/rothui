77aec741cfea9aa6f949da4c7a21e45bab18179b
Erik Raetz <erik.raetz@googlemail.com>
2016-11-14 18:35:49 +0100

    Raid test

--- wow7.0/oUF_SimpleConfig/raid.lua
+++ wow7.0/oUF_SimpleConfig/raid.lua
@@ -14,16 +14,16 @@ local A, L = ...
 
 L.C.raid = {
   enabled = true,
-  size = {130,26},
+  size = {110,26},
   points = { --list of 8 points, one for each raid group
     {"TOPLEFT",20,-20},
-    {"TOP", "oUF_SimpleRaidHeader1", "BOTTOM", 0, -10},
-    {"TOP", "oUF_SimpleRaidHeader2", "BOTTOM", 0, -10},
-    {"TOP", "oUF_SimpleRaidHeader3", "BOTTOM", 0, -10},
+    {"TOP", "oUF_SimpleRaidHeader1", "BOTTOM", 0, -0},
+    {"TOP", "oUF_SimpleRaidHeader2", "BOTTOM", 0, -0},
+    {"TOP", "oUF_SimpleRaidHeader3", "BOTTOM", 0, -0},
     {"LEFT", "oUF_SimpleRaidHeader1", "RIGHT", 10, 0},
-    {"TOP", "oUF_SimpleRaidHeader5", "BOTTOM", 0, -10},
-    {"TOP", "oUF_SimpleRaidHeader6", "BOTTOM", 0, -10},
-    {"TOP", "oUF_SimpleRaidHeader7", "BOTTOM", 0, -10},
+    {"TOP", "oUF_SimpleRaidHeader5", "BOTTOM", 0, -0},
+    {"TOP", "oUF_SimpleRaidHeader6", "BOTTOM", 0, -0},
+    {"TOP", "oUF_SimpleRaidHeader7", "BOTTOM", 0, -0},
   },
   scale = 1*L.C.globalscale,
   --healthbar

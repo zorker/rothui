a1ed1a7fd4a12b0a871c6a4be2c71a4f83121f81
Erik Raetz <erik.raetz@googlemail.com>
2018-01-26 18:31:22 +0100

    Added two new tags
    oUF_Simple:role
    oUF_Simple:leader

--- wow7.0/oUF_Simple/core/tags.lua
+++ wow7.0/oUF_Simple/core/tags.lua
@@ -33,6 +33,29 @@ end
 --tag event: oUF_Simple:health
 oUF.Tags.Events["oUF_Simple:health"] = "UNIT_HEALTH UNIT_MAXHEALTH UNIT_CONNECTION"
 
+--tag method: oUF_Simple:role
+oUF.Tags.Methods["oUF_Simple:role"] = function(unit)
+  local role = UnitGroupRolesAssigned(unit)
+  if role == "TANK" then
+    return "|TInterface\\LFGFrame\\LFGRole:14:14:0:0:64:16:32:48:0:16|t"
+  elseif role == "HEALER" then
+    return "|TInterface\\LFGFrame\\LFGRole:14:14:0:0:64:16:48:64:0:16|t"
+  elseif role == "DAMAGER" then
+    return "|TInterface\\LFGFrame\\LFGRole:14:14:0:0:64:16:16:32:0:16|t"
+  end
+end
+--tag event: oUF_Simple:role
+oUF.Tags.Events["oUF_Simple:role"] = "PLAYER_ROLES_ASSIGNED GROUP_ROSTER_UPDATE"
+
+--tag method: oUF_Simple:leader
+oUF.Tags.Methods["oUF_Simple:leader"] = function(unit)
+  if UnitIsGroupLeader(unit) then
+    return "|TInterface\\GroupFrame\\UI-Group-LeaderIcon:14:14:0:0|t"
+  end
+end
+--tag event: oUF_Simple:leader
+oUF.Tags.Events["oUF_Simple:leader"] = "PARTY_LEADER_CHANGED GROUP_ROSTER_UPDATE"
+
 --load tags from the config
 if L.C.tagMethods and type(L.C.tagMethods) == "table" and
    L.C.tagEvents  and type(L.C.tagEvents) == "table" then

841cb952804f94a2886461e304cea98b47a2e647
Erik Raetz <erik.raetz@googlemail.com>
2018-01-27 02:03:57 +0100

    Test setup

--- wow7.0/oUF_Orbs/oUF_Orbs.lua
+++ wow7.0/oUF_Orbs/oUF_Orbs.lua
@@ -16,20 +16,102 @@ L.C = {}
 
 --mediapath
 L.C.mediapath = "interface\\addons\\"..A.."\\media\\"
-local size = 200
+local size = 128
 
 local function AddTexture(self,file,index)
-  local t = self:CreateTexture(nil,"BACKGROUND",index)
+  local t = self:CreateTexture(nil,"BACKGROUND",nil,index)
   t:SetTexture(L.C.mediapath..file)
   t:SetAllPoints()
   return t
 end
 
-local frame = CreateFrame("Frame",nil,UIParent)
-frame:SetSize(size,size)
-frame:SetPoint("CENTER")
+local function CreateUnitFrame()
 
-local orbBg = AddTexture(frame,"orb_bg",-8)
-local orbFill = AddTexture(frame,"orb_fill",-7)
-orbFill:SetVertexColor(0,.8,0)
-local orbHl = AddTexture(frame,"orb_hl",-6)
+  local frame = CreateFrame("Frame",nil,UIParent)
+  frame:SetSize(size,size)
+
+  local healthbar = CreateFrame("Frame",nil,frame)
+  healthbar:SetAllPoints()
+
+  local orbBg = AddTexture(healthbar,"orb_bg",-8)
+  local orbFill = AddTexture(healthbar,"orb_fill",-7)
+  orbFill:SetVertexColor(0,.8,0)
+  local orbHl = AddTexture(healthbar,"orb_hl",-6)
+
+  local powerbar = CreateFrame("Frame",nil,frame)
+  powerbar:SetAllPoints()
+
+  local ringBottomBg = AddTexture(powerbar,"ring_bottom_bg",-5)
+  local ringBottomFill = AddTexture(powerbar,"ring_bottom_fill",-4)
+  ringBottomFill:SetVertexColor(.8,0,0)
+  local ringBottomHl = AddTexture(powerbar,"ring_bottom_hl",-3)
+
+  local alternativepowerbar = CreateFrame("Frame",nil,frame)
+  alternativepowerbar:SetAllPoints()
+
+  local ringTopBg = AddTexture(alternativepowerbar,"ring_top_bg",-2)
+  local ringTopFill = AddTexture(alternativepowerbar,"ring_top_fill",-1)
+  ringTopFill:SetVertexColor(0,.4,.8)
+  local ringTopHl = AddTexture(alternativepowerbar,"ring_top_hl",0)
+
+  ringTopFill:Hide()
+
+  local castbar = CreateFrame("Frame",nil,frame)
+  castbar:SetAllPoints()
+  castbar:Hide()
+
+  local ringOuterBg = AddTexture(castbar,"ring_outer_bg",1)
+  local ringOuterFill = AddTexture(castbar,"ring_outer_fill",2)
+  ringOuterFill:SetVertexColor(.8,.6,0)
+  local ringOuterHl = AddTexture(castbar,"ring_outer_hl",3)
+
+  frame.castbar = castbar
+  frame.alternativepowerbar = alternativepowerbar
+  frame.powerbar = powerbar
+  frame.healthbar = healthbar
+
+  return frame
+
+end
+
+local player = CreateUnitFrame()
+player:SetPoint("CENTER",-200,0)
+player:SetScale(1.2)
+player.alternativepowerbar:Show()
+
+local pet = CreateUnitFrame()
+pet:SetPoint("TOPRIGHT",player,"BOTTOMLEFT",20,20)
+pet:SetScale(0.8)
+
+local focus = CreateUnitFrame()
+focus:SetPoint("BOTTOMRIGHT",player,"TOPLEFT",20,-20)
+focus:SetScale(0.8)
+
+local target = CreateUnitFrame()
+target:SetPoint("CENTER",200,0)
+target:SetScale(1.2)
+target.castbar:Show()
+
+local targettarget = CreateUnitFrame()
+targettarget:SetPoint("TOPRIGHT",target,"BOTTOMLEFT",20,20)
+targettarget:SetScale(0.8)
+
+local party1 = CreateUnitFrame()
+party1:SetPoint("TOPLEFT",20,-20)
+party1:SetScale(0.8)
+
+local party2 = CreateUnitFrame()
+party2:SetPoint("LEFT",party1,"RIGHT",20,0)
+party2:SetScale(0.8)
+
+local party3 = CreateUnitFrame()
+party3:SetPoint("LEFT",party2,"RIGHT",20,0)
+party3:SetScale(0.8)
+
+local party4 = CreateUnitFrame()
+party4:SetPoint("LEFT",party3,"RIGHT",20,0)
+party4:SetScale(0.8)
+
+local party5 = CreateUnitFrame()
+party5:SetPoint("LEFT",party4,"RIGHT",20,0)
+party5:SetScale(0.8)

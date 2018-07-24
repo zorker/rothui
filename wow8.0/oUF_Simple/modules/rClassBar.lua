e364db4112a6e32ef96d123cffd4925e15c9e022
Erik Raetz <erik.raetz@googlemail.com>
2018-07-24 20:48:15 +0200

    rClassBar module fixed

--- wow8.0/oUF_Simple/modules/rClassBar.lua
+++ wow8.0/oUF_Simple/modules/rClassBar.lua
@@ -4,25 +4,37 @@ local oUF = L.oUF or oUF
 
 local _, PlayerClass = UnitClass'player'
 local ClassPowerID, ClassPowerType
-local RequireSpec, RequireSpell
+local RequireSpec, RequireSpell, RequirePower
 local unpack = unpack
+
+local SPEC_MAGE_ARCANE = SPEC_MAGE_ARCANE or 1
+local SPEC_MONK_WINDWALKER = SPEC_MONK_WINDWALKER or 3
+local SPEC_PALADIN_RETRIBUTION = SPEC_PALADIN_RETRIBUTION or 3
+local SPEC_WARLOCK_DESTRUCTION = SPEC_WARLOCK_DESTRUCTION or 3
+local SPELL_POWER_ENERGY = Enum.PowerType.Energy or 3
+local SPELL_POWER_COMBO_POINTS = Enum.PowerType.ComboPoints or 4
+local SPELL_POWER_SOUL_SHARDS = Enum.PowerType.SoulShards or 7
+local SPELL_POWER_HOLY_POWER = Enum.PowerType.HolyPower or 9
+local SPELL_POWER_CHI = Enum.PowerType.Chi or 12
+local SPELL_POWER_ARCANE_CHARGES = Enum.PowerType.ArcaneCharges or 16
 
 do
   if(PlayerClass == 'MONK') then
     ClassPowerID = SPELL_POWER_CHI
-    ClassPowerType = "CHI"
+    ClassPowerType = 'CHI'
     RequireSpec = SPEC_MONK_WINDWALKER
   elseif(PlayerClass == 'PALADIN') then
     ClassPowerID = SPELL_POWER_HOLY_POWER
-    ClassPowerType = "HOLY_POWER"
+    ClassPowerType = 'HOLY_POWER'
     RequireSpec = SPEC_PALADIN_RETRIBUTION
   elseif(PlayerClass == 'WARLOCK') then
     ClassPowerID = SPELL_POWER_SOUL_SHARDS
-    ClassPowerType = "SOUL_SHARDS"
+    ClassPowerType = 'SOUL_SHARDS'
   elseif(PlayerClass == 'ROGUE' or PlayerClass == 'DRUID') then
     ClassPowerID = SPELL_POWER_COMBO_POINTS
     ClassPowerType = 'COMBO_POINTS'
     if(PlayerClass == 'DRUID') then
+      RequirePower = SPELL_POWER_ENERGY
       RequireSpell = 5221 -- Shred
     end
   elseif(PlayerClass == 'MAGE') then
@@ -51,7 +63,7 @@ local function UpdateSplits(self,maxSegments)
   bar.maxSegments = maxSegments
   if not maxSegments or maxSegments < 2 then
     if not bar.splits then return end
-    for i=1,7 do
+    for i=1,8 do
       if bar.splits[i] and bar.splits[i]:IsShown() then
         bar.splits[i]:Hide()
       end
@@ -61,7 +73,7 @@ local function UpdateSplits(self,maxSegments)
   if maxSegments > 8 then maxSegments = 5 end --anything above 8 will be split into 5 parts of 20%.
   if not bar.splits then bar.splits = {} end
   local p = bar:GetWidth()/maxSegments
-  for i=1,7 do
+  for i=1,8 do
     if i>maxSegments-1 then
       if bar.splits[i] and bar.splits[i]:IsShown() then
         bar.splits[i]:Hide()
@@ -75,10 +87,11 @@ local function UpdateSplits(self,maxSegments)
 end
 
 local function Update(self, event, unit, powerType)
-  if(not (unit == 'player' and powerType == ClassPowerType
-  or unit == 'vehicle' and powerType == 'COMBO_POINTS')) then
+  if(not (self.unit == unit and (unit == 'player' and powerType == ClassPowerType
+    or unit == 'vehicle' and powerType == 'COMBO_POINTS'))) then
     return
   end
+  if event == 'ClassPowerDisable' then return end
   local cb = self.rClassBar
   local ppcur, ppmax
   if unit == 'vehicle' then
@@ -111,24 +124,22 @@ local function Path(self, ...)
 end
 
 local function ClassPowerEnable(self)
-  self:RegisterEvent('UNIT_DISPLAYPOWER', Path)
   self:RegisterEvent('UNIT_POWER_FREQUENT', Path)
   self:RegisterEvent('UNIT_MAXPOWER', Path)
+  self.rClassBar.isEnabled = true
   if(UnitHasVehicleUI('player')) then
     Path(self, 'ClassPowerEnable', 'vehicle', 'COMBO_POINTS')
   else
     Path(self, 'ClassPowerEnable', 'player', ClassPowerType)
   end
-  self.rClassBar.isEnabled = true
 end
 
 local function ClassPowerDisable(self)
-  self:UnregisterEvent('UNIT_DISPLAYPOWER', Path)
   self:UnregisterEvent('UNIT_POWER_FREQUENT', Path)
   self:UnregisterEvent('UNIT_MAXPOWER', Path)
-  Path(self, 'ClassPowerDisable', 'player', ClassPowerType)
   self.rClassBar:Hide()
   self.rClassBar.isEnabled = false
+  Path(self, 'ClassPowerDisable', 'player', ClassPowerType)
 end
 
 local function Visibility(self, event, unit)
@@ -139,21 +150,26 @@ local function Visibility(self, event, unit)
     unit = 'vehicle'
   elseif(ClassPowerID) then
     if(not RequireSpec or RequireSpec == GetSpecialization()) then
-      if(not RequireSpell or IsPlayerSpell(RequireSpell)) then
-        self:UnregisterEvent('SPELLS_CHANGED', Visibility)
-        shouldEnable = true
-      else
-        self:RegisterEvent('SPELLS_CHANGED', Visibility, true)
+      -- use 'player' instead of unit because 'SPELLS_CHANGED' is a unitless event
+      if(not RequirePower or RequirePower == UnitPowerType('player')) then
+        if(not RequireSpell or IsPlayerSpell(RequireSpell)) then
+          self:UnregisterEvent('SPELLS_CHANGED', Visibility)
+          shouldEnable = true
+          unit = 'player'
+        else
+          self:RegisterEvent('SPELLS_CHANGED', Visibility, true)
+        end
       end
     end
   end
   local isEnabled = element.isEnabled
+  local powerType = unit == 'vehicle' and 'COMBO_POINTS' or ClassPowerType
   if(shouldEnable and not isEnabled) then
     ClassPowerEnable(self)
   elseif(not shouldEnable and (isEnabled or isEnabled == nil)) then
     ClassPowerDisable(self)
   elseif(shouldEnable and isEnabled) then
-    Path(self, event, unit, unit == 'vehicle' and 'COMBO_POINTS' or ClassPowerType)
+    Path(self, event, unit, powerType)
   end
 end
 
@@ -174,13 +190,19 @@ local function Enable(self, unit)
   if(RequireSpec or RequireSpell) then
     self:RegisterEvent('PLAYER_TALENT_UPDATE', VisibilityPath, true)
   end
+  if(RequirePower) then
+    self:RegisterEvent('UNIT_DISPLAYPOWER', VisibilityPath)
+  end
   return true
 end
 
 local function Disable(self)
-  local element = self.rClassBar
-  if(not element) then return end
-  ClassPowerDisable(self)
+  if self.rClassBar then
+    ClassPowerDisable(self)
+    self:UnregisterEvent('PLAYER_TALENT_UPDATE', VisibilityPath)
+    self:UnregisterEvent('UNIT_DISPLAYPOWER', VisibilityPath)
+    self:UnregisterEvent('SPELLS_CHANGED', Visibility)
+  end
 end
 
 oUF:AddElement('rClassBar', VisibilityPath, Enable, Disable)

da27a9cf4a86f995bdc3705bf171976ae86a6693
Erik Raetz <erik.raetz@googlemail.com>
2018-09-22 17:29:29 +0200

    Cheap shot test for rogue

--- wow8.0/rFilter_Zork/debuff.lua
+++ wow8.0/rFilter_Zork/debuff.lua
@@ -15,3 +15,7 @@ local A, L = ...
 if L.C.playerName == "Zörk" then
   rFilter:CreateDebuff(115767,"target",36,{"CENTER"},"[spec:3,combat]show;hide",{0.2,1},true,nil) --deep wounds
 end
+
+if L.C.playerClass == "ROGUE" then
+  rFilter:CreateDebuff(1833,"target",36,{"CENTER"},"[combat]show;hide",{0.2,1},true,"player") --deep wounds
+end

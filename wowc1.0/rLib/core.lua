851e982feed4009bdae965b29bb6875846c15e2e
Erik Raetz <erik.raetz@googlemail.com>
2019-09-05 12:11:26 +0200

    copyTable renamed to CopyTable

--- wowc1.0/rLib/core.lua
+++ wowc1.0/rLib/core.lua
@@ -20,22 +20,22 @@ rLib.addonName = A
 -- Functions
 -----------------------------
 
---copyTable
-local function copyTable(orig)
+--CopyTable
+local function CopyTable(orig)
   local orig_type = type(orig)
   local copy
   if orig_type == 'table' then
     copy = {}
     for orig_key, orig_value in next, orig, nil do
-      copy[copyTable(orig_key)] = copyTable(orig_value)
+      copy[CopyTable(orig_key)] = CopyTable(orig_value)
     end
-    setmetatable(copy, copyTable(getmetatable(orig)))
+    setmetatable(copy, CopyTable(getmetatable(orig)))
   else -- number, string, boolean, etc
     copy = orig
   end
   return copy
 end
-rLib.CopyTable = copyTable
+rLib.CopyTable = CopyTable
 
 --rLib:RegisterCallback
 function rLib:RegisterCallback(event, callback, ...)

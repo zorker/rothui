3a0facc7403d79900960a3460685b2b3841402b8
Erik Raetz <erik.raetz@googlemail.com>
2019-09-07 10:05:51 +0200

    New addon to show/hide the Blizzard player frame via state driver

---
+++ wowc1.0/rPlayerFrame/config.lua
@@ -0,0 +1,28 @@
+
+-- rPlayerFrame: config
+-- zork, 2019
+
+-----------------------------
+-- Variables
+-----------------------------
+
+local A, L = ...
+
+-----------------------------
+-- Config
+-----------------------------
+
+L.C = {
+  frameVisibility = "[mod:shift][combat][@target,exists] show; hide",
+  --fader via OnShow trigger
+  fader = {
+    fadeInAlpha = 1,
+    fadeInDuration = 0.3,
+    fadeInSmooth = "OUT",
+    fadeOutAlpha = 0,
+    fadeOutDuration = 0.9,
+    fadeOutSmooth = "OUT",
+    fadeOutDelay = 0,
+    trigger = "OnShow",
+  },
+}
---
+++ wowc1.0/rPlayerFrame/core.lua
@@ -0,0 +1,30 @@
+
+-- rPlayerFrame: core
+-- zork, 2019
+
+-----------------------------
+-- Variables
+-----------------------------
+
+local A, L = ...
+
+-----------------------------
+-- Init
+-----------------------------
+
+--using code from Semlar (https://www.wowinterface.com/forums/showthread.php?t=48971)
+local frame = CreateFrame("Frame", nil, nil, "SecureHandlerStateTemplate")
+frame:SetFrameRef("PlayerFrame", PlayerFrame)
+frame:SetAttribute("_onstate-display", [[
+  if newstate == "show" then
+    self:GetFrameRef("PlayerFrame"):Show()
+  else
+    self:GetFrameRef("PlayerFrame"):Hide()
+  end
+]])
+RegisterStateDriver(frame, "display", L.C.frameVisibility)
+
+--OnShow fader
+if L.C.fader then
+  rLib:CreateFrameFader(PlayerFrame, L.C.fader)
+end

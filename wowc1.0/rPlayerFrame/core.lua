15c983bad548e27fb67a21385e422a46af2fa770
Erik Raetz <erik.raetz@googlemail.com>
2019-09-22 12:44:46 +0200

    Added status coloring to player and target frame

--- wowc1.0/rPlayerFrame/core.lua
+++ wowc1.0/rPlayerFrame/core.lua
@@ -8,6 +8,8 @@
 
 local A, L = ...
 
+local RAID_CLASS_COLORS,FACTION_BAR_COLORS = RAID_CLASS_COLORS,FACTION_BAR_COLORS
+
 -----------------------------
 -- Init
 -----------------------------
@@ -28,6 +30,52 @@ RegisterStateDriver(frame, "display", L.C.frameVisibility)
 PlayerFrameTexture:SetVertexColor(unpack(L.C.textureColor))
 TargetFrameTextureFrameTexture:SetVertexColor(unpack(L.C.textureColor))
 TargetFrameToTTextureFrameTexture:SetVertexColor(unpack(L.C.textureColor))
+
+--add a bar texture to the player frame
+local playerColor = RAID_CLASS_COLORS[select(2, UnitClass("player"))]
+local playerFrameNameBackground = PlayerFrame:CreateTexture()
+playerFrameNameBackground:SetPoint("TOPLEFT",PlayerFrameBackground)
+playerFrameNameBackground:SetPoint("BOTTOMRIGHT",PlayerFrameBackground,0,22)
+playerFrameNameBackground:SetTexture(TargetFrameNameBackground:GetTexture())
+playerFrameNameBackground:SetVertexColor(playerColor.r,playerColor.g,playerColor.b,1)
+PlayerName:SetTextColor(1,1,1)
+
+--check for target events to listen for
+local function CanEventPass(event,unit)
+  if event == "PLAYER_TARGET_CHANGED" then
+    return true
+  --elseif event == "UNIT_HEALTH_FREQUENT" and unit == "target" then
+    --return true
+  elseif event == "UNIT_HEALTH" and unit == "target" then
+    return true
+  end
+  return false
+end
+
+--change the bar texture of target frame
+local function OnEvent(self,event,unit)
+  if not CanEventPass(event,unit) then return end
+  if not UnitExists("target") then return end
+  if not unit then unit = "target" end
+  local r,g,b
+  if UnitIsDeadOrGhost(unit) then
+    r,g,b = 0.4,0.4,0.4
+  elseif UnitIsUnit(unit.."target", "player") then
+    r,g,b = 1,0,0 --reverse threat color, red if I am the target
+  elseif UnitIsPlayer(unit) then
+    local _, class = UnitClass(unit)
+    local color = RAID_CLASS_COLORS[class]
+    r,g,b = color.r,color.g,color.b
+  elseif UnitIsTapDenied(unit) then
+    r,g,b = 0.9,0.9,0.9
+  else
+    local color = FACTION_BAR_COLORS[UnitReaction(unit, "player")]
+    r,g,b = color.r,color.g,color.b
+  end
+  TargetFrameNameBackground:SetVertexColor(r,g,b,1)
+end
+TargetFrame:HookScript("OnEvent", OnEvent)
+TargetFrameTextureFrameName:SetTextColor(1,1,1)
 
 --OnShow fader
 if L.C.fader then

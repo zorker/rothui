214ff050f558745662571d249a109ffb14752aaf
Erik Raetz <erik.raetz@googlemail.com>
2024-12-05 21:32:26 +0100

    Own toc parameters need to start with X-

--- wow11.0/rIngameModelViewer/init.lua
+++ wow11.0/rIngameModelViewer/init.lua
@@ -8,7 +8,7 @@ local A, L = ...
 L.name = A
 L.version = C_AddOns.GetAddOnMetadata(L.name, "Version")
 L.versionNumber = tonumber(L.version) or 0
-L.dbversion = tonumber(C_AddOns.GetAddOnMetadata(L.name, "DBVersion")) or 1
+L.dbversion = tonumber(C_AddOns.GetAddOnMetadata(L.name, "X-DB-Version")) or 1
 L.locale = GetLocale()
 
 L.F = {} -- local functions

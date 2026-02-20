97dadccc748662bc4cb1d6ade7305eb35b1e91f9
Erik Raetz <erik.raetz@googlemail.com>
2026-02-20 20:27:20 +0100

    Chat changes

--- wow12.0/rLayout/chat.lua
+++ wow12.0/rLayout/chat.lua
@@ -3,25 +3,37 @@ local addonName, ns = ...
 local editBoxFocusAlpha = 1
 local editBoxAlpha = 0
 
+local function ApplyClamp(cf)
+  ns.eventFrame.SetClampRectInsets(cf,0,0,0,0)
+end
+
 --SkinChat
 local function SkinChat()
   for i = 1, NUM_CHAT_WINDOWS do
-    local chatframe = _G["ChatFrame"..i]
-    if not chatframe then return end
-    local name = chatframe:GetName()
-    _G[name.."ButtonFrame"]:Hide()
-    _G[name.."EditBox"]:SetAltArrowKeyMode(false)
-    _G[name.."EditBox"]:ClearAllPoints()
-    _G[name.."EditBox"]:SetPoint("BOTTOMLEFT",_G[name.."Background"],"TOPLEFT",QuickJoinToastButton:GetWidth(),20)
-    _G[name.."EditBox"]:SetPoint("BOTTOMRIGHT",_G[name.."Background"],"TOPRIGHT",0,20)
-    _G[name.."EditBoxFocusLeft"]:SetAlpha(editBoxFocusAlpha)
-    _G[name.."EditBoxFocusMid"]:SetAlpha(editBoxFocusAlpha)
-    _G[name.."EditBoxFocusRight"]:SetAlpha(editBoxFocusAlpha)
-    _G[name.."EditBoxLeft"]:SetAlpha(editBoxAlpha)
-    _G[name.."EditBoxMid"]:SetAlpha(editBoxAlpha)
-    _G[name.."EditBoxRight"]:SetAlpha(editBoxAlpha)
-    chatframe:SetClampRectInsets(0,0,0,0)
-    hooksecurefunc(chatframe, "SetClampRectInsets", function(self) self:SetClampRectInsets(0,0,0,0) end)
+    local cf = _G["ChatFrame"..i]
+    if not cf then return end
+    local cbg   = _G["ChatFrame"..i.."Background"]
+    local cbf   = _G["ChatFrame"..i.."ButtonFrame"]
+    local ceb   = _G["ChatFrame"..i.."EditBox"]
+    local cebfl = _G["ChatFrame"..i.."EditBoxFocusLeft"]
+    local cebfm = _G["ChatFrame"..i.."EditBoxFocusMid"]
+    local cebfr = _G["ChatFrame"..i.."EditBoxFocusRight"]
+    local cebl  = _G["ChatFrame"..i.."EditBoxLeft"]
+    local cebm  = _G["ChatFrame"..i.."EditBoxMid"]
+    local cebr  = _G["ChatFrame"..i.."EditBoxRight"]
+    cbf:Hide()
+    ceb:SetAltArrowKeyMode(false)
+    ceb:ClearAllPoints()
+    ceb:SetPoint("BOTTOMLEFT",cbg,"TOPLEFT",QuickJoinToastButton:GetWidth(),20)
+    ceb:SetPoint("BOTTOMRIGHT",cbg,"TOPRIGHT",0,20)
+    cebfl:SetAlpha(editBoxFocusAlpha)
+    cebfm:SetAlpha(editBoxFocusAlpha)
+    cebfr:SetAlpha(editBoxFocusAlpha)
+    cebl:SetAlpha(editBoxAlpha)
+    cebm:SetAlpha(editBoxAlpha)
+    cebr:SetAlpha(editBoxAlpha)
+    cf:SetClampRectInsets(0,0,0,0)
+    hooksecurefunc(cf, "SetClampRectInsets", ApplyClamp)
   end
   TextToSpeechButtonFrame:Hide()
   ChatFrame1:ClearAllPoints()
@@ -30,4 +42,10 @@ local function SkinChat()
   QuickJoinToastButton:SetClampRectInsets(-10,-10,-10,-10)
 end
 
+local function MoveChat()
+  ChatFrame1:ClearAllPoints()
+  ChatFrame1:SetPoint("BOTTOMLEFT", 10, 10)
+end
+
 ns.SkinChat = SkinChat
+ns.MoveChat = MoveChat
--- wow12.0/rLayout/init.lua
+++ wow12.0/rLayout/init.lua
@@ -6,10 +6,20 @@ local function OnPlayerLogin()
   ns.DisableCombatFeedback()
 end
 
+local function OnPlayerEnteringWorld()
+  ns.MoveChat()
+end
+
 local eventFrame = CreateFrame("Frame")
 eventFrame:RegisterEvent("PLAYER_LOGIN")
+eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 eventFrame:SetScript("OnEvent", function(_, eventName)
   if eventName == "PLAYER_LOGIN" then
     OnPlayerLogin()
   end
+  if eventName == "PLAYER_ENTERING_WORLD" then
+    OnPlayerEnteringWorld()
+  end
 end)
+
+ns.eventFrame = eventFrame

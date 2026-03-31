1e302c549266c09c49c74416aa2c8dfd6e455bbd
Erik Raetz <erik.raetz@googlemail.com>
2026-03-31 20:52:21 +0200

    Just react to the spell start. API is secret.

--- wow12.0/rLayout/spellalert.lua
+++ wow12.0/rLayout/spellalert.lua
@@ -2,16 +2,20 @@ local A, L = ...
 
 --PlaySpellAlertSound
 local function PlaySpellAlertSound(unit, spellID, isChanneled)
+  --[[
   local spellName, notInterruptible, _;
   if isChanneled then
     spellName, _, _, _, _, _, notInterruptible = UnitChannelInfo(unit);
   else
     spellName, _, _, _, _, _, _, notInterruptible = UnitCastingInfo(unit);
   end
-  local isInterruptible = (notInterruptible == false);
-  if isInterruptible then
-    PlaySoundFile(7466102)
-  end
+  ]]
+  --local isInterruptible = (notInterruptible == false);
+  --if isInterruptible then
+    --PlaySoundFile(7466102) --riff
+    --PlaySoundFile(7466945) --bell
+    PlaySoundFile(1129273) --high bell
+  --end
 end
 
 L.F.PlaySpellAlertSound = PlaySpellAlertSound

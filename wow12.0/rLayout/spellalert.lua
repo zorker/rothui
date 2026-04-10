c7b3c0e34fb6261d502030f0bd167774efc078d1
Erik Raetz <erik.raetz@googlemail.com>
2026-04-10 08:28:42 +0200

    All of it is secure. Just make it beep when focus casts something

--- wow12.0/rLayout/spellalert.lua
+++ wow12.0/rLayout/spellalert.lua
@@ -1,21 +1,12 @@
 local A, L = ...
 
+local strfind, FocusFrameSpellBar = strfind, FocusFrameSpellBar
+
 --PlaySpellAlertSound
 local function PlaySpellAlertSound(unit, spellID, isChanneled)
-  --[[
-  local spellName, notInterruptible, _;
-  if isChanneled then
-    spellName, _, _, _, _, _, notInterruptible = UnitChannelInfo(unit);
-  else
-    spellName, _, _, _, _, _, _, notInterruptible = UnitCastingInfo(unit);
-  end
-  ]]
-  --local isInterruptible = (notInterruptible == false);
-  --if isInterruptible then
-    --PlaySoundFile(7466102) --riff
-    --PlaySoundFile(7466945) --bell
-    PlaySoundFile(1129273) --high bell
-  --end
+  --PlaySoundFile(7466102) --riff
+  --PlaySoundFile(7466945) --bell
+  PlaySoundFile(1129273) --high bell
 end
 
 L.F.PlaySpellAlertSound = PlaySpellAlertSound
